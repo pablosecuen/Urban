06b46239841bf83675ededa36aafabb830672bf5
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-04-23 03:42:40 -0300

    ruta de put de usuario

--- api/src/routers/inputs/user.ts
+++ api/src/routers/inputs/user.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { newUser } from "../../controllers/inputs/user";
+import { newUser,updateUser } from "../../controllers/inputs/user";
 
 const router = Router();
 
 // Ruta para crear un nuevo usuario
 
 router.post("/", newUser);
+router.put("/:id", updateUser);
 
 export default router;
