92f88f97006624002c70d83145b1a243e5769cc9
AgustinChavero <agustin1996orellano@gmail.com>
2023-04-24 16:13:45 -0300

    Rutas añadidas Index

--- api/src/routers/index.ts
+++ api/src/routers/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import userInputRouter from "./inputs/user";
 import userOutputRouter from "./outputs/user";
+import chauffeurInputRouter from "./inputs/chauffeur";
+import chauffeurOutputRouter from "./outputs/chauffeur";
 import travelInputRouter from "./inputs/travels";
 
 const router = Router();
@@ -11,7 +13,11 @@ router.use("/user", userInputRouter);
 // Rutas para salida de datos
 router.use("/user", userOutputRouter);
 
-router.use("/travels", travelInputRouter)
+// Rutas para Choferes
+router.use("/chauffeur", chauffeurInputRouter);
+router.use("/chauffeur", chauffeurOutputRouter);
+
+router.use("/travels", travelInputRouter);
 
 export default router;
 
