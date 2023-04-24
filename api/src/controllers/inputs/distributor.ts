44a4ccab5106c4759e7e21bfe53163470cc3aaa8
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-04-24 13:39:25 -0300

    controller updateDistributor

--- api/src/controllers/inputs/distributor.ts
+++ api/src/controllers/inputs/distributor.ts
@@ -22,3 +22,24 @@ export const newDistributor = async (req: Request, res: Response) => {
     res.status(400).json({ messege: error.message });
   }
 };
+
+export const updateDistributor = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id; // obtener id del distribuidor que se va a actualizar
+    const data: Distributor = req.body; //datos de distribuidor a actualizar
+
+    //verificar si existe el usuario en la base de datos
+    const docRef = await db.collection("distributors").doc(id).get();
+    if (!docRef.exists) {
+      throw new Error("No se encontró el distributor");
+    }
+    // Actualizar el usuario en Firestore
+    await db.collection("distributors").doc(id).update(data);
+    res
+      .status(201)
+      .json({ menssage: "Distribuidor actualizado correctamente" });
+  } catch (error) {
+    console.error("Error al actualizar el Distribuidor", error);
+    res.status(400).json({ messege: error.message });
+  }
+};
