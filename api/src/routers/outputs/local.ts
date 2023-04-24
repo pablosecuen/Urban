fb27bf0a8e933cf737366029e77d3c6d57ef3795
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-04-24 18:05:30 -0300

    getAllLocals - router

--- api/src/routers/outputs/local.ts
+++ api/src/routers/outputs/local.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { searchLocal } from "../../controllers/outputs/local";
+import { searchLocal, allLocals } from "../../controllers/outputs/local";
 
 const router = Router();
 
 // Ruta para obtener un local por su name
 router.get("/:name", searchLocal);
+
+router.get("/", allLocals);
 
 export default router;
