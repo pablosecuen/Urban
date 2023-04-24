42801a828629ba5e0902195682b6ccb0eaa56378
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-04-24 12:35:34 -0300

    rutas get de distribuidores

--- api/src/routers/index.ts
+++ api/src/routers/index.ts
@@ -3,17 +3,20 @@ import userInputRouter from "./inputs/user";
 import userOutputRouter from "./outputs/user";
 import travelInputRouter from "./inputs/travels";
 import distributorInputRouter from "./inputs/distributor";
+import distributorOutputRouter from "./outputs/distributor";
 
 const router = Router();
 
 // Rutas para entrada de datos
 router.use("/user", userInputRouter);
 
-// Rutas para salida de datos
-router.use("/user", userOutputRouter);
-
 router.use("/travels", travelInputRouter);
 
 router.use("/distributor", distributorInputRouter);
+
+// Rutas para salida de datos
+router.use("/user", userOutputRouter);
+
+router.use("/distributor", distributorOutputRouter);
 
 export default router;
