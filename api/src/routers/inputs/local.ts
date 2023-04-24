f39900bc16b4d91012fafac4484c7d211012d10f
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-04-24 17:42:59 -0300

    updateLocal - router

--- api/src/routers/inputs/local.ts
+++ api/src/routers/inputs/local.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { newLocal } from "../../controllers/inputs/local";
+import { newLocal, updateLocal } from "../../controllers/inputs/local";
 
 const router = Router();
 
 // Ruta para crear un nuevo usuario
 router.post("/", newLocal);
+
+router.put("/:id", updateLocal);
 
 export default router;
