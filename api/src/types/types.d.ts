81e70945a2bef23eaaf3c06e3111f19cfd15ed98
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-04-25 15:49:27 -0300

    ADD variable de products

--- api/src/types/types.d.ts
+++ api/src/types/types.d.ts
@@ -1,3 +1,5 @@
 type TravelStatus = "pending" | "approved" | "rejected";
 
 type OrderStatus = "pending" | "approved" | "rejected";
+
+type TypeProduct = "food" | "drink" | "snack" | "other" | "all";
