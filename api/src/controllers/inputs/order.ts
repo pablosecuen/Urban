77b9d73e4dfc2aee7f7a47141099e42824236c63
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-04-25 01:01:54 -0300

    add relaciones de orden con usuarios y repartidor

--- api/src/controllers/inputs/order.ts
+++ api/src/controllers/inputs/order.ts
@@ -1,12 +1,19 @@
 import { Request, Response } from "express";
 import { db } from "../../connection/connection";
+import firebase from 'firebase/compat/app';
 import Order from "../../schema/order";
 
 export const newOrder = async (req: Request, res: Response): Promise<void> => {
   try {
-    const data: Order = req.body;
+    const { userId, distributorId, ...data }: Order & { userId: string, distributorId: string } = req.body;
 
     const docRef = await db.collection("orders").add(data);
+    await db.collection('users').doc(userId).update({
+      'history.orders': firebase.firestore.FieldValue.arrayUnion(docRef.id)
+    });
+    await db.collection('distributors').doc(distributorId).update({
+      'history.orders': firebase.firestore.FieldValue.arrayUnion(docRef.id)
+    });
     res.status(201).json({ id: docRef.id });
   } catch (error) {
     console.error("Error al crear la orden", error);
