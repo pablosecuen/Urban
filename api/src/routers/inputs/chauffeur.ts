8b7cf96bd554f41422a7d5c2340ea706a44ca382
AgustinChavero <agustin1996orellano@gmail.com>
2023-04-26 14:47:52 -0300

    Adaptacion de routers inputs para borrado logico

--- api/src/routers/inputs/chauffeur.ts
+++ api/src/routers/inputs/chauffeur.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { newChauffeur, updateChauffeur } from "../../controllers/inputs/chauffeur";
-import { newChauffeurValidated, updateChauffeurValidated } from "../../utils/validations/chauffeur";
+import { newChauffeur, updateChauffeur, deleteChauffeur } from "../../controllers/inputs/chauffeur";
+import {
+  newChauffeurValidated,
+  updateChauffeurValidated,
+  deleteChauffeurValidated,
+} from "../../utils/validations/chauffeur";
 
 const router = Router();
 
 // Ruta para crear y actualizar choferes
 router.post("/", newChauffeurValidated, newChauffeur);
 router.put("/:id", updateChauffeurValidated, updateChauffeur);
+router.put("/delete/:id", deleteChauffeurValidated, deleteChauffeur);
 
 export default router;
