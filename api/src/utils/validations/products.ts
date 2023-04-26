cd2e19ed805f58a9e3d7b39b0fdfb13c16fd2d23
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-04-26 15:23:52 -0300

    ADD validacion de borrado prodcuto

--- api/src/utils/validations/products.ts
+++ api/src/utils/validations/products.ts
@@ -4,7 +4,7 @@ import { Products, ProductsToUpdate } from "../../schema/products";
 export const newProductValidated = (req: Request, res: Response, next: NextFunction): void => {
   try {
     const data: Products = req.body;
-    if (!data.name || !data.price || !data.description || !data.img || !data.type || !data.stock || data.localId )
+    if (!data.name || !data.price || !data.description || !data.img || !data.type || !data.stock || data.localId)
       throw Error("Datos incompletos");
     next();
   } catch (error) {
@@ -18,6 +18,22 @@ export const updateProductValidated = (req: Request, res: Response, next: NextFu
     const allowProperties = ["name", "price", "description", "img", "type", "stock", "deleted"];
     if (Object.keys(data).some((key) => !allowProperties.includes(key)))
       throw Error("Datos no permitidos");
+    next();
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const deleteProductValidated = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  try {
+    const data: ProductsToUpdate = req.body;
+    const allowProperties: string[] = ["delete"];
+    if (Object.keys(data).some((key) => !allowProperties.includes(key)))
+      throw new Error("Datos no permitidos");
     next();
   } catch (error) {
     res.status(400).json({ message: error.message });
