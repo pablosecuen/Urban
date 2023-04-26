c06716c5d96f3e420d3453f4b0f1f015c5501db0
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-04-26 18:02:06 -0300

    page gestiona tus viajes

--- client/app/home/gestion/page.tsx
+++ client/app/home/gestion/page.tsx
@@ -1,3 +1,66 @@
 export default function Gestion() {
-  return <div className="w-4/5 mx-auto border-2">Gestion</div>;
+  // Los botones dentro de cada li tienen que ser Links que nos lleven a la ruta "/home/gestion/${id}" con la id del viaje
+  return (
+    <div className="w-3/5 mx-auto p-10 h-3/5 flex flex-col gap-10">
+      <div className="rounded-3xl bg-sky-600 text-white font-bold tracking-widest p-4 text-center shadow-xl shadow-black/40">
+        <h3>Historial de viajes</h3>
+      </div>
+      <ul className="flex flex-col gap-3">
+        <li className="flex gap-4 px-4 items-center">
+          <div className="text-center border-2 border-blue rounded-full p-2">
+            <small>12/01/23</small>
+            <small>12:00-Medellin-La Ceja</small>
+          </div>
+          <div className="w-1/4">
+            <button>Gestionar</button>
+          </div>
+        </li>
+        <li className="flex gap-4 px-4 items-center">
+          <div className="text-center border-2 border-blue rounded-full p-2">
+            <small>12/01/23</small>
+            <small>12:00-Medellin-La Ceja</small>
+          </div>
+          <div className="w-1/4">
+            <button>Gestionar</button>
+          </div>
+        </li>
+        <li className="flex gap-4 px-4 items-center">
+          <div className="text-center border-2 border-blue rounded-full p-2">
+            <small>12/01/23</small>
+            <small>12:00-Medellin-La Ceja</small>
+          </div>
+          <div className="w-1/4">
+            <button>Gestionar</button>
+          </div>
+        </li>
+        <li className="flex gap-4 px-4 items-center">
+          <div className="text-center border-2 border-blue rounded-full p-2">
+            <small>12/01/23</small>
+            <small>12:00-Medellin-La Ceja</small>
+          </div>
+          <div className="w-1/4">
+            <button>Gestionar</button>
+          </div>
+        </li>
+        <li className="flex gap-4 px-4 items-center">
+          <div className="text-center border-2 border-blue rounded-full p-2">
+            <small>12/01/23</small>
+            <small>12:00-Medellin-La Ceja</small>
+          </div>
+          <div className="w-1/4">
+            <button>Gestionar</button>
+          </div>
+        </li>
+        <li className="flex gap-4 px-4 items-center">
+          <div className="text-center border-2 border-blue rounded-full p-2">
+            <small>12/01/23</small>
+            <small>12:00-Medellin-La Ceja</small>
+          </div>
+          <div className="w-1/4">
+            <button>Gestionar</button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
