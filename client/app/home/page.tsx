7067bc2cb11264eac4e8012e91363c765a84c044
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-04-26 17:53:44 -0300

    cambios en page

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -1,7 +1,19 @@
-import logo from "../../assets/imagenes/UrbanLogo.png";
-import Image, { StaticImageData } from "next/image";
-import { Children } from "react";
-
 export default function Home() {
-  return <></>;
+  return (
+    <div className="flex flex-col w-4/5">
+      <p className="text-center font-bold text-2xl my-2">Bienvedios a Urban!</p>
+      <p className="text-center">
+        Nuestra mision es entregar al usuario el poder de decidir su medio de transporte acercando a
+        la oferta a la palma de sumano, empoderandote con mayor información asistiendo en que tomes
+        la mejor decision disponble dentro de tus necesidades, en nuestra app podras encontrar todo
+        tipo de servicios de transporte, desde buses intermunicipales, taxis publicos, transportes
+        privados, inclusive una seccion de cadeteria, para que puedas encontrar soluciones reales a
+        tus problemas cotidianos en la menor cantidad de tiempo posibles, acercando a tu mano un
+        poder increible para gestionar tu tiempo y tus días de la mejor manera posible
+      </p>
+      <p className="text-center mt-8 font-bold text-xl">
+        Selecciona el tipo de servicio para poder continuar
+      </p>
+    </div>
+  );
 }
