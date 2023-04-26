e50d1c6e89a466042cf84da2047db5346d3c6854
Benja <benyamin0714@gmail.com>
2023-04-26 19:18:44 -0300

    Muy pequenia correccion

--- client/app/home/reserva/page.tsx
+++ client/app/home/reserva/page.tsx
@@ -9,7 +9,10 @@ import { MdPets } from "react-icons/md";
 export default function Reserva() {
   return (
     <div className="w-4/5 mx-auto border-2">
-      <form className="flex flex-col gap-5 py-32 items-center justify-center">
+      <h1 className="text-blue text-center text-xl mt-12">
+        Llena el formulario para encontrar tu viaje
+      </h1>
+      <form className="flex flex-col gap-5 pb-32 pt-12 items-center justify-center">
         <div className="flex items-center justify-center">
           <HiOutlineLocationMarker className="w-10 text-blue" />
           <input className="pl-2 w-2/3" placeholder="Desde que lugar..." type="text" />
