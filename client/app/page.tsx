fd564dbb6f20c0f33afa1ec95bc839ee1e3a95a3
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-04-26 15:39:24 -0300

    page padre

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,27 @@
+"use client";
 import Login from "@component/components/Login/Login";
 import logo from "../assets/imagenes/UrbanLogo.png";
 import Image, { StaticImageData } from "next/image";
+import { useState } from "react";
+
+interface LoginProps {
+  onLogin: (email: string, password: string) => void;
+  onRegisterClick: () => void;
+  onRegister: (name: string, email: string, password: string) => void;
+  onLoginClick: () => void;
+}
 
 export default function Home() {
-  return <></>;
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <>
+      <div className="h-full border-2 flex justify-center align-center items-center sm:h-full">
+        <Image src={logo as StaticImageData} alt="logo" className="w-full aspect-ratio-square" />
+      </div>
+      <div className="h-full border-2 sm:h-full">
+        <Login />
+      </div>
+    </>
+  );
 }
