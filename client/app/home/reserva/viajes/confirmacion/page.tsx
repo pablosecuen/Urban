c69dae819356e329d133570b31ef272a33b48985
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-04-26 17:53:50 -0300

    cambios en page

--- client/app/home/reserva/viajes/confirmacion/page.tsx
+++ client/app/home/reserva/viajes/confirmacion/page.tsx
@@ -1,4 +1,29 @@
 "use client";
 export default function Confirmacion() {
-  return <div className="w-4/5 mx-auto border-2">confirmacion</div>;
+  return (
+    <div className="flex flex-col justify-center items-center gap-4">
+      <p className="lg:font-bold lg:text-3xl lg:text-center">Viaje Confirmado</p>
+      <div className="flex justify-center">
+        <p className="lg:font-bold lg:text-3xl lg:text-center">Nombre del conductor</p>
+        <p className=" lg:text-3xl lg:text-center">Luis martin</p>
+      </div>
+      <div className="flex justify-center">
+        <p className="lg:font-bold lg:text-3xl lg:text-center"> Hora de recogida</p>
+        <p className=" lg:text-3xl lg:text-center">3:00 horas</p>
+      </div>
+      <div className="flex justify-center">
+        <p className="lg:font-bold lg:text-3xl lg:text-center">Lugar de recogida</p>
+        <p className=" lg:text-3xl lg:text-center">la casa de ima</p>
+      </div>
+      <div className="flex justify-center">
+        <p className="lg:font-bold lg:text-3xl lg:text-center">Valor a pagar</p>
+        <p className=" lg:text-3xl lg:text-center">un toco de plata</p>
+      </div>
+      <p className="text-gray-400 text-center">
+        {" "}
+        tocando el boton para abonar el servicio aceptas nuestros terminos y condiciones de uso
+      </p>
+      <button className="w-1/2 mt-10">Ir a pagar</button>
+    </div>
+  );
 }
