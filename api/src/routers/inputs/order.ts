133ef799cbd9755e479227d1b3fed10a7356f15a
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-04-26 10:45:22 -0300

    change validacion de ordenes

--- api/src/routers/inputs/order.ts
+++ api/src/routers/inputs/order.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
 import { newOrder, updateOrder } from "../../controllers/inputs/order";
+import { newOrderValidate } from "../../utils/validations/order";
 
 const router = Router();
 
 //Ruta creancion Orden
-router.post("/", newOrder);
+router.post("/", newOrderValidate, newOrder);
 //Ruta actualizar Orden
 router.put("/:id", updateOrder);
 
--- api/src/utils/validations/order.ts
+++ api/src/utils/validations/order.ts
@@ -1,17 +1,23 @@
 import Order from "../../schema/order";
+import { Request, Response, NextFunction } from "express";
 
-export const validateOrder = (dataOrder: Order): boolean => {
-  if (
-    !dataOrder.userId ||
-    !dataOrder.distributorId ||
-    !dataOrder.localId ||
-    !dataOrder.date ||
-    !dataOrder.price ||
-    !dataOrder.destination ||
-    !dataOrder.order
-  ) {
-    return false;
+export const newOrderValidate = (req: Request, res: Response, next: NextFunction): void => {
+  // Validar que todas las propiedades tengan un valor válido
+  try {
+    const dataOrder: Order = req.body;
+    if (
+      !dataOrder.userId ||
+      !dataOrder.distributorId ||
+      !dataOrder.localId ||
+      !dataOrder.date ||
+      !dataOrder.price ||
+      !dataOrder.destination ||
+      !dataOrder.order
+    ) {
+      throw new Error("Faltan datos");
+      next();
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
-
-  return true;
 };
