dbcbaefb37afe8251b57b39d8dfb0aad3ae0044e
Benja <benyamin0714@gmail.com>
2023-04-26 18:50:39 -0300

    Page reserva

--- client/app/home/reserva/page.tsx
+++ client/app/home/reserva/page.tsx
@@ -1,3 +1,47 @@
+import {
+  HiUserGroup,
+  HiOutlineCalendar,
+  HiOutlineBriefcase,
+  HiOutlineLocationMarker,
+} from "react-icons/hi";
+import { MdPets } from "react-icons/md";
+
 export default function Reserva() {
-  return <div className="w-4/5 mx-auto border-2">Reserva</div>;
+  return (
+    <div className="w-4/5 mx-auto border-2">
+      <form className="flex flex-col gap-5 py-32 items-center justify-center">
+        <div className="flex items-center justify-center">
+          <HiOutlineLocationMarker className="w-10 text-blue" />
+          <input className="pl-2 w-2/3" placeholder="Desde que lugar..." type="text" />
+        </div>
+
+        <div className="flex items-center justify-center">
+          <HiOutlineLocationMarker className="w-10 text-blue" />
+          <input className="pl-2 w-2/3" placeholder="Hasta que lugar..." type="text" />
+        </div>
+
+        <div className="flex items-center justify-center">
+          <HiOutlineCalendar className="w-10 text-blue" />
+          <input className="pl-2 w-2/3" placeholder="Cuando?..." type="text" />
+        </div>
+
+        <div className="flex items-center justify-center">
+          <HiUserGroup className="w-10 text-blue" />
+          <input className="pl-2 w-2/3" placeholder="Cantidad de pasajeros..." type="number" />
+        </div>
+
+        <div className="flex items-center justify-center">
+          <HiOutlineBriefcase className="w-10 text-blue" />
+          <input className="pl-2 w-2/3" placeholder="Equipaje..." type="text" />
+        </div>
+
+        <div className="flex items-center justify-center">
+          <MdPets className="w-10 text-blue" />
+          <input className="pl-2 w-2/3" placeholder="Mascotas..." type="text" />
+        </div>
+
+        <button className="w-1/2">Buscar tu viaje!</button>
+      </form>
+    </div>
+  );
 }
