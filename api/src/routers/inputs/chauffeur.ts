a6737f164b18cb487871228b71296de12bb6f3aa
AgustinChavero <agustin1996orellano@gmail.com>
2023-04-26 13:17:53 -0300

    Modificacion de rutas para las validaciones

--- api/src/routers/inputs/chauffeur.ts
+++ api/src/routers/inputs/chauffeur.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { newChauffeur, updateChauffeur } from "../../controllers/inputs/chauffeur";
+import { newChauffeurValidated, updateChauffeurValidated } from "../../utils/validations/chauffeur";
 
 const router = Router();
 
 // Ruta para crear y actualizar choferes
-router.post("/", newChauffeur);
-router.put("/:id", updateChauffeur);
+router.post("/", newChauffeurValidated, newChauffeur);
+router.put("/:id", updateChauffeurValidated, updateChauffeur);
 
 export default router;
