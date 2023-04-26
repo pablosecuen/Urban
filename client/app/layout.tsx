884a84eb0f139727a0c51b8c98a49004d6ed2b13
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-04-26 15:39:14 -0300

    layout padre

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,9 +1,5 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import NavBar from "../components/NavBar/NavBar";
-import logo from "../assets/imagenes/UrbanLogo.png";
-import Image, { StaticImageData } from "next/image";
-import Login from "@component/components/Login/Login";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,27 +8,11 @@ export const metadata = {
   description: "Solucion en tranportes a tu alcance ",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="flex flex-col h-full sm:flex-row">
-          <div className="h-full border-2 flex justify-center align-center items-center sm:h-full">
-            <Image
-              src={logo as StaticImageData}
-              alt="logo"
-              className="w-full aspect-ratio-square"
-            />
-          </div>
-          <div className="h-full border-2 sm:h-full">
-            <Login />
-            {children}
-          </div>
-        </main>
+        <main className="flex flex-col h-full sm:flex-row">{children}</main>
       </body>
     </html>
   );
