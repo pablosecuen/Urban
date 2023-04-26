86d5bea68d2f1479e5ef5f8ca6de6f3582a5a61d
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-04-26 09:16:46 -0300

    Change en validacion de distribuidores

--- api/src/routers/inputs/distributor.ts
+++ api/src/routers/inputs/distributor.ts
@@ -1,13 +1,11 @@
 import { Router } from "express";
-import {
-  newDistributor,
-  updateDistributor,
-} from "../../controllers/inputs/distributor";
+import { newDistributor, updateDistributor } from "../../controllers/inputs/distributor";
+import { newDistributorValidate } from "../../utils/validations/distributor";
 
 const router = Router();
 
 //Ruta creancion distribuidor
-router.post("/", newDistributor);
+router.post("/", newDistributorValidate, newDistributor);
 //Ruta actualizar distribuidor
 router.put("/:id", updateDistributor);
 
