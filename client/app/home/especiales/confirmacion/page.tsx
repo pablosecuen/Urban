768d7764794a758abbe2ffeb6dfa98c1d86801c4
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-04-27 16:50:02 -0300

    sombreado sutil para formuladiors

--- client/app/home/especiales/confirmacion/page.tsx
+++ client/app/home/especiales/confirmacion/page.tsx
@@ -4,23 +4,23 @@ import Link from "next/link";
 
 export default function Confirmacion() {
   return (
-    <div className="flex flex-col justify-center items-center gap-4">
-      <p className="lg:font-bold lg:text-3xl lg:text-center">Pedido especial confirmado</p>
+    <div className="flex flex-col justify-center items-center gap-4 py-20 lg:py-0">
+      <p className="font-bold lg:text-3xl text-center">Pedido especial confirmado</p>
       <div className="flex justify-center">
-        <p className="lg:font-bold lg:text-3xl lg:text-center">Nombre del ccadete</p>
-        <p className=" lg:text-3xl lg:text-center">Luis martin</p>
+        <p className="font-bold lg:text-3xl text-center">Nombre del cadete</p>
+        <p className=" lg:text-3xl text-center">Luis martin</p>
       </div>
       <div className="flex justify-center">
-        <p className="lg:font-bold lg:text-3xl lg:text-center"> Hora de recogida</p>
-        <p className=" lg:text-3xl lg:text-center">3:00 horas</p>
+        <p className="font-bold lg:text-3xl text-center"> Hora de recogida</p>
+        <p className=" lg:text-3xl text-center">3:00 horas</p>
       </div>
       <div className="flex justify-center">
-        <p className="lg:font-bold lg:text-3xl lg:text-center">Lugar de recogida</p>
-        <p className=" lg:text-3xl lg:text-center">Lugar indicado</p>
+        <p className="font-bold lg:text-3xl text-center">Lugar de recogida</p>
+        <p className=" lg:text-3xl text-center">Lugar indicado</p>
       </div>
       <div className="flex justify-center">
-        <p className="lg:font-bold lg:text-3xl lg:text-center">Valor a pagar</p>
-        <p className=" lg:text-3xl lg:text-center">un toco de plata</p>
+        <p className="font-bold lg:text-3xl text-center">Valor a pagar</p>
+        <p className=" lg:text-3xl text-center">un toco de plata</p>
       </div>
       <p className="text-gray-400 text-center">
         {" "}
