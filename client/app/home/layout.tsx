17c62fcf37de878de9be94023f97eab3c0da23e4
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-04-27 16:33:01 -0300

    cambkios para mobile

--- client/app/home/layout.tsx
+++ client/app/home/layout.tsx
@@ -4,6 +4,7 @@ import logo from "../../assets/imagenes/UrbanIsoLogo.png";
 import Image, { StaticImageData } from "next/image";
 
 import Menu from "@component/components/Menu/Menu";
+import Link from "next/link";
 
 export default function Home({ children }: { children: React.ReactNode }) {
   return (
@@ -12,10 +13,9 @@ export default function Home({ children }: { children: React.ReactNode }) {
         <NavBar />
       </header>
 
-      <div className="flex-col container flex lg:flex-row justify-center items-center py-20">
-        <div className="lg:w-1/2 w-full flex flex-col gap-8 justify-center items-center">
-          <Image src={logo as StaticImageData} alt="logo" className="lg:w-96 w-48" />
-
+      <div className="flex-col container flex lg:flex-row justify-center mx-auto items-center lg:pb-20">
+        <div className="lg:w-1/2 w-full flex flex-col gap-8 justify-center  items-center">
+          <Image src={logo as StaticImageData} alt="logo" className="lg:w-96 w-24" />
           <Menu />
         </div>
         <div className="lg:w-1/2 w-full  flex justify-center items-center">{children}</div>
