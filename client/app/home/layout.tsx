462e5e0c2c63d3a51be70826a3c92313646b4caf
httpfido <fidel.romano.2016@hotmail.com>
2023-04-27 03:47:59 -0300

    implementacion archivo data

--- client/app/home/layout.tsx
+++ client/app/home/layout.tsx
@@ -2,11 +2,6 @@ import NavBar from "@component/components/NavBar/NavBar";
 import Footer from "@component/components/Footer/Footer"
 import logo from "../../assets/imagenes/UrbanLogo.png";
 import Image, { StaticImageData } from "next/image";
-// import sos from "../../assets/icons/sos.png";
-// import escudo from "../../assets/icons/escudo.png";
-// import planet from "../../assets/icons/planet.png";
-// import contract from "../../assets/icons/contract.png";
-// import Link from "next/link";
 import Menu from "@component/components/Menu/Menu";
 
 export default function Home({ children }: { children: React.ReactNode }) {
@@ -23,41 +18,6 @@ export default function Home({ children }: { children: React.ReactNode }) {
         <div className="w-1/2  flex justify-center items-center">{children}</div>
       </div>
       <Footer />
-      {/* <footer className="h-96 bg-verde flex items-center align-middle justify-center">
-        <div className="flex flex-col">
-          {" "}
-          <span className="text-black text-3xl font-bold text-center  flex justify-center">
-            Todo lo que necesitas saber
-          </span>
-          <span className=" text-center  flex justify-center">
-            Encuentra la oferta de servicios de transporte terrestre público y privado disponible en
-            tiempo real
-          </span>
-          <div className="flex justify-around items-center align-middle mt-10">
-            <div className="flex flex-col h-48  w-60 gap-2 justify-between">
-              <Image src={sos} alt="sos" className="h-28 w-28 flex self-center "></Image>
-              <p className="text-center">
-                Verificamos cuidadosamente la identidad y antecendentes de nuestros transportadores
-                y usuarios
-              </p>
-            </div>
-            <div className="flex flex-col  w-60  h-48  gap-2 justify-between">
-              <Image src={escudo} alt="protected" className="h-28 w-28 flex self-center"></Image>
-              <p className="text-center">Nuestro boton de emergencia esta disponible 24 horas</p>
-            </div>
-            <div className="flex flex-col  w-60 gap-2  h-48 justify-between">
-              <Image src={planet} alt="sos" className="h-32 w-32 flex self-center"></Image>
-              <p className="text-center">
-                Viajando con nuestros aliados contribuyes al cuidado del medio ambiente
-              </p>
-            </div>
-            <div className="flex flex-col  w-60  gap-2  h-48 justify-between">
-              <Image src={contract} alt="sos" className="h-28 w-28 flex self-center"></Image>
-              <p className="text-center">Nuestras politicas</p>
-            </div>
-          </div>
-        </div>
-      </footer> */}
     </div>
   );
 }
---
+++ client/assets/data.js
@@ -0,0 +1,35 @@
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * I M P O R T S * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+
+// Icons Footer
+import sos from "./icons/sos.png";
+import escudo from "./icons/escudo.png";
+import planet from "./icons/planet.png";
+import contract from "./icons/contract.png";
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * E X P O R T S * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+
+// Icons Footer
+export const iconsData = [
+  {
+    image: sos,
+    alt: "sos",
+    description:
+      "Verificamos cuidadosamente la identidad y antecedentes de nuestros transportadores y usuarios",
+  },
+  {
+    image: escudo,
+    alt: "protected",
+    description: "Nuestro botón de emergencia está disponible 24 horas",
+  },
+  {
+    image: planet,
+    alt: "sos",
+    description:
+      "Viajando con nuestros aliados contribuyes al cuidado del medio ambiente",
+  },
+  {
+    image: contract,
+    alt: "sos",
+    description: "Nuestras políticas",
+  },
+];
