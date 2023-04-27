58658cff92decdbaa2b7014cfb9eb74ed39b2654
AgustinChavero <agustin1996orellano@gmail.com>
2023-04-27 19:04:23 -0300

    Eliminacion de validaciones delete en ruta de product

--- api/src/routers/inputs/product.ts
+++ api/src/routers/inputs/product.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { newProduct, updateProduct, deletedProduct } from "../../controllers/inputs/product";
-import { deleteProductValidated, newProductValidated, updateProductValidated } from "../../utils/validations/products";
+import { newProductValidated, updateProductValidated } from "../../utils/validations/products";
 
 const router = Router();
 
 router.post("/", newProductValidated, newProduct);
 router.put("/:id", updateProductValidated, updateProduct);
-router.delete("/:id", deleteProductValidated, deletedProduct);
+router.delete("/:id", deletedProduct);
 
 export default router;
