51f2f8c5129fa68ae91a69d30bdbc1897caf42e0
AgustinChavero <agustin1996orellano@gmail.com>
2023-04-27 11:32:26 -0300

    Ruta de login-chauffeur habilitada

--- api/src/routers/inputs/login.ts
+++ api/src/routers/inputs/login.ts
@@ -4,12 +4,11 @@ import { loginChauffeur } from "../../controllers/inputs/login/loginChauffeur";
 import { loginDistrutor } from "../../controllers/inputs/login/loginDistrutor";
 import { loginLocal } from "../../controllers/inputs/login/loginLocal";
 
-
 const router = Router();
 
 router.post("/user", loginUser);
 
-// router.post("/chauffeur", loginChauffeur);
+router.post("/chauffeur", loginChauffeur);
 
 // router.post("/distrutor", loginDistrutor);
 
