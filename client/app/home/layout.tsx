ce04c08f4cde6fa09ba4d2c0a04074a2eaa0062f
Benja <benyamin0714@gmail.com>
2023-04-27 12:45:35 -0300

    Puse un borde nomas

--- client/app/home/layout.tsx
+++ client/app/home/layout.tsx
@@ -1,5 +1,5 @@
 import NavBar from "@component/components/NavBar/NavBar";
-import Footer from "@component/components/Footer/Footer"
+import Footer from "@component/components/Footer/Footer";
 import logo from "../../assets/imagenes/UrbanIsoLogo.png";
 import Image, { StaticImageData } from "next/image";
 import Menu from "@component/components/Menu/Menu";
@@ -7,11 +7,11 @@ import Menu from "@component/components/Menu/Menu";
 export default function Home({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <header >
+      <header>
         <NavBar />
       </header>
       <div className="flex-col container mx-auto lg:flex lg:flex-row lg:justify-center py-20">
-        <div className="w-1/2 flex flex-col gap-8 justify-center items-center">
+        <div className="w-1/2 border-2 flex flex-col gap-8 justify-center items-center">
           <Image src={logo as StaticImageData} alt="logo" className="w-96" />
           <Menu />
         </div>
