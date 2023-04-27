0942784ede2a660182f931905c1deae8ae5644ae
Benja <benyamin0714@gmail.com>
2023-04-27 12:46:08 -0300

    Componente modificado

--- client/app/home/reserva/viajes/page.tsx
+++ client/app/home/reserva/viajes/page.tsx
@@ -1,44 +1,52 @@
 import { FaMotorcycle, FaCar, FaBus, FaTaxi } from "react-icons/fa";
 export default function Viajes() {
   return (
-    <div className="w-4/5 mx-auto border-2 p-20">
+    <div className="ml-12 mx-auto border-2 p-10">
       <div className="flex flex-col gap-10 border-2">
         <h1 className="text-blue text-center text-xl">
           Estas son las mejores opciones encontradas
         </h1>
-        <div className="flex items-center justify-center">
-          <FaMotorcycle size="x10" className="text-blue w-10" />
-          <span className="text-center">
-            Cootransandina - 1:00pm <br />
-            Desde: Envigrado ST, Hasta: Lugar <br />
-            tiempo estimado: 4 horas
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex gap-2 items-center">
+            <FaBus size="50" className="text-blue w-20" />
+            <span className="text-gray-600 font-semibold text-xl">Bus</span>
+          </div>
+          <span className="pt-2 text-gray-600">
+            Conductor: Luis G. Cootransandina - 1:00pm - Estrella St <br />
+            Tiempo estimado: 4 horas
           </span>
         </div>
         <hr />
-        <div className="flex items-center justify-center">
-          <FaCar size="x10" className="text-blue w-10" />
-          <span className="text-center">
-            Cootransandina - 1:00pm <br />
-            Desde: Envigrado ST, Hasta: Lugar <br />
-            tiempo estimado: 4 horas
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex gap-2 items-center">
+            <FaCar size="50" className="text-blue w-20" />
+            <span className="text-gray-600 font-semibold text-xl">Automovil privado</span>
+          </div>
+          <span className="pt-2 text-gray-600">
+            Conductor: Luis G. Cootransandina - 1:00pm - Estrella St <br />
+            Tiempo estimado: 4 horas
           </span>
         </div>
         <hr />
-        <div className="flex items-center justify-center">
-          <FaBus size="x10" className="text-blue w-10" />
-          <span className="text-center">
-            Cootransandina - 1:00pm <br />
-            Desde: Envigrado ST, Hasta: Lugar <br />
-            tiempo estimado: 4 horas
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex gap-2 items-center">
+            <FaTaxi size="50" className="text-blue w-20" />
+            <span className="text-gray-600 font-semibold text-xl">Taxi Publico</span>
+          </div>
+          <span className="pt-2 text-gray-600">
+            Conductor: Luis G. Cootransandina - 1:00pm - Estrella St <br />
+            Tiempo estimado: 4 horas
           </span>
         </div>
         <hr />
-        <div className="flex items-center justify-center">
-          <FaTaxi size="x10" className="text-blue w-10" />
-          <span className="text-center">
-            Cootransandina - 1:00pm <br />
-            Desde: Envigrado ST, Hasta: Lugar <br />
-            tiempo estimado: 4 horas
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex gap-2 items-center">
+            <FaMotorcycle size="50" className="text-blue w-20" />
+            <span className="text-gray-600 font-semibold text-xl">Motocicleta privada</span>
+          </div>
+          <span className="pt-2 text-gray-600">
+            Conductor: Luis G. Cootransandina - 1:00pm - Estrella St <br />
+            Tiempo estimado: 4 horas
           </span>
         </div>
       </div>
