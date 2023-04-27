6a4503624312d039b3d142966c238e8bb975669b
AgustinChavero <agustin1996orellano@gmail.com>
2023-04-27 19:04:38 -0300

    Eliminacion de validaciones delete en ruta de owner

--- api/src/routers/inputs/owner.ts
+++ api/src/routers/inputs/owner.ts
@@ -1,16 +1,12 @@
 import { Router } from "express";
 import { newOwner, updateOwner, deleteOwner } from "../../controllers/inputs/owner";
-import {
-  newOwnerValidated,
-  updateOwnerValidated,
-  deleteOwnerValidated,
-} from "../../utils/validations/owner";
+import { newOwnerValidated, updateOwnerValidated } from "../../utils/validations/owner";
 
 const router = Router();
 
 // Ruta para crear y actualizar choferes
 router.post("/", newOwnerValidated, newOwner);
 router.put("/:id", updateOwnerValidated, updateOwner);
-router.put("/delete/:id", deleteOwnerValidated, deleteOwner);
+router.delete("/delete/:id", deleteOwner);
 
 export default router;
