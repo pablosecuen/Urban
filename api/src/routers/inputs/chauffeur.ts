d3e5d326a43d0f0a202f8d128288967e37ef5337
AgustinChavero <agustin1996orellano@gmail.com>
2023-04-27 19:05:20 -0300

    Eliminacion de validaciones delete en ruta de chauffeur

--- api/src/routers/inputs/chauffeur.ts
+++ api/src/routers/inputs/chauffeur.ts
@@ -1,16 +1,12 @@
 import { Router } from "express";
 import { newChauffeur, updateChauffeur, deleteChauffeur } from "../../controllers/inputs/chauffeur";
-import {
-  newChauffeurValidated,
-  updateChauffeurValidated,
-  deleteChauffeurValidated,
-} from "../../utils/validations/chauffeur";
+import { newChauffeurValidated, updateChauffeurValidated } from "../../utils/validations/chauffeur";
 
 const router = Router();
 
 // Ruta para crear y actualizar choferes
 router.post("/", newChauffeurValidated, newChauffeur);
 router.put("/:id", updateChauffeurValidated, updateChauffeur);
-router.put("/delete/:id", deleteChauffeurValidated, deleteChauffeur);
+router.delete("/delete/:id", deleteChauffeur);
 
 export default router;
