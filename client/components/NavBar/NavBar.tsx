b0f9df30044d8b552f64bc76832c9f15090dfd9a
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-04-28 19:10:40 -0300

    seteo de redux con un get all user de prueba

--- client/components/NavBar/NavBar.tsx
+++ client/components/NavBar/NavBar.tsx
@@ -1,9 +1,8 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image, { StaticImageData } from "next/image";
 import logo from "../../assets/imagenes/UrbanIso.png";
-
 import { useMediaQuery } from "react-responsive";
 import { links, linksMobile } from "../../assets/data";
 import { HiMenuAlt1 } from "react-icons/hi";
@@ -18,61 +17,52 @@ export default function NavBar() {
     console.log(showMenu);
   };
 
+  // const handleLogout = () => {
+  //   localStorage.removeItem("user");
+  //   setUserData(null);
+  // };
+
   return (
     <header>
       {isMobile ? (
-        <nav className="w-full h-12 flex bg-verde justify-between ">
-          <Image
-            src={logo as StaticImageData}
-            alt="logo"
-            className="h-10 w-auto self-center"
-          />
+        <nav className="flex h-12 w-full justify-between bg-verde ">
+          <Image src={logo as StaticImageData} alt="logo" className="h-10 w-auto self-center" />
 
           <HiMenuAlt1
             onClick={toggleMenu}
-            className="md:hidden text-xl mr-4 cursor-pointer w-1/12 h-full z-10"
+            className="z-10 mr-4 h-full w-1/12 cursor-pointer text-xl md:hidden"
           />
 
           <div
-            className={`md:hidden absolute w-0 overflow-hidden top-12 left-0  h-full bg-white  transition-all ease-in-out duration-1000 ${
+            className={`absolute left-0 top-12 h-full w-0 overflow-hidden  bg-white transition-all  duration-1000 ease-in-out md:hidden ${
               showMenu && "w-1/2 shadow-custom-md"
             }`}
           >
             <ul className="flex flex-col items-center p-4">
               {linksMobile.map((link) => (
                 <li key={link.id} className="my-2 flex items-center ">
-                  <link.icon className="w-6 h-6" />
+                  <link.icon className="h-6 w-6" />
                   <Link href={link.route} onClick={toggleMenu}>
                     <span className="mx-2">{link.label}</span>
                   </Link>
                 </li>
               ))}
             </ul>
-
           </div>
         </nav>
       ) : (
-        <nav className="w-full h-12 bg-verde">
+        <nav className="h-12 w-full bg-verde">
           <div className="container mx-auto flex justify-between">
-            <Image
-              src={logo as StaticImageData}
-              alt="logo"
-              className="h-10 w-auto self-center"
-            />
+            <Image src={logo as StaticImageData} alt="logo" className="h-10 w-auto self-center" />
 
-            <ul className="flex space-x-4 w-1/3 items-center">
+            <ul className="flex w-1/3 items-center space-x-4">
               {links.map((link) => (
-             
-                  <li
-                    key={link.id}
-                    className="flex items-center px-2 justify-center"
-                  >
-                    <Link href={link.route} className="flex">
-                      <link.icon className="w-5 h-6" />
-                      {link.label}
-                    </Link>
-                  </li>
-               
+                <li key={link.id} className="flex items-center justify-center px-2">
+                  <Link href={link.route} className="flex">
+                    <link.icon className="h-6 w-5" />
+                    {link.label}
+                  </Link>
+                </li>
               ))}
             </ul>
           </div>
