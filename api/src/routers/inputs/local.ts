788bf9895caee4100c6b55fefb5e8f2abc7b6f51
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-04-28 12:44:28 -0300

    locals - ruta y controlador de habilitacion

--- api/src/routers/inputs/local.ts
+++ api/src/routers/inputs/local.ts
@@ -1,13 +1,12 @@
 import { Router } from "express";
-import { deleteLocal, newLocal, updateLocal } from "../../controllers/inputs/local";
+import { deleteLocal, newLocal, updateLocal, enableLocal } from "../../controllers/inputs/local";
 import { newLocalValidate, updateLocalValidate } from "../../utils/validations/local";
 
 const router = Router();
 
 router.post("/", newLocalValidate, newLocal);
-
 router.put("/:id", updateLocalValidate, updateLocal);
-
+router.patch("/enable/:id", enableLocal);
 router.delete("/delete/:id", deleteLocal);
 
 export default router;
