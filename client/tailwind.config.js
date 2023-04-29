ce6f094cd9e0432a92a4d314bb79aa4cccac99e8
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-04-29 15:30:02 -0300

    construiccion de un dashboard con rutas multiproposito

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -12,6 +12,18 @@ module.exports = {
         verdeurban: "#76F900",
         celeste: "#07BABF",
         blue: "rgb(59,130,246)",
+        blueGray: {
+          50: "#F8FAFC",
+          100: "#F1F5F9",
+          200: "#E2E8F0",
+          300: "#CBD5E1",
+          400: "#334155",
+          500: "#64748B",
+          600: "#334155", // Added blueGray-600 color
+          700: "#334155",
+          800: "#343746",
+          900: "#1F2933",
+        },
       },
       boxShadow: {
         "custom-md": "5px 13px 20px black",
