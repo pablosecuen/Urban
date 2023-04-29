9958efb4d7dcdc1fd89f7d27f885e73cbebdf02a
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-04-29 12:41:59 -0300

    pol

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
+require("dotenv").config();
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
