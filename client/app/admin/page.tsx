5176e7d4bad2f25a5dc08ec49144631db1d49860
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-04-29 15:30:06 -0300

    construiccion de un dashboard con rutas multiproposito

--- client/app/admin/page.tsx
+++ client/app/admin/page.tsx
@@ -15,11 +15,13 @@ export default function Dashboard() {
   return (
     <>
       <div className="flex flex-wrap">
-        <div className="mb-12 w-full px-4 xl:mb-0 xl:w-8/12">
-          <CardLineChart />
-        </div>
-        <div className="w-full px-4 xl:w-4/12">
-          <CardBarChart />
+        <div className="mt-4 flex flex-wrap ">
+          <div className="mb-12 w-full px-4 xl:mb-0 xl:w-8/12">
+            <CardLineChart />
+          </div>
+          <div className="w-full px-4 xl:w-4/12">
+            <CardBarChart />
+          </div>
         </div>
       </div>
       <div className="mt-4 flex flex-wrap">
