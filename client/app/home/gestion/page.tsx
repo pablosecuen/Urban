d27c453d6f8d4d13fd10cc69a0a95f5a7920e5d4
httpfido <fidel.romano.2016@hotmail.com>
2023-04-28 22:51:39 -0300

    /gestion mobile

--- client/app/home/gestion/page.tsx
+++ client/app/home/gestion/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Gestion() {
   // Array de objetos que representan los datos de cada viaje
-  const viajes = [
+  const viajesHardcodeados = [
     { id: 1, fecha: "12/01/23", ruta: "12:00-Medellin-La Ceja" },
     { id: 2, fecha: "13/01/23", ruta: "13:00-La Ceja-Medellin" },
     { id: 3, fecha: "14/01/23", ruta: "14:00-Medellin-La Ceja" },
@@ -12,25 +12,25 @@ export default function Gestion() {
   ];
 
   return (
-    <div className="container mx-auto flex h-3/5 w-3/5 flex-col gap-10 border border-black lg:p-10">
-      <h3 className="rounded-3xl bg-sky-600 p-4 text-center font-bold tracking-widest text-white shadow-xl shadow-black/40">
+    <section className="container mx-auto flex h-3/5 w-full lg:w-3/5 flex-col gap-10 border border-black lg:p-10">
+      <h3 className="rounded-3xl bg-sky-600 p-4 text-center font-bold tracking-widest text-white">
         Historial de viajes
       </h3>
-      <ul className="flex flex-col gap-3">
+      <ul className="flex flex-col gap-3 ">
         {/* Usamos map para generar un li por cada objeto del array */}
-        {viajes.map((viaje) => (
-          <li key={viaje.id} className="flex items-center gap-4 px-4">
-            <div className="rounded-full border-2 border-blue p-2 text-center">
+        {viajesHardcodeados.map((viaje) => (
+          <li key={viaje.id} className="flex items-center gap-4 px-4 border border-black">
+            <article className=" text-center">
               <small>{viaje.fecha}</small>
               <small>{viaje.ruta}</small>
-            </div>
+            </article>
             <div className="w-1/4">
               {/* Usamos Link de Next para crear un enlace */}
-              <Link href={`/home/gestion/${viaje.id}`}>Gestionar</Link>
+              <Link href={`/home/gestion/${viaje.id}`} className="rounded-full border-2 border-blue p-2">Gestionar</Link>
             </div>
           </li>
         ))}
       </ul>
-    </div>
+    </section>
   );
 }
