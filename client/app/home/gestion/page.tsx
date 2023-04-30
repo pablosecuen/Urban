5715cb2faadfca69e72a440a0e852aa5fe129738
httpfido <fidel.romano.2016@hotmail.com>
2023-04-29 22:22:25 -0300

    scroll

--- client/app/home/gestion/page.tsx
+++ client/app/home/gestion/page.tsx
@@ -29,24 +29,26 @@ const getData = async (): Promise<Data> => {
 export default async function Gestion() {
   const data = await getData();
   console.log(data);
+const hardcodeo = ["stefa", "fido", "puto", "el", "que", "habla"]
 
   return (
     <section className="container flex h-auto lg:h-96 w-full flex-col gap-2  bg-slate-100 lg:container lg:mx-auto lg:bg-none lg:p-10">
       <h3 className="rounded-3xl p-4  text-left font-bold tracking-widest">Historial de viajes</h3>
-      <ul className="flex flex-col gap-3 ">
+      <ul className= {` h-full flex flex-col gap-3 ${hardcodeo.length > 1 && "overflow-y-scroll overflow-scroll overflow-x-hidden"}`}>
         {/* Usamos map para generar un li por cada objeto del array */}
-        {data.travels.map((viaje: any) => (
+        {hardcodeo.map((viaje: any) => (
           <Link href={`/home/gestion/${viaje.id}`}>
             <li
-              key={viaje.id}
+              // key={viaje.id}
               className="group flex items-center gap-4 rounded-full border bg-white p-2 px-4 transition-all duration-200 hover:border-blue"
             >
-              <article className="flex flex-col text-left ">
+              {viaje}
+              {/* <article className="flex flex-col text-left ">
                 <small className="font-bold ">{viaje.destination}</small>
                 <small>
                   {viaje.date.slice(0, 10)} {viaje.date.slice(11, 16)}
                 </small>
-              </article>
+              </article> */}
               {/* Usamos Link de Next para crear un enlace */}
               <FiChevronRight
                 size={25}
