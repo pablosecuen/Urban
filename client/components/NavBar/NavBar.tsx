fb775b39f2b3b5ff49e5ffeec00375546607c9f0
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-05-02 17:48:04 -0300

    modularizado de interface

--- client/components/NavBar/NavBar.tsx
+++ client/components/NavBar/NavBar.tsx
@@ -1,13 +1,13 @@
 "use client";
-import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
-import logo from "../../assets/imagenes/UrbanIso.png";
 import { useMediaQuery } from "react-responsive";
+import { User, userData } from "../../app/types/User";
 import { links, linksMobile } from "../../assets/data";
 import { HiMenuAlt1 } from "react-icons/hi";
-import { User } from "../../app/types/User";
+import logo from "../../assets/imagenes/UrbanIso.png";
 
 export default function NavBar() {
   const isMobile = useMediaQuery({ query: "(max-width: 700px)" });
@@ -32,11 +32,6 @@ export default function NavBar() {
     localStorage.removeItem("user");
     setUserData(null);
   };
-
-  interface userData {
-    name: string;
-    img: string;
-  }
 
   return (
     <header className="bg-verde">
