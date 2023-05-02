9b131d1955918bd4b79f24419c91f7179e23f912
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-05-02 17:23:01 -0300

    controlador de obtener conductores

--- api/src/controllers/outputs/busDriver.ts
+++ api/src/controllers/outputs/busDriver.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import { db } from "../../connection/connection";
 
+/**
+ * Controlador para buscar un conductor por id
+ */
 export const searhBusDriver = async (req: Request, res: Response): Promise<void> => {
   try {
     const id: string = req.params.id;
@@ -17,4 +20,35 @@ export const searhBusDriver = async (req: Request, res: Response): Promise<void>
   }
 };
 
-export const getBusDriver = async (req: Request, res: Response) => {};
+/**
+ * controlador para buscar todos los conductores
+ */
+export const getBusDriver = async (req: Request, res: Response) => {
+  try {
+    const allProperties = Object.keys(req.query);
+
+    let query: any = db.collection("busDriver");
+
+    allProperties.forEach((property) => {
+      if (property === "page" || property === "pageSize") {
+        return;
+      }
+      query = query.where(property, "==", req.query[property]);
+    });
+
+    const busDriversSnapshot = await query.get();
+    const page = Number(req.query.page) || 1;
+    const pageSize = Number(req.query.pageSize) || 6;
+    const startIndex = (page - 1) * pageSize;
+    const endIndex = page * pageSize;
+    const totalPages = Math.ceil(busDriversSnapshot.size / pageSize);
+
+    const busDriversData = busDriversSnapshot.docs
+      .slice(startIndex, endIndex)
+      .map((doc) => ({ id: doc.id, ...doc.data() }));
+    res.status(201).json({ busDrivers: busDriversData, totalPages });
+  } catch (error) {
+    console.error("Error al obtener los conductores", error);
+    res.status(500).json({ message: "Error al obtener los conductores" });
+  }
+};
