e5aedf19051fe37c6a48c245ce82c24ef9012805
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-05-02 17:57:30 -0300

    ADD rutas passage outputs

--- api/src/routers/outputs/passage.ts
+++ api/src/routers/outputs/passage.ts
@@ -5,9 +5,6 @@ const router = Router();
 
 //Ruta para crear bondis
 
-router.post("/", newBus);
-router.put("/:id", updateBus);
-router.patch("/:id", enableBus);
-router.delete("/:id", deleteBus);
+
 
 export default router;
