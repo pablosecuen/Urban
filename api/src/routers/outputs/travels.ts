62c4f1cf349cdcaa4c8bde848d171fad345cf787
FedericoA15 <federicoalmeida15@gmail.com>
2023-05-02 13:44:28 -0300

    ADD router viaje por id

--- api/src/routers/outputs/travels.ts
+++ api/src/routers/outputs/travels.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { travelByUser,travelByChauffeur } from "../../controllers/outputs/travels";
+import { travelByUser,travelByChauffeur, searchTravel } from "../../controllers/outputs/travels";
 
 const router = Router();
 
 // Ruta para crear un nuevo usuario
 
+router.get("/:id", searchTravel);
 router.get("/user/:userId", travelByUser);
 router.get("/chauffeur/:chauffeurId", travelByChauffeur);
 
