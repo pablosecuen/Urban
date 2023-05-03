1d067c560993d08b587391a201fd8d9dc2d326f3
Yuarinaim <benyamin0714@gmail.com>
2023-05-03 13:48:44 -0300

    Responsive lg login

--- client/components/Login/Login.tsx
+++ client/components/Login/Login.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
-import { CredentialResponse } from "@react-oauth/google";
+// import { CredentialResponse } from "@react-oauth/google";
 
 interface UserToRegister {
   name: string;
@@ -61,7 +61,6 @@ const Login = () => {
     }
   };
 
-
   const createUser = async (userData: UserToRegister) => {
     try {
       const response = await axios.post("http://localhost:3000/user", userData);
@@ -118,7 +117,6 @@ const Login = () => {
           className="my-8 flex h-[450px] w-4/5 flex-col items-center justify-between rounded-3xl border-2 px-4 pb-4 align-middle shadow-lg shadow-black/40 lg:h-[600px] lg:w-1/2"
         >
           <div className="flex h-1/2 flex-col items-center justify-center align-middle">
-            {" "}
             <label className="text-center">
               Name:
               <input type="text" value={name} onChange={handleNameChange} />
@@ -137,14 +135,19 @@ const Login = () => {
             </label>
           </div>
           <div className="flex h-1/2 flex-col items-center justify-between align-middle">
-            {" "}
             <button className="mx-auto w-1/2 py-2 font-semibold" onClick={handleRegister}>
               Register
             </button>
             {/* <button onClick={handleRegisterClick}>Apple Id</button>
             <button onClick={handleRegisterClick}>Facebook</button> */}
             <div className="flex w-auto flex-col items-center justify-center gap-3">
-            <a className=" flex w-56 items-center justify-between gap-3 whitespace-nowrap rounded-sm border border-[#888] bg-white py-2 font-semibold text-[#757575] shadow-md shadow-black/30" href="http://localhost:3000/login/auth/google" rel="noopener noreferrer">Login with google</a>
+              <a
+                className=" flex w-56 items-center justify-between gap-3 whitespace-nowrap rounded-sm border border-[#888] bg-white py-2 font-semibold text-[#757575] shadow-md shadow-black/30"
+                href="http://localhost:3000/login/auth/google"
+                rel="noopener noreferrer"
+              >
+                Login with google
+              </a>
               <button className="bg-blue-700 hover:bg-blue-800 flex items-center whitespace-nowrap rounded px-4 py-2 font-semibold text-white shadow-md  shadow-black/30">
                 <svg className="mr-2 h-6 w-6 fill-current" viewBox="0 0 24 24">
                   <path d="M20.02 0H3.98A3.98 3.98 0 0 0 0 3.98v16.04A3.98 3.98 0 0 0 3.98 24h8.22v-9.29H8.65v-3.62h3.55V9.02c0-3.52 2.14-5.44 5.28-5.44 1.54 0 2.87.12 3.26.18v3.66l-2.23.001c-1.75 0-2.09.83-2.09 2.05v2.69h4.19l-.55 3.62h-3.64V24h7.12A3.98 3.98 0 0 0 24 20.02V3.98C24 1.78 22.2 0 20.02 0z" />
@@ -163,41 +166,51 @@ const Login = () => {
       ) : (
         <form
           onSubmit={handleRegister}
-          className="my-8 flex h-[450px] w-4/5 flex-col items-center justify-between rounded-3xl border-2 px-4 pb-4 align-middle shadow-lg shadow-black/40 lg:h-[600px] lg:w-1/2"
+          className="my-8 flex h-[450px] w-4/5 flex-col items-center justify-between rounded-3xl border-2 px-4 pb-4 align-middle shadow-lg shadow-black/40 lg:h-[600px]"
         >
-          <div className="flex h-1/2 flex-col items-center justify-center align-middle">
-            {" "}
+          <div className="flex h-1/2 flex-col items-center justify-center gap-2 align-middle">
             <label className="text-center">
-              Email:
-              <input type="email" value={email} onChange={handleEmailChange} />
+              Email: <br />
+              <input className="lg:w-2/3" type="email" value={email} onChange={handleEmailChange} />
             </label>
             <label className="text-center">
-              Password:
-              <input type="password" value={password} onChange={handlePasswordChange} />
+              Password: <br />
+              <input
+                className="lg:w-2/3"
+                type="password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
             </label>
-          </div>
-          <div className="flex h-1/2 flex-col items-center justify-between align-middle ">
-            <button className="mx-auto w-1/2 py-2 font-semibold" onClick={handleLogin}>
+            <button className="mx-auto mt-6 w-1/2 py-2 font-semibold" onClick={handleLogin}>
               Login
             </button>
+          </div>
+          <span className="text-center text-gray-500">OR</span>
+          <div className="mt-6 flex h-1/2 flex-col items-center justify-between align-middle ">
             {/* <button onClick={handleRegisterClick}>Apple Id</button> */}
             <div className="flex w-auto flex-col items-center justify-center gap-3">
-            <a className=" flex w-56 items-center justify-between gap-3 whitespace-nowrap rounded-sm border border-[#888] bg-white py-2 font-semibold text-[#757575] shadow-md shadow-black/30" href="http://localhost:3000/login/auth/google" rel="noopener noreferrer">Login with google</a>
+              <a
+                className=" flex w-56 items-center justify-between gap-3 whitespace-nowrap rounded-sm border border-[#888] bg-white py-2 font-semibold text-[#757575] shadow-md shadow-black/30"
+                href="http://localhost:3000/login/auth/google"
+                rel="noopener noreferrer"
+              >
+                Login with google
+              </a>
               <button className="bg-blue-700 hover:bg-blue-800 flex items-center whitespace-nowrap rounded px-4 py-2 font-semibold text-white shadow-md  shadow-black/30">
                 <svg className="mr-2 h-6 w-6 fill-current" viewBox="0 0 24 24">
                   <path d="M20.02 0H3.98A3.98 3.98 0 0 0 0 3.98v16.04A3.98 3.98 0 0 0 3.98 24h8.22v-9.29H8.65v-3.62h3.55V9.02c0-3.52 2.14-5.44 5.28-5.44 1.54 0 2.87.12 3.26.18v3.66l-2.23.001c-1.75 0-2.09.83-2.09 2.05v2.69h4.19l-.55 3.62h-3.64V24h7.12A3.98 3.98 0 0 0 24 20.02V3.98C24 1.78 22.2 0 20.02 0z" />
                 </svg>
                 <span>Login with Facebook</span>
               </button>
+              <button
+                className="py-2 text-xs font-semibold lg:mx-auto lg:mt-6"
+                onClick={handleRegisterClick}
+              >
+                <p>No tienes cuenta? Registrate aqui</p>
+              </button>
             </div>
             {/* <button onClick={handleRegisterClick}>Facebook</button> */}
-
-            <button
-              className="w-1/2 py-2 text-xs font-semibold lg:mx-auto lg:mt-6"
-              onClick={handleRegisterClick}
-            >
-              <p>no tienes cuenta? registrate aqui</p>
-            </button>
           </div>
         </form>
       )}
