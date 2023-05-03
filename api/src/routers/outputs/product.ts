8ea656c56fec15e544c04d0685d4370095679199
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-05-03 00:58:41 -0300

    ADD ruta de producto byId

--- api/src/routers/outputs/product.ts
+++ api/src/routers/outputs/product.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getAllProducts, getAllProductsByType, getAllProductsByStore } from "../../controllers/outputs/product";
+import { getAllProducts, getAllProductsByType, getAllProductsByStore, getProductId } from "../../controllers/outputs/product";
 
 const router = Router();
 
 router.get("/", getAllProducts);
+router.get("/:id", getProductId);
 router.get("/:productType", getAllProductsByType);
 router.get("/store/:store", getAllProductsByStore);
 
