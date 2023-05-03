72aef36c87364cf78dbfba64f750e34d3b134e0a
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-05-03 18:34:35 -0300

    Update passage.ts

--- api/src/schema/passage.ts
+++ api/src/schema/passage.ts
@@ -1,19 +1,18 @@
 export interface PassageToRegister {
-  origin: string,
-  stock: number,
-  destination: string,
-  description: string,
-  departureDate: string,
-  arrivalDate: string,
-  duration: string,
-  price: number,
+  origin: string;
+  stock: number;
+  destination: string;
+  description: string;
+  departureDate: string;
+  arrivalDate: string;
+  duration: string;
+  price: number;
+  numberSeats: number;
 }
 
 export interface Passage extends PassageToRegister {
-  deleted: boolean,
-  status: boolean,
+  deleted: boolean;
+  status: boolean;
 }
 
-export interface PassageToUpdate extends Partial<PassageToRegister> {
-
-}
+export interface PassageToUpdate extends Partial<PassageToRegister> {}
