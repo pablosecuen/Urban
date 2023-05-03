eb2df369297ec15fd46cf21b1993915c7dea3101
AgustinChavero <agustin1996orellano@gmail.com>
2023-05-03 13:26:53 -0300

    Actualizacion de validaciones Travels

--- api/src/utils/validations/travels.ts
+++ api/src/utils/validations/travels.ts
@@ -1,11 +1,27 @@
 import { Request, Response, NextFunction } from "express";
 import { Travel, TravelToUpdate } from "../../schema/travels";
+import {
+  isChauffeurIdValid,
+  isDestinationValid,
+  isOriginValid,
+  isPriceValid,
+  isUserIdValid,
+} from "./validators";
 
 export const newTravelValidated = (req: Request, res: Response, next: NextFunction): void => {
   try {
     const data: Travel = req.body;
     if (!data.userId || !data.chauffeurId || !data.price || !data.destination || !data.origin)
       throw Error("Datos incompletos");
+    if (
+      !isUserIdValid(data.userId) ||
+      !isChauffeurIdValid(data.chauffeurId) ||
+      !isDestinationValid(data.destination) ||
+      !isOriginValid(data.origin) ||
+      !isPriceValid(data.price)
+    ) {
+      throw new Error("Datos no validos");
+    }
     next();
   } catch (error) {
     res.status(400).json({ message: error.message });
