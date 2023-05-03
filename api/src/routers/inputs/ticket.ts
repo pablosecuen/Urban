0800b83750ca893ff26b9489e2e84538a2771c4c
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-05-03 17:19:20 -0300

    ruta de cancelTicket

--- api/src/routers/inputs/ticket.ts
+++ api/src/routers/inputs/ticket.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { aceptTicket, newTicket } from "../../controllers/inputs/ticket";
+import { aceptTicket, cancelTicket, newTicket } from "../../controllers/inputs/ticket";
 
 const router = Router();
 
 router.post("/", newTicket);
 
 router.patch("/acept/:id", aceptTicket);
+
+router.patch("/cancel/:id", cancelTicket);
 
 export default router;
