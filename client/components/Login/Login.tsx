8cc7c10311f88defc414aa56a5e2141ed29f8b2b
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-05-04 18:21:04 -0300

    agregando transition opacity al componente

--- client/components/Login/Login.tsx
+++ client/components/Login/Login.tsx
@@ -6,14 +6,23 @@ import LoginForm from "../LoginForm/LoginForm";
 
 const Login = () => {
   const [isRegister, setIsRegister] = useState(false);
-
+  console.log(isRegister);
   return (
-    <div className="mt-0 flex h-full w-full items-center justify-center lg:h-screen">
-      {isRegister ? (
+    <div className="relative mt-0 flex h-full w-full items-center justify-center lg:h-screen">
+      <div
+        className={` transition_all absolute top-0 grid w-96 place-content-center opacity-0 lg:top-52 ${
+          isRegister && "z-10 opacity-100"
+        }`}
+      >
         <RegisterForm isRegister={isRegister} setIsRegister={setIsRegister} />
-      ) : (
+      </div>
+      <div
+        className={`transition_all absolute top-0 grid place-content-center  opacity-0 lg:top-52 ${
+          !isRegister && "z-10 opacity-100"
+        }`}
+      >
         <LoginForm isRegister={isRegister} setIsRegister={setIsRegister} />
-      )}
+      </div>
     </div>
   );
 };
