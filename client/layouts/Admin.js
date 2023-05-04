3304a1f02d12a9a2611b2dc9e829b288e76b4833
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-04 16:04:46 -0300

    dashboard dinamico para estadisticas

--- client/layouts/Admin.js
+++ client/layouts/Admin.js
@@ -4,18 +4,18 @@ import React from "react";
 
 import AdminNavbar from "components/Navbars/AdminNavbar.js";
 import Sidebar from "components/Sidebar/Sidebar.js";
-import HeaderStats from "components/Headers/HeaderStats.js";
+import HeaderStats from "components/Headers/HeaderStats.tsx";
 import FooterAdmin from "components/Footers/FooterAdmin.js";
 
 export default function Admin({ children }) {
   return (
     <>
       <Sidebar />
-      <div className="relative md:ml-64 bg-blueGray-100">
+      <div className="relative bg-blueGray-100 md:ml-64">
         <AdminNavbar />
         {/* Header */}
         <HeaderStats />
-        <div className="px-4 md:px-10 mx-auto w-full -m-24">
+        <div className="-m-24 mx-auto w-full px-4 md:px-10">
           {children}
           <FooterAdmin />
         </div>
