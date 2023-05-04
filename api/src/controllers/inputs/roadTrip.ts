e74ab796e665ab18b40e4fd35905fe8fddc374e2
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-05-04 09:31:51 -0300

    Controladore create ruta

--- api/src/controllers/inputs/roadTrip.ts
+++ api/src/controllers/inputs/roadTrip.ts
@@ -1,11 +1,22 @@
 import { Request, Response } from "express";
 import { db } from "../../connection/connection";
+import { RoadTrip, RoadTripToRegister } from "../../schema/roadTrip";
 
+/**
+ * Controlador para crear la ruta
+ * * @body datos para crear la ruta
+ */
 export const newRoadTrip = async (req: Request, res: Response): Promise<void> => {
   try {
-    res.status(201).json();
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error al crear un nuevo producto");
+    const data: RoadTripToRegister = req.body;
+    const dataFormated: RoadTrip = {
+      ...data,
+      deleted: false,
+    };
+    const docRef = await db.collection("roadTrip").add(dataFormated);
+    res.status(201).json({ id: docRef.id });
+  } catch (innerError) {
+    console.error("Error al crear la ruta", innerError);
+    res.status(500).send("Error al crear una nueva ruta");
   }
 };
