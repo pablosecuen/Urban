007314da70a7e265f237ed47e9f704ff5f253005
httpfido <fidel.romano.2016@hotmail.com>
2023-05-04 17:04:51 -0300

    redux: creacion de store passages

---
+++ client/Redux/passage/passageActions.tsx
@@ -0,0 +1,26 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// Define async thunk actions to fetch user data
+export const getAllUsers = async () => {
+  const response = await axios.get("http://localhost:3000/user?page=1&pageSize=1000");
+  return response;
+};
+
+export const getUserById = createAsyncThunk("users/getUserById", async (id: string) => {
+  const response = await fetch(`/api/user/${id}`);
+  const data = await response.json();
+  return data;
+});
+
+export const getUserByName = createAsyncThunk("users/getUserName", async (name: string) => {
+  const response = await fetch(`/api/users?name=${name}`);
+  const data = await response.json();
+  return data;
+});
+
+export const getUserByPatent = createAsyncThunk("users/getUserByPatent", async (patent: string) => {
+  const response = await fetch(`/api/users?patent=${patent}`);
+  const data = await response.json();
+  return data;
+});
---
+++ client/Redux/passage/passageSlice.tsx
@@ -0,0 +1,40 @@
+import {
+  createSlice,
+  createAsyncThunk,
+  AsyncThunkPayloadCreator,
+  AsyncThunk,
+  ThunkDispatch,
+} from "@reduxjs/toolkit";
+import { getAllUsers, getUserById, getUserByName, getUserByPatent } from "./passageActions";
+import { User } from "../../app/types/User";
+import { AxiosResponse } from "axios";
+interface UserState {
+  allUsers: User[]; // change any[] to your specific type
+}
+
+const initialState: UserState = {
+  allUsers: [], // provide an empty array as the initial state value for allUsers
+};
+
+type ResponseType = AxiosResponse<any, any>;
+
+export const fetchAllUsers: AsyncThunk<User[], void, {}> = createAsyncThunk(
+  "users/fetchAllUsers",
+  async () => {
+    const response: ResponseType = await getAllUsers();
+    return response.data.users; // return the users array
+  }
+);
+
+const userSlice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchAllUsers.fulfilled, (state, action) => {
+      state.allUsers = action.payload;
+    });
+  },
+});
+
+export default userSlice.reducer;
