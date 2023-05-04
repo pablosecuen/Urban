97f45741865d86e396319b97e6a7e438822e59f2
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-05-04 17:37:46 -0300

    cambio de estilado ROTUNDO

--- client/components/LoginForm/LoginForm.tsx
+++ client/components/LoginForm/LoginForm.tsx
@@ -64,87 +64,102 @@ const LoginForm = ({ isRegister, setIsRegister }: { isRegister: boolean; setIsRe
   return (
     <form
       onSubmit={handleLogin}
-      className="my-8 flex h-[450px] w-4/5 flex-col items-center justify-between rounded-3xl border-2 px-4 pb-4 align-middle shadow-lg shadow-black/40 lg:ml-20 lg:h-4/5 lg:w-4/5"
+      className="relative mx-2 h-[500px] max-w-md rounded-lg bg-slate-900/90 px-4 py-8 text-sm text-white/90 shadow-md shadow-black/40"
     >
-      <div className="flex h-1/2 flex-col items-center justify-center gap-6 ">
-        <label
-          className={`transition_all relative text-center ${
-            errores.messageEmail ? "text-red-500" : ""
-          }`}
-        >
-          Email:
+      {/* div que contiene Email y Password */}
+      <div className="flex h-[250px] flex-col gap-5 px-5">
+        {/* email */}
+        <div className="relative">
+          <label
+            className={`transition_all relative px-1 text-center ${
+              errores.messageEmail ? "text-red-500" : ""
+            }`}
+          >
+            Email:
+          </label>
           <input
             type="email"
             name="email"
             value={userData.email}
             onChange={handleInputChange}
-            className={`transition_all mt-1 ${
+            className={`transition_all mt-1 px-2 text-black ${
               errores.messageEmail && "focus-visible:outline-red-500 "
             }`}
           />
           <small
-            className={`transition_all absolute ${
+            className={`transition_all absolute text-right ${
               errores.messageEmail ? "opacity-100" : "opacity-0"
             } -bottom-6 left-0 text-left font-medium text-red-500`}
           >
             {errores.messageEmail}
           </small>
-        </label>
-        <label
-          className={`relative text-center ${
-            errores.messagePassword ? "text-red-500" : ""
-          } transition_all`}
-        >
-          Password:
+        </div>
+        {/* password  */}
+        <div className="relative">
+          <label
+            className={`relative px-1 text-center ${
+              errores.messagePassword ? "text-red-500" : ""
+            } transition_all`}
+          >
+            Password:
+          </label>
           <input
             type="password"
             name="password"
             value={userData.password}
             onChange={handleInputChange}
-            className={`transition_all mt-1  ${
+            className={`transition_all px-1  text-black ${
               errores.messagePassword && " focus-visible:outline-red-500"
             }`}
           />
           <small
-            className={`transition_all ${
+            className={`transition_all text-right ${
               errores.messagePassword ? "opacity-100" : "opacity-0"
             } absolute -bottom-6 left-0 text-left font-medium text-red-500`}
           >
             {errores.messagePassword}
           </small>
-        </label>
+        </div>
       </div>
-      <div className="flex h-1/2 flex-col items-center justify-between align-middle ">
-        <button className="mx-auto mt-2 w-1/2 py-2 font-semibold" onClick={handleLogin}>
+      {/* div que contiene boton de login  */}
+      <div className="py-4 text-center">
+        <button className="mx-auto w-1/2" onClick={handleLogin}>
           Login
         </button>
       </div>
-      <div className="flex h-1/2 flex-col items-center justify-between align-middle ">
-        {/* <button onClick={handleRegisterClick}>Apple Id</button> */}
-        <div className="flex w-1/2 flex-col items-center justify-center gap-3">
-          <Link
-            className=" flex  items-center justify-center gap-3 whitespace-nowrap rounded-sm border border-[#888] bg-white py-2 font-semibold text-[#757575] shadow-md shadow-black/30"
-            href="http://localhost:3000/login/auth/google"
-            rel="noopener noreferrer"
-          >
-            <Google width="24" height="24" />
-            Login with google
-          </Link>
-          <button className="bg-blue-700 hover:bg-blue-800 flex items-center whitespace-nowrap rounded px-4 py-2 font-semibold text-white shadow-md  shadow-black/30">
-            <svg className="mr-2 h-6 w-6 fill-current" viewBox="0 0 24 24">
-              <path d="M20.02 0H3.98A3.98 3.98 0 0 0 0 3.98v16.04A3.98 3.98 0 0 0 3.98 24h8.22v-9.29H8.65v-3.62h3.55V9.02c0-3.52 2.14-5.44 5.28-5.44 1.54 0 2.87.12 3.26.18v3.66l-2.23.001c-1.75 0-2.09.83-2.09 2.05v2.69h4.19l-.55 3.62h-3.64V24h7.12A3.98 3.98 0 0 0 24 20.02V3.98C24 1.78 22.2 0 20.02 0z" />
-            </svg>
-            <span>Login with Facebook</span>
-          </button>
-          <button
-            className="py-3 text-xs font-semibold lg:mx-auto lg:mt-6"
-            onClick={handleRegisterClick}
-          >
-            <p>No tienes cuenta? Registrate aqui</p>
-          </button>
-        </div>
-        {/* <button onClick={handleRegisterClick}>Facebook</button> */}
+
+      {/* <button onClick={handleRegisterClick}>Apple Id</button> */}
+
+      <div className="mx-auto flex w-4/5 flex-col gap-2">
+        <Link
+          className="mx-auto flex w-2/3 items-center justify-center gap-2 rounded-md border border-[#888] bg-white py-2  font-semibold text-[#757575] shadow-md shadow-black/30"
+          href="http://localhost:3000/login/auth/google"
+          rel="noopener noreferrer"
+        >
+          <Google width="16" height="16" />
+          Login with google
+        </Link>
+        <Link
+          className="mx-auto flex w-2/3 items-center justify-center gap-2 rounded-md border border-[#888] bg-white py-2  font-semibold text-blue shadow-md shadow-black/30"
+          href="http://localhost:3000/login/auth/google"
+          rel="noopener noreferrer"
+        >
+          <svg className="mr-2 h-6 w-6 fill-current" viewBox="0 0 24 24">
+            <path d="M20.02 0H3.98A3.98 3.98 0 0 0 0 3.98v16.04A3.98 3.98 0 0 0 3.98 24h8.22v-9.29H8.65v-3.62h3.55V9.02c0-3.52 2.14-5.44 5.28-5.44 1.54 0 2.87.12 3.26.18v3.66l-2.23.001c-1.75 0-2.09.83-2.09 2.05v2.69h4.19l-.55 3.62h-3.64V24h7.12A3.98 3.98 0 0 0 24 20.02V3.98C24 1.78 22.2 0 20.02 0z" />
+          </svg>
+          Login with Facebook
+        </Link>
       </div>
+      <small className="absolute bottom-5 right-5 block pt-2 text-right">
+        No tienes cuenta?{" "}
+        <span
+          onClick={handleRegisterClick}
+          className="font-semibold text-blue hover:cursor-pointer hover:underline"
+        >
+          Registrate aqui
+        </span>
+      </small>
+      {/* <button onClick={handleRegisterClick}>Facebook</button> */}
     </form>
   );
 };
