0ee08b192fb99e4007b75445c8703760ce00a05e
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-04 16:04:52 -0300

    dashboard dinamico para estadisticas

--- client/Redux/travel/travelActions.tsx
+++ client/Redux/travel/travelActions.tsx
@@ -1,27 +1,14 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
 // Define async thunk actions to fetch user data
 export const getAllTravels = async () => {
-  const data = await axios.get("http://localhost:3000/passage?pageSize=10000");
-  const passages = data.data;
-  return passages;
+  const response = await axios.get("http://localhost:3000/travels?page=1&pageSize=10000");
+  return response;
 };
 
-// export const getUserById = createAsyncThunk("users/getUserById", async (id: string) => {
-//   const response = await fetch(`/api/user/${id}`);
-//   const data = await response.json();
-//   return data;
-// });
-
-// export const getUserByName = createAsyncThunk("users/getUserName", async (name: string) => {
-//   const response = await fetch(`/api/users?name=${name}`);
-//   const data = await response.json();
-//   return data;
-// });
-
-// export const getUserByPatent = createAsyncThunk("users/getUserByPatent", async (patent: string) => {
-//   const response = await fetch(`/api/users?patent=${patent}`);
-//   const data = await response.json();
-//   return data;
-// });
+export const getTravelsById = createAsyncThunk("travels/getTravelsById", async (id: string) => {
+  const response = await fetch(`/api/travels/${id}`);
+  const data = await response.json();
+  return data;
+});
