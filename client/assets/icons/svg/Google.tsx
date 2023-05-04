883f616a9aa7dbaf1f6675066ee08d1da72416a6
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-05-04 17:36:36 -0300

    add classname

--- client/assets/icons/svg/Google.tsx
+++ client/assets/icons/svg/Google.tsx
@@ -14,6 +14,7 @@ export const Google = ({
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       fill="#000000"
+      className="w-6 "
     >
       <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
