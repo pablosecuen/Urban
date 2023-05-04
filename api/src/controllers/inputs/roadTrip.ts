e3b74dadb486f3249db450bdfdea73dacce2d0f2
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-05-04 09:32:22 -0300

    Controlador update ruta

--- api/src/controllers/inputs/roadTrip.ts
+++ api/src/controllers/inputs/roadTrip.ts
@@ -20,3 +20,24 @@ export const newRoadTrip = async (req: Request, res: Response): Promise<void> =>
     res.status(500).send("Error al crear una nueva ruta");
   }
 };
+
+/**
+ * Controlador para actualizar la ruta
+ * @param id recibe el Id de la ruta,
+ * @body  trae los datos a actualizar
+ */
+export const roadTripUpdate = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const id: string = req.params.id;
+    const data: RoadTrip = req.body;
+    const docRef = await db.collection("roadTrip").doc(id).get();
+    if (!docRef) {
+      throw new Error("La ruta no existe");
+    }
+    await db.collection("roadTrip").doc(id).update({ data });
+    res.status(201).json({ menssage: "Ruta actualizada correctamente" });
+  } catch (error) {
+    console.error("Error al actualizar la ruta", error);
+    res.status(400).json({ messege: error.message });
+  }
+};
