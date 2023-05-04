4ee16b1c6885d248ccb1f40870163aeb0d84156d
httpfido <fidel.romano.2016@hotmail.com>
2023-05-04 20:46:26 -0300

    /reserva map

--- client/app/home/reserva/viajes/page.tsx
+++ client/app/home/reserva/viajes/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Link from "next/link";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -8,29 +8,32 @@ import { AnyAction } from "@reduxjs/toolkit";
 import { fetchAllPassages } from "@component/Redux/passage/passageSlice";
 
 export default function Viajes() {
-
   const dispatch: ThunkDispatch<RootState, undefined, AnyAction> = useDispatch();
   const allPassages = useSelector((state: RootState) => state.passage.allPassages);
 
   useEffect(() => {
     dispatch(fetchAllPassages());
-  }, []);  
+  }, []);
+
+  console.log(allPassages);
 
   return (
-
     <div className="mx-auto h-full rounded-3xl p-10 shadow-2xl shadow-black/40 lg:ml-12 ">
       <div className="flex flex-col gap-4 ">
         <h1 className="text-center text-xl text-blue">
           Estas son las mejores opciones encontradas
         </h1>
 
-    {allPassages.map((passage) => (
-      <div key={passage.id}>
-        {passage.duration}
-        </div>
-    ))}
-
-
+        {allPassages.map((passage) => (
+          <section key={passage.id} className="flex w-full flex-col border border-black">
+            <h1 className="border capitalize">
+              {passage.origin} - {passage.destination}
+            </h1>
+            <small>
+              {passage.departureDate} - {passage.arrivalDate}
+            </small>
+          </section>
+        ))}
       </div>
       <div className="flex gap-2 pt-10">
         <button>Ver mas opciones</button>
