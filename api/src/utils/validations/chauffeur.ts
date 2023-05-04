83852988818faaed9221169c88d35ae5eda365b2
AgustinChavero <agustin1996orellano@gmail.com>
2023-05-04 14:06:19 -0300

    Chauffeur middleware validators aplicado

--- api/src/utils/validations/chauffeur.ts
+++ api/src/utils/validations/chauffeur.ts
@@ -2,12 +2,17 @@ import { Request, Response, NextFunction } from "express";
 import { ChauffeurToRegister, ChauffeurToUpdate } from "../../schema/chauffeur";
 import {
   arePaymentsValid,
+  isAddressValid,
+  isBirthdayValid,
   isCcValid,
   isEmailValid,
   isFirstNameValid,
+  isImgValid,
   isLastNameValid,
   isLicenseValid,
+  isNationalityValid,
   isPasswordValid,
+  isPhoneValid,
   isVehicleToChauffeurValid,
 } from "./validators";
 
@@ -25,23 +30,25 @@ export const newChauffeurValidated = (req: Request, res: Response, next: NextFun
       "birthday",
       "cc",
       "img",
-      "license"
+      "license",
     ];
     if (Object.keys(data).some((key) => !allowProperties.includes(key)))
       throw Error("Datos no permitidos");
 
-    // if (
-    //   !isFirstNameValid(data.firstName) ||
-    //   !isLastNameValid(data.lastName) ||
-    //   !isEmailValid(data.email) ||
-    //   !isPasswordValid(data.password) ||
-    //   // !isPhoneValid(data.phone) ||
-    //   !isCcValid(data.cc) ||
-    //   !isLicenseValid(data.license)
-    //   // ||
-    //   // !isAddressValid(data.address)
-    // )
-    // throw Error("Datos incompletos o no válidos");
+    if (
+      !isFirstNameValid(data.firstName) ||
+      !isLastNameValid(data.lastName) ||
+      !isAddressValid(data.address) ||
+      !isEmailValid(data.email) ||
+      !isPasswordValid(data.password) ||
+      !isPhoneValid(data.phone) ||
+      !isNationalityValid(data.nationality) ||
+      !isBirthdayValid(data.birthday) ||
+      !isCcValid(data.cc) ||
+      !isImgValid(data.img) ||
+      !isLicenseValid(data.license)
+    )
+      throw Error("Datos incompletos o no válidos");
     next();
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -56,8 +63,8 @@ export const updateChauffeurValidated = (req: Request, res: Response, next: Next
       throw Error("Datos no permitidos");
 
     if (
-      (data?.phone) ||
-      (data?.address) ||
+      data?.phone ||
+      data?.address ||
       (data?.vehicle && !isVehicleToChauffeurValid(data.vehicle)) ||
       (data?.payments && !arePaymentsValid(data.payments))
     )
