3a122e0a0e758fabf33bddfddfc6dc7d4eaf7164
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-05-04 09:48:49 -0300

    get for id rutas controller

--- api/src/controllers/outputs/roadTrip.ts
+++ api/src/controllers/outputs/roadTrip.ts
@@ -35,3 +35,24 @@ export const getRoadTrip = async (req: Request, res: Response): Promise<void> =>
     res.status(500).json({ message: "Error al obtener las rutas" });
   }
 };
+
+/**
+ * Controlador para buscar una ruta por id
+ * @param id  id del ruta
+ */
+export const searhBusDriver = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const id: string = req.params.id;
+    const doc = await db.collection("roadTrip").doc(id).get();
+    if (!doc.exists) {
+      res.status(404).send({ message: "Ruta no encontrada" });
+    } else {
+      const conductor = { id: doc.id, ...doc.data() };
+      res.json(conductor);
+    }
+    res.status(201).json();
+  } catch (error) {
+    console.error("Error al obtener la ruta", error);
+    res.status(500).json({ message: "Error al obtener la ruta" });
+  }
+};
