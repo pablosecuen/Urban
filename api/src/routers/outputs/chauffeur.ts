129e41b3979d7b228fa321cf159cf0e934211e66
FedericoA15 <federicoalmeida15@gmail.com>
2023-05-05 19:16:03 -0300

    ADD  get chofer by name

--- api/src/routers/outputs/chauffeur.ts
+++ api/src/routers/outputs/chauffeur.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { searchChauffeur, allChauffeur, searchChauffeurByPatent } from "../../controllers/outputs/chauffeur";
+import { searchChauffeur, allChauffeur, searchChauffeurByPatent, searchChauffeurName } from "../../controllers/outputs/chauffeur";
 
 const router = Router();
 
 // Ruta para obtener choferes
 router.get("/", allChauffeur);
 router.get("/:id", searchChauffeur);
+router.get("/:name", searchChauffeurName);
 router.get("/patent/:patent", searchChauffeurByPatent);
 
 export default router;
