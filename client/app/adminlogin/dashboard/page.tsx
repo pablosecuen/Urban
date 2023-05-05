57004e2be09ab2be6815e2f0302bed39045b0239
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-05 12:25:02 -0300

    Login para admins y enrutado nuevo

---
+++ client/app/adminlogin/dashboard/page.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+// components
+
+import CardLineChart from "../../../components/Cards/CardLineChart.js";
+import CardBarChart from "../../../components/Cards/CardBarChart.js";
+import CardPageVisits from "../../../components/Cards/CardPageVisits";
+import CardSocialTraffic from "../../../components/Cards/CardSocialTraffic.js";
+
+// layout for page
+
+import Admin from "../../../layouts/Admin.js";
+
+export default function Dashboard() {
+  return (
+    <>
+      <div className="flex flex-wrap">
+        <div className="mt-4 flex flex-wrap ">
+          <div className="mb-12 w-full px-4 xl:mb-0 xl:w-8/12">
+            <CardLineChart />
+          </div>
+          <div className="w-full px-4 xl:w-4/12">
+            <CardBarChart />
+          </div>
+        </div>
+      </div>
+      <div className="mt-4 flex flex-wrap">
+        <div className="mb-12 w-full px-4 xl:mb-0 xl:w-8/12">
+          <CardPageVisits />
+        </div>
+        <div className="w-full px-4 xl:w-4/12">
+          <CardSocialTraffic />
+        </div>
+      </div>
+    </>
+  );
+}
+
+Dashboard.layout = Admin;
