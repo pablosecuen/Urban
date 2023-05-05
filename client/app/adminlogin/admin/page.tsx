615f1f60a8444102c2364ff3a14d40977cfce02b
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-05 11:38:47 -0300

    armando verificacion de adminlogin

--- client/app/adminlogin/admin/page.tsx
+++ client/app/adminlogin/admin/page.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 // components
 
-import CardLineChart from "../../components/Cards/CardLineChart.js";
-import CardBarChart from "../../components/Cards/CardBarChart.js";
-import CardPageVisits from "../../components/Cards/CardPageVisits";
-import CardSocialTraffic from "../../components/Cards/CardSocialTraffic.js";
+import CardLineChart from "../../../components/Cards/CardLineChart.js";
+import CardBarChart from "../../../components/Cards/CardBarChart.js";
+import CardPageVisits from "../../../components/Cards/CardPageVisits";
+import CardSocialTraffic from "../../../components/Cards/CardSocialTraffic.js";
 
 // layout for page
 
-import Admin from "../../layouts/Admin.js";
+import Admin from "../../../layouts/Admin.js";
 
 export default function Dashboard() {
   return (
