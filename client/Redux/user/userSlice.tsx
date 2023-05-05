1d637a041ea0695cd894a1b665b3ec9283a2e93a
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-05-05 17:40:40 -0300

    user slice

--- client/Redux/user/userSlice.tsx
+++ client/Redux/user/userSlice.tsx
@@ -1,19 +1,30 @@
-
 import { createSlice, createAsyncThunk, AsyncThunk } from "@reduxjs/toolkit";
-import { getAllUsers, getUserById, getUserByName, getUserByPatent } from "./userActions";
+import {
+  getAllUsers,
+  getUserById,
+  getUsersByName,
+  getUsersByCc,
+  getUsersByEmail,
+} from "./userActions";
 
 import { User } from "../../app/types/User";
 import { AxiosResponse } from "axios";
 interface UserState {
-  allUsers: User[]; // change any[] to your specific type
+  allUsers: User[];
+  userById: User | null;
+  usersByName: User[];
+  usersByCc: User[];
+  usersByEmail: User[];
 }
 
 const initialState: UserState = {
-  allUsers: [], // provide an empty array as the initial state value for allUsers
+  allUsers: [],
+  userById: null,
+  usersByName: [],
+  usersByCc: [],
+  usersByEmail: [],
 };
-
 type ResponseType = AxiosResponse<any, any>;
-
 export const fetchAllUsers: AsyncThunk<User[], void, {}> = createAsyncThunk(
   "users/fetchAllUsers",
   async () => {
@@ -27,9 +38,22 @@ const userSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchAllUsers.fulfilled, (state, action) => {
-      state.allUsers = action.payload;
-    });
+    builder
+      .addCase(getAllUsers.fulfilled, (state, action) => {
+        state.allUsers = action.payload;
+      })
+      .addCase(getUserById.fulfilled, (state, action) => {
+        state.userById = action.payload;
+      })
+      .addCase(getUsersByName.fulfilled, (state, action) => {
+        state.usersByName = action.payload;
+      })
+      .addCase(getUsersByCc.fulfilled, (state, action) => {
+        state.usersByCc = action.payload;
+      })
+      .addCase(getUsersByEmail.fulfilled, (state, action) => {
+        state.usersByEmail = action.payload;
+      });
   },
 });
 
