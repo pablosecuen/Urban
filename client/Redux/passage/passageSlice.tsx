0964ff474a8e2f944dcf4f489efee56a33807949
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-05-05 18:15:23 -0300

    ADD fetch por id y query de passages

--- client/Redux/passage/passageSlice.tsx
+++ client/Redux/passage/passageSlice.tsx
@@ -3,16 +3,18 @@ import {
   createAsyncThunk,
   AsyncThunk,
 } from "@reduxjs/toolkit";
-import { getAllPassages } from "./passageActions";
+import { getAllPassages, getPassagesByQuery, getPassagesId } from "./passageActions";
 import { Passage } from "../../app/types/Passages";
 import { AxiosResponse } from "axios";
 
 interface PassageState {
-  allPassages: Passage[]; 
+  allPassages: Passage[];
+  passageById: Passage | null;
 }
 
 const initialState: PassageState = {
-  allPassages: [], 
+  allPassages: [],
+  passageById: null,
 };
 
 type ResponseType = AxiosResponse<any, any>;
@@ -21,18 +23,47 @@ export const fetchAllPassages: AsyncThunk<Passage[], void, {}> = createAsyncThun
   "passage/fetchAllPassages",
   async () => {
     const response: ResponseType = await getAllPassages();
-    return response.data.passages; 
+    return response.data.passages;
   }
 );
+
+export const fetchPassageById: AsyncThunk<Passage | null, string, {}> =
+  createAsyncThunk("passage/fetchPassageById", async (id: string) => {
+    const response: ResponseType = await getPassagesId(id);
+    return response.data;
+  });
+  
+  type QueryParams = {
+    destination?: string;
+    origin?: string;
+    price?: number;
+    departureDate?: string;
+    arrivalDate?: string;
+  }
+
+export const fetchPassagesByQuery: AsyncThunk<Passage[], QueryParams, {}> = createAsyncThunk(
+    "passage/fetchPassagesByQuery",
+    async (queryParams) => {
+      const response: ResponseType = await getPassagesByQuery(queryParams);
+      return response.data.passages;
+    }
+  );
 
 const passageSlice = createSlice({
   name: "passages",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchAllPassages.fulfilled, (state, action) => {
-      state.allPassages = action.payload;
-    });
+    builder
+      .addCase(fetchAllPassages.fulfilled, (state, action) => {
+        state.allPassages = action.payload;
+      })
+      .addCase(fetchPassageById.fulfilled, (state, action) => {
+        state.passageById = action.payload;
+      })
+      .addCase(fetchPassagesByQuery.fulfilled, (state, action) => {
+        state.allPassages = action.payload;
+      });
   },
 });
 
