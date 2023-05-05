10ba9dc739247d95a384682a01b13a9aef264781
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-05 11:37:13 -0300

    armando verificacion de adminlogin

---
+++ client/app/adminlogin/admin/tables/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import CardTable from "../../../../../components/Cards/CardRutas";
+import TableDropdown from "../../../../../components/Dropdowns/TableDropdown";
+
+export default function Table() {
+  return (
+    <>
+      <div className="mt-4 flex flex-wrap">
+        <div className="mb-12 w-full px-4">
+          <CardTable />
+        </div>
+        <div className="mb-12 w-full px-4">
+          <CardTable color="dark" />
+        </div>
+      </div>
+    </>
+  );
+}
