2a7307495b99a9d3f6e06d6b67d6d222a7b52bb6
AgustinChavero <agustin1996orellano@gmail.com>
2023-05-05 16:07:59 -0300

    Nuevas routers para outputs de vehicle

--- api/src/routers/outputs/vehicle.ts
+++ api/src/routers/outputs/vehicle.ts
@@ -4,6 +4,9 @@ import {
   searchVehicleByPatent,
   searchVehicleByOwner,
   searchVehicleByYear,
+  getVehicleById,
+  searchVehicleByChauffeur,
+  searchVehicleByBrand,
 } from "../../controllers/outputs/vehicle";
 
 const router = Router();
@@ -11,8 +14,11 @@ const router = Router();
 // Ruta para obtener vehiculos
 
 router.get("/", getVehicles);
+router.get("/:id", getVehicleById);
 router.get("/patent/:patent", searchVehicleByPatent);
+router.get("/chauffeur/:chauffeurId", searchVehicleByChauffeur);
 router.get("/owner/:owner", searchVehicleByOwner);
+router.get("/brand/:brand", searchVehicleByBrand);
 router.get("/year/:year", searchVehicleByYear);
 
 export default router;
