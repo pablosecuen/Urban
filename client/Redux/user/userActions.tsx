5ac54effb0de36158a8103f4fe2c9cf40c48cc78
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-05-05 16:31:24 -0300

    actions de user

--- client/Redux/user/userActions.tsx
+++ client/Redux/user/userActions.tsx
@@ -2,20 +2,27 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 // Define async thunk actions to fetch user data
-export const getAllUsers = async () => {
+export const getAllUsers = createAsyncThunk("users/getAllUsers", async () => {
   const response = await axios.get("http://localhost:3000/user?page=1&pageSize=1000");
-  return response;
-};
+  return response.data.users;
+});
 
 export const getUserById = createAsyncThunk("users/getUserById", async (id: string) => {
-  const response = await fetch(`/api/user/${id}`);
-  const data = await response.json();
-  return data;
+  const response = await axios.get(`http://localhost:3000/user/${id}`);
+  return response.data.user;
 });
 
-export const getUserByName = createAsyncThunk("users/getUserName", async (name: string) => {
-  const response = await fetch(`/api/users?name=${name}`);
-  const data = await response.json();
-  return data;
+export const getUsersByName = createAsyncThunk("users/getUsersByName", async (name: string) => {
+  const response = await axios.get(`http://localhost:3000/user?name=${name}`);
+  return response.data.users;
 });
 
+export const getUsersByCc = createAsyncThunk("users/getUsersByCc", async (cc: string) => {
+  const response = await axios.get(`http://localhost:3000/user?dni=${cc}`);
+  return response.data.users;
+});
+
+export const getUsersByEmail = createAsyncThunk("users/getUsersByEmail", async (email: string) => {
+  const response = await axios.get(`http://localhost:3000/user?email=${email}`);
+  return response.data.users;
+});
