ca8f2aaf48f2361baab282be2dc39cc216cc1208
httpfido <fidel.romano.2016@hotmail.com>
2023-05-06 04:49:32 -0300

    /reserva values

--- client/app/home/reserva/page.tsx
+++ client/app/home/reserva/page.tsx
@@ -1,6 +1,8 @@
-'use client'
+"use client";
+import { fetchPassagesByQuery } from "@component/Redux/passage/passageSlice";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import {
   HiUserGroup,
@@ -24,34 +26,34 @@ import { MdPets } from "react-icons/md";
 //arrivalDate
 
 export default function Reserva() {
+  const dispatch = useDispatch();
 
-const [origin, setOrigin] = useState("");
-const [destination, setDestination] = useState("");
-const [price, setPrice] = useState("");
-const [departureDate, setDepartureDate] = useState("");
-const [arrivalDate, setArrivalDate] = useState("");
+  const [origin, setOrigin] = useState("");
+  const [destination, setDestination] = useState("");
+  const [price, setPrice] = useState("");
+  const [departureDate, setDepartureDate] = useState("");
+  const [arrivalDate, setArrivalDate] = useState("");
 
-const handleOriginChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  setOrigin(e.target.value);  
-}
-const handleDestinationChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  setDestination(e.target.value);
-}
+  const handleOriginChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setOrigin(e.target.value);
+  };
+  const handleDestinationChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDestination(e.target.value);
+  };
 
-const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  setPrice(e.target.value);
-}
+  const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPrice(e.target.value);
+  };
 
-const handleDepartureDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  setDepartureDate(e.target.value);
-}
+  const handleDepartureDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDepartureDate(e.target.value);
+  };
 
-const handleArrivalDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  setArrivalDate(e.target.value);
-}
+  const handleArrivalDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setArrivalDate(e.target.value);
+  };
 
-  const handleSubmit = (e: any) => {}
-
+  const handleSubmit = (e: any) => {};
 
   return (
     <section className="mx-auto w-4/5 rounded-3xl border-2 shadow-2xl shadow-black/40 lg:h-[510px]">
@@ -61,20 +63,35 @@ const handleArrivalDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       <form className="flex flex-col items-center justify-center gap-5 pb-32 pt-12">
         <div className="flex items-center justify-center">
           <HiOutlineLocationMarker className="w-10 text-blue" />
-          <input className="w-2/3 pl-2" placeholder="Desde que lugar..." type="text" 
-          onChange={handleOriginChange}/>
+          <input
+            className="w-2/3 pl-2"
+            placeholder="Desde que lugar..."
+            type="text"
+            value={origin}
+            onChange={handleOriginChange}
+          />
         </div>
 
         <div className="flex items-center justify-center">
           <HiOutlineLocationMarker className="w-10 text-blue" />
-          <input className="w-2/3 pl-2" placeholder="Hasta que lugar..." type="text" 
-          onChange={handleDestinationChange}/>
+          <input
+            className="w-2/3 pl-2"
+            placeholder="Hasta que lugar..."
+            type="text"
+            value={destination}
+            onChange={handleDestinationChange}
+            />
         </div>
 
         <div className="flex items-center justify-center">
           <HiOutlineCalendar className="w-10 text-blue" />
-          <input className="w-2/3 pl-2" placeholder="Cuando?..." type="text" 
-          onChange={handleDepartureDateChange}/>
+          <input
+            className="w-2/3 pl-2"
+            placeholder="Cuando?..."
+            type="text"
+            value={departureDate}
+            onChange={handleDepartureDateChange}
+            />
         </div>
 
         <div className="flex items-center justify-center">
