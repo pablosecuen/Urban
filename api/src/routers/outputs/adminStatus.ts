38ee247a726a882d64dfec38d0a0edd136148305
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-05-06 17:30:14 -0300

    Cambio de ruas general para dmin outputs

--- api/src/routers/outputs/adminStatus.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getAdminState } from "../../controllers/outputs/adminStatus";
-
-const router = Router();
-
-router.get("/", getAdminState);
-
-export default router;
