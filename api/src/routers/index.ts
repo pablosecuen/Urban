d471069ef4e914091234ad70a8c718f3f2fd8c96
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-05-06 16:39:17 -0300

    Change distributor a dealer

--- api/src/routers/index.ts
+++ api/src/routers/index.ts
@@ -15,8 +15,8 @@ import chauffeurOutputRouter from "./outputs/chauffeur";
 import travelInputRouter from "./inputs/travels";
 import travelOutputRouter from "./outputs/travels";
 
-import distributorInputRouter from "./inputs/distributor";
-import distributorOutputRouter from "./outputs/distributor";
+import dealerInputRouter from "./inputs/dealer";
+import dealerOutputRouter from "./outputs/dealer";
 
 import orderInputRouter from "./inputs/order";
 import orderOutputRouter from "./outputs/order";
@@ -59,7 +59,7 @@ router.use("/user", userInputRouter);
 
 router.use("/travels", travelInputRouter);
 
-router.use("/distributor", distributorInputRouter);
+router.use("/dealer", dealerInputRouter);
 
 router.use("/order", orderInputRouter);
 
@@ -106,7 +106,7 @@ router.use("/owner", ownerOutputRouter);
 
 router.use("/products", productOutputRouter);
 
-router.use("/distributor", distributorOutputRouter);
+router.use("/dealer", dealerOutputRouter);
 
 router.use("/order", orderOutputRouter);
 
