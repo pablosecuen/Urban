19a8d3a52e8bd90687844af180b486b8eeb17341
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-05-07 18:45:39 -0300

    ADD ruta  para hablitar vehiculo

--- api/src/routers/inputs/admin.ts
+++ api/src/routers/inputs/admin.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { updateAdminStatus, updateStatusChauffeur, updateStatusDelivery } from "../../controllers/inputs/admin";
+import { updateAdminStatus, updateStatusChauffeur, updateStatusDelivery, updateStatusVehicle } from "../../controllers/inputs/admin";
 
 const router = Router();
 
 router.put("/status", updateAdminStatus);
 router.patch("/changeStatusChauffeur/:id", updateStatusChauffeur);
 router.patch("/changeStatusDelivery/:id", updateStatusDelivery);
+router.patch("/changeStatusVehicle/:id", updateStatusVehicle);
 
 export default router;
