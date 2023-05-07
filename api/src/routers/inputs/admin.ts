a3b79f82d1f2d64a59e3a909a3a845dc7e6dd770
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-05-07 17:48:15 -0300

    ADD rutas para hablitar chofer y repartidor

--- api/src/routers/inputs/admin.ts
+++ api/src/routers/inputs/admin.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { updateAdminStatus } from "../../controllers/inputs/admin";
+import { updateAdminStatus, updateStatusChauffeur, updateStatusDelivery } from "../../controllers/inputs/admin";
 
 const router = Router();
 
 router.put("/status", updateAdminStatus);
-router.delete("/");
+router.delete("/changeStatusChauffeur/:id", updateStatusChauffeur);
+router.delete("/changeStatusDelivery/:id", updateStatusDelivery);
 
 export default router;
