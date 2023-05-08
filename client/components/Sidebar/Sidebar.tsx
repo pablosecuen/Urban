2bc2d7b5e8b66334798222b41c46e25b3b53b5ee
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-05-08 15:14:24 -0300

    Sidebar admin - add Chauffeurs

--- client/components/Sidebar/Sidebar.tsx
+++ client/components/Sidebar/Sidebar.tsx
@@ -146,6 +146,26 @@ const Sidebar: React.VFC<SidebarProps> = (
                     ></path>
                   </g>
                 </svg>
+                <Link href="/admin/dashboard/chauffeurs">Chauffeurs</Link>
+              </li>
+
+              <li className="flex items-center justify-start gap-2">
+                <svg
+                  width="25px"
+                  height="25px"
+                  viewBox="0 0 1024 1024"
+                  fill="#000000"
+                  className="w-auto"
+                >
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                  <g id="SVGRepo_iconCarrier">
+                    <path
+                      fill="#000000"
+                      d="M764.416 254.72a351.68 351.68 0 0 1 86.336 149.184H960v192.064H850.752a351.68 351.68 0 0 1-86.336 149.312l54.72 94.72-166.272 96-54.592-94.72a352.64 352.64 0 0 1-172.48 0L371.136 936l-166.272-96 54.72-94.72a351.68 351.68 0 0 1-86.336-149.312H64v-192h109.248a351.68 351.68 0 0 1 86.336-149.312L204.8 160l166.208-96h.192l54.656 94.592a352.64 352.64 0 0 1 172.48 0L652.8 64h.128L819.2 160l-54.72 94.72zM704 499.968a192 192 0 1 0-384 0 192 192 0 0 0 384 0z"
+                    ></path>
+                  </g>
+                </svg>
                 <Link href="/admin/dashboard/travels">Travels</Link>
               </li>
 
