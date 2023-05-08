8758f8c23b6a325a31eda608d038af9c2ada0e13
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-05-08 17:01:05 -0300

    variables y estados para el paymente succesfull

--- client/app/home/especiales/confirmacion/pagos/checkout/page.tsx
+++ client/app/home/especiales/confirmacion/pagos/checkout/page.tsx
@@ -1,17 +1,26 @@
-
+import axios from "axios";
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
-import success from "../../../../../../../assets/imagenes/success.png"
+import success from "../../../../../../../assets/imagenes/success.png";
+import { useState } from "react";
 
+export default function Checkout() {
+  const queryParams = new URLSearchParams(location.search);
+  const paymentId: string | null = queryParams.get("payment_id");
+  const merchantOrder: string | null = queryParams.get("merchant_order_id");
+  const status: string | null = queryParams.get("status");
+  const [dataState, setDataState] = useState({});
+  const userId: string | null = "";
 
-export default function Checkout () {
-    return (
-        <div className="flex flex-col justify-center gap-8">
-            <p className="text-center text-2xl font-bold">Su pago ha sido realizado con éxito !</p>
-            <Image src={success} alt="pago exitoso" className="h-80 w-80 self-center"></Image>
-            <Link href="/home">
-            <button className="w-1/2 flex  mx-auto py-1 text-center justify-center">Volver al inicio</button>
-            </Link>
-        </div>
-    )
+  return (
+    <div className="flex flex-col justify-center gap-8">
+      <p className="text-center text-2xl font-bold">Su pago ha sido realizado con éxito !</p>
+      <Image src={success} alt="pago exitoso" className="h-80 w-80 self-center"></Image>
+      <Link href="/home">
+        <button className="mx-auto flex  w-1/2 justify-center py-1 text-center">
+          Volver al inicio
+        </button>
+      </Link>
+    </div>
+  );
 }
