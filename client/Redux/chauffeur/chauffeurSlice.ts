4ae4f92bafa8252901e79472cf14da4a2082b58a
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-05-08 15:00:18 -0300

    chauffeurSlice  - reestructuracion

--- client/Redux/chauffeur/chauffeurSlice.ts
+++ client/Redux/chauffeur/chauffeurSlice.ts
@@ -1,54 +1,53 @@
 import { createSlice, createAsyncThunk, AsyncThunk } from "@reduxjs/toolkit";
-// import { getAllPassages } from "./passageActions";
-import { AxiosResponse } from "axios";
-import { Chauffeur, ChauffeursArrayResponse } from "@component/app/types/Chauffeur";
-import { getAllChauffeurs } from "./chauffeurActions";
+import { Chauffeur, FilteredChauffeurs } from "@component/app/types/Chauffeur";
+import { getFilteredChauffeurs } from "./chauffeurActions";
 
 interface ChauffeurInitialState {
-  allChauffeurs: ChauffeursArrayResponse | null;
-  chauffeurById: Chauffeur | null;
-  chauffeurByCc: Chauffeur | null;
-  chauffeurByCe: Chauffeur | null;
-  chauffeurByPatent: Chauffeur | null;
-  chauffeurByName: Chauffeur[];
-  chauffeursPendingPayment: Chauffeur[];
+  filteredChauffeurs: FilteredChauffeurs | null;
+  chauffeursPendingPayment: Chauffeur[]; // van a traer el balance desde el back? sino adaptar
+  status: "idle" | "loading" | "succeeded" | "failed"; // verificar si funciona el mismo status para todas las actions
+  error: string | null;
 }
 
 const initialState: ChauffeurInitialState = {
-  allChauffeurs: null,
-  chauffeurById: null,
-  chauffeurByCc: null,
-  chauffeurByCe: null,
-  chauffeurByPatent: null,
-  chauffeurByName: [],
+  filteredChauffeurs: null,
   chauffeursPendingPayment: [],
+  status: "idle",
+  error: null,
 };
-
-type ResponseType = AxiosResponse<any, any>;
-
-export const fetchAllChauffeurs: AsyncThunk<ChauffeursArrayResponse, void, {}> = createAsyncThunk(
-  "cahuffeur/fetchAllChauffeurs",
-  async () => {
-    const response: ResponseType = await getAllChauffeurs({ page: 1, pageSize: 10 });
-    return response.data;
-  }
-);
-export const getChauffeurById: AsyncThunk<Chauffeur, void, {}> = createAsyncThunk(
-  "cahuffeur/getChauffeurById",
-  async () => {
-    const response: ResponseType = await getAllChauffeurs({ page: 1, pageSize: 10 });
-    return response.data.chauffeur;
-  }
-);
 
 const chauffeurSlice = createSlice({
   name: "chauffeurs",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchAllChauffeurs.fulfilled, (state, action) => {
-      state.allChauffeurs = action.payload;
-    });
+    builder
+      .addMatcher(
+        (action) => [getFilteredChauffeurs.pending].includes(action.type),
+        (state) => {
+          state.status = "loading";
+        }
+      )
+      .addMatcher(
+        (action) => [getFilteredChauffeurs.fulfilled].includes(action.type),
+        (state, action) => {
+          state.status = "succeeded";
+          state.filteredChauffeurs = {
+            // ...action.payload, // esto es igual a lo de abajo pero puede cambiar desde el backend
+            chauffeurs: action.payload.chauffeurs,
+            currentPage: action.payload.currentPage,
+            totalPages: action.payload.totalPages,
+            activeFilters: action.payload.activeFilters,
+          };
+        }
+      )
+      .addMatcher(
+        (action) => [getFilteredChauffeurs.rejected].includes(action.type),
+        (state, action) => {
+          state.status = "failed";
+          state.error = action.payload as string;
+        }
+      );
   },
 });
 
