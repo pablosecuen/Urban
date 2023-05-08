1e3292e32d7172d323b7eb9f36f8a57810964b76
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-08 19:56:42 -0300

    fix en el renderizado de admin travels, correcta distribucion de props a componentes hijos

--- client/app/admin/dashboard/travels/page.tsx
+++ client/app/admin/dashboard/travels/page.tsx
@@ -1,20 +1,51 @@
-import React from "react";
+"use client";
 
-import CardTravels from "../../../../components/Cards/CardTravels";
+import React, { useEffect, useState } from "react";
+import CardTravels from "@component/components/Cards/CardTravels";
+import { ThunkDispatch } from "redux-thunk";
+import { AnyAction } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@component/Redux/store/store";
+import { getAllTravels } from "@component/Redux/travel/travelActions";
+import { Travel } from "@component/app/types/Travels";
+import CardSelectedTravel from "@component/components/Cards/CardTravelsSelected";
 
-import CardProfile from "@component/components/Cards/CardProfile";
+export interface CardTravelsProps {
+  allTravels: Travel[];
+  handleClickFunction: (travel: Travel) => void;
+  selectedTravel: Travel | null;
+}
 
-// components
+export interface CardTravelProps {
+  selectedTravel: Travel | null;
+}
 
-export default function Users() {
+export default function Travels() {
+  const dispatch: ThunkDispatch<RootState, undefined, AnyAction> = useDispatch();
+  const allTravels = useSelector((state: RootState) => state.travel.allTravels);
+  const [selectedTravel, setSelectedTravel] = useState<Travel | null>(null);
+
+  useEffect(() => {
+    dispatch(getAllTravels());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleClickFunction = (travel: Travel) => {
+    setSelectedTravel(travel);
+  };
+
   return (
     <>
       <div className="flex flex-wrap">
         <div className="w-full px-4 lg:w-8/12">
-          <CardTravels />
+          <CardTravels
+            allTravels={allTravels}
+            handleClickFunction={handleClickFunction}
+            selectedTravel={selectedTravel}
+          />
         </div>
         <div className="w-full px-4 lg:w-4/12">
-          <CardProfile selectedUser={null} />
+          <CardSelectedTravel selectedTravel={selectedTravel} />
         </div>
       </div>
     </>
