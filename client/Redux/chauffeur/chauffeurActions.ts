4ebda4113ed08b84e1c85395326f1e28a4676179
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-05-08 15:00:28 -0300

    chauffeurActions - reestructuracion

--- client/Redux/chauffeur/chauffeurActions.ts
+++ client/Redux/chauffeur/chauffeurActions.ts
@@ -1,16 +1,14 @@
+import { Chauffeur, ChauffeurQueryParams } from "@component/app/types/Chauffeur";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
-// Define async thunk actions to fetch user data
-interface GetAllChauffeursArgs {
-  page: number;
-  pageSize: number;
-}
-export const getAllChauffeurs = async ({ page, pageSize }: GetAllChauffeursArgs) => {
-  const response = await axios.get(
-    `http://localhost:3000/chauffeur?page=${page}&pageSize=${pageSize}`
-  );
-  console.log(response);
-
-  return response;
-};
+export const getFilteredChauffeurs = createAsyncThunk<AxiosResponse, ChauffeurQueryParams>(
+  "chauffeur/getFilteredChauffeurs",
+  async (queryParams: ChauffeurQueryParams): Promise<AxiosResponse> => {
+    const urlSearchParams = new URLSearchParams(queryParams as Record<string, string>);
+    const response: AxiosResponse = await axios.get(
+      `http://localhost:3000/cahuffeur?page=1&pageSize=10&${urlSearchParams.toString()}`
+    );
+    return response.data; // podriamos usar un adapter para asignar en el state toda la info con el spred
+  }
+);
