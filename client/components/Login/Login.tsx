834f4d17442aa3ef1e41cb852e4faf4674bab311
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-05-08 18:11:28 -0300

    sessionstorage de isregister

--- client/components/Login/Login.tsx
+++ client/components/Login/Login.tsx
@@ -5,23 +5,29 @@ import RegisterForm from "../RegisterForm/RegisterForm";
 import LoginForm from "../LoginForm/LoginForm";
 
 const Login = () => {
-  const [isRegister, setIsRegister] = useState(false);
-  console.log(isRegister);
+  const isRegisterFromSessionStorage = sessionStorage.getItem("isRegister");
+  const [isRegister, setIsRegister] = useState(Boolean(isRegisterFromSessionStorage) || false);
+
+  const handleSetIsRegister = (value: boolean) => {
+    setIsRegister(value);
+    sessionStorage.setItem("isRegister", value.toString());
+  };
+
   return (
     <div className="relative mt-0 flex  items-center justify-center lg:h-screen ">
       <div
-        className={` transition_all absolute bottom-0 left-0 right-0 top-56 m-auto grid w-96 place-content-center opacity-0 lg:top-0   ${
+        className={` transition_all absolute bottom-0 left-0 right-0 top-56 m-auto grid w-96 place-content-center opacity-0 lg:-left-96 lg:top-0   ${
           isRegister && "z-10 opacity-100"
         }`}
       >
-        <RegisterForm isRegister={isRegister} setIsRegister={setIsRegister} />
+        <RegisterForm isRegister={isRegister} setIsRegister={handleSetIsRegister} />
       </div>
       <div
-        className={`transition_all absolute bottom-0 left-0 right-0 top-56 m-auto grid place-content-center opacity-0  lg:top-0  ${
+        className={`transition_all absolute bottom-0 left-0 right-0 top-56 m-auto grid place-content-center opacity-0 lg:-left-96  lg:top-0  ${
           !isRegister && "z-10 opacity-100"
         }`}
       >
-        <LoginForm isRegister={isRegister} setIsRegister={setIsRegister} />
+        <LoginForm isRegister={isRegister} setIsRegister={handleSetIsRegister} />
       </div>
     </div>
   );
