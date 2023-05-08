a5551849c0b962e847f7c2e921bb2d6265c5cbdc
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-08 19:07:44 -0300

    fix redux a casos builder individuales

--- client/Redux/passage/passageActions.tsx
+++ client/Redux/passage/passageActions.tsx
@@ -4,22 +4,29 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 // Define async thunk actions to fetch user data
-export const getAllPassages = createAsyncThunk<Passage[], void, {}>("passage/getAllPassages", async () => {
-  const response = await axios.get("http://localhost:3000/passage?page=1&pageSize=10000");
-  return response.data.passages;
-})
+export const getAllPassages = createAsyncThunk<Passage[], void, {}>(
+  "passage/getAllPassages",
+  async () => {
+    const response = await axios.get("http://localhost:3000/passage?page=1&pageSize=10000");
+    return response.data.passages;
+  }
+);
 
-export const getPassagesId = createAsyncThunk<Passage | null, string, {}>("passage/getPassagesId", async (id: string) => {
+export const getPassagesId = createAsyncThunk<Passage, string, {}>(
+  "passage/getPassagesId",
+  async (id: string) => {
     const response = await axios.get(`http://localhost:3000/passage/${id}`);
     return response.data;
-})
+  }
+);
 
-export const getPassagesByQuery = createAsyncThunk<Passage[], QueryParams, {}>("passage/fetchPassagesByQuery", async (queryParams: QueryParams) => {
-  const urlSearchParams = new URLSearchParams(queryParams as Record<string, string>);
-  const response = await axios.get(
-    `http://localhost:3000/passage?page=1&pageSize=10000&${urlSearchParams.toString()}`
-  );
-  console.log(response.data.passages);
-  return response.data;
-})
-
+export const getPassagesByQuery = createAsyncThunk<Passage[], QueryParams, {}>(
+  "passage/fetchPassagesByQuery",
+  async (queryParams: QueryParams) => {
+    const urlSearchParams = new URLSearchParams(queryParams as Record<string, string>);
+    const response = await axios.get(
+      `http://localhost:3000/passage?page=1&pageSize=10000&${urlSearchParams.toString()}`
+    );
+    return response.data;
+  }
+);
