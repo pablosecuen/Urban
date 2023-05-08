665f6ec34d59593b3e63bf45ac4e792026c89dcf
httpfido <fidel.romano.2016@hotmail.com>
2023-05-08 12:01:04 -0300

    /reserva nueva interface

--- client/app/types/Passages.ts
+++ client/app/types/Passages.ts
@@ -1,15 +1,30 @@
-export interface PassageToRegister {
+interface PassageToRegister {
+  id: string;
+  duration: string;
+  numberSeat: number;
+  createdAt: string;
+  deleted: boolean;
+  price: number;
   origin: string;
-  stock: number;
   destination: string;
   description: string;
   departureDate: string;
+  stock: number;
   arrivalDate: string;
-  duration: string;
-  price: number;
-  numberSeat: number;
-  createdAt: string
+  status: boolean;
 }
+// export interface PassageToRegister {
+//   origin: string;
+//   stock: number;
+//   destination: string;
+//   description: string;
+//   departureDate: string;
+//   arrivalDate: string;
+//   duration: string;
+//   price: number;
+//   numberSeat: number;
+//   createdAt: string
+// }
 export interface Passage extends PassageToRegister {
   deleted: boolean;
   status: boolean;
