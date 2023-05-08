af94ee2eaba9c6d77aad7a4e57f56f6874c1854d
Benja <benyamin0714@gmail.com>
2023-05-08 13:43:42 -0300

    DatePicker

--- client/app/home/paqueteria/page.tsx
+++ client/app/home/paqueteria/page.tsx
@@ -1,36 +1,40 @@
 import { HiOutlineLocationMarker, HiOutlineCalendar, HiOutlineBriefcase } from "react-icons/hi";
 import Link from "next/link";
-
+import PickDate from "@component/components/PickDate/PickDate";
 
 export default function Paqueteria() {
   return (
-    <div className="w-4/5 mx-auto border-2 rounded-3xl shadow-2xl shadow-black/40 mb-8 lg:mb-0">
-      <h1 className="text-blue text-center text-xl mt-12">
+    <div className="mx-auto mb-8 w-4/5 rounded-3xl border-2 shadow-2xl shadow-black/40 lg:mb-0">
+      <h1 className="mt-12 text-center text-xl text-blue">
         Llena el formulario para encontrar tu cadete
       </h1>
-      <form className="flex flex-col gap-5 pb-20 pt-12 items-center justify-center">
+      <form className="flex flex-col items-center justify-center gap-5 pb-20 pt-12">
         <div className="flex items-center justify-center">
           <HiOutlineLocationMarker className="w-10 text-blue" />
-          <input className="pl-2 w-2/3" placeholder="Desde que lugar..." type="text" />
+          <input className="w-2/3 pl-2" placeholder="Desde que lugar..." type="text" />
         </div>
 
         <div className="flex items-center justify-center">
           <HiOutlineLocationMarker className="w-10 text-blue" />
-          <input className="pl-2 w-2/3" placeholder="Hasta que lugar..." type="text" />
+          <input className="w-2/3 pl-2" placeholder="Hasta que lugar..." type="text" />
         </div>
 
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center xl:ml-[119px]">
           <HiOutlineCalendar className="w-10 text-blue" />
-          <input className="pl-2 w-2/3" placeholder="Cuando?..." type="text" />
+          <PickDate />
+          {/* <input className="pl-2 w-2/3" placeholder="Cuando?..." type="text" /> */}
         </div>
 
         <div className="flex items-center justify-center">
           <HiOutlineBriefcase className="w-10 text-blue" />
-          <input className="pl-2 w-2/3" placeholder="Items a transportar..." type="text" />
+          <input className="w-2/3 pl-2" placeholder="Items a transportar..." type="text" />
         </div>
 
-        <Link href={'home/paqueteria/transportes'} className="w-1/2 bg-blue text-white rounded-lg p-2 text-center">
-           <span>Buscar a tu cadete!</span> 
+        <Link
+          href={"home/paqueteria/transportes"}
+          className="w-1/2 rounded-lg bg-blue p-2 text-center text-white"
+        >
+          <span>Buscar a tu cadete!</span>
         </Link>
       </form>
     </div>
