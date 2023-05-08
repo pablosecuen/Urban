588a8b76d390b9bda3a7b1d44e683ca50c3e52a8
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-08 17:06:19 -0300

    cambie el slice y actions de vehiculos para que quede con el metodo .march del builder

--- client/app/home/especiales/confirmacion/pagos/checkout/page.tsx
+++ client/app/home/especiales/confirmacion/pagos/checkout/page.tsx
@@ -1,17 +1,17 @@
-
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
-import success from "../../../../../../../assets/imagenes/success.png"
+import success from "../../../../../../assets/imagenes/success.png";
 
-
-export default function Checkout () {
-    return (
-        <div className="flex flex-col justify-center gap-8">
-            <p className="text-center text-2xl font-bold">Su pago ha sido realizado con éxito !</p>
-            <Image src={success} alt="pago exitoso" className="h-80 w-80 self-center"></Image>
-            <Link href="/home">
-            <button className="w-1/2 flex  mx-auto py-1 text-center justify-center">Volver al inicio</button>
-            </Link>
-        </div>
-    )
+export default function Checkout() {
+  return (
+    <div className="flex flex-col justify-center gap-8">
+      <p className="text-center text-2xl font-bold">Su pago ha sido realizado con éxito !</p>
+      <Image src={success} alt="pago exitoso" className="h-80 w-80 self-center" />
+      <Link href="/home">
+        <button className="mx-auto flex  w-1/2 justify-center py-1 text-center">
+          Volver al inicio
+        </button>
+      </Link>
+    </div>
+  );
 }
