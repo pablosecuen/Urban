cdcf714bebb38ad0d39a70cc6ef89eb049676763
Benja <benyamin0714@gmail.com>
2023-05-08 14:22:57 -0300

    responsive mobile

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
   }, [token]);
 
   return (
-    <div className="shadow/30 mb-8 w-full justify-center rounded-3xl border-2 px-4 py-4 shadow-lg xl:mb-0 xl:flex xl:w-4/5 xl:flex-col xl:px-24">
+    <div className="shadow/30 mb-8 w-full justify-center rounded-3xl border-2 px-4 py-4 shadow-lg min-[420px]:mt-32 min-[420px]:w-4/5 ">
       <p className="my-2 text-center text-2xl font-bold">
         Bienvedios a <span className="text-verdeurban">U</span>
         <span className=" text-celeste">rban</span>!
@@ -53,8 +53,8 @@ export default function Home() {
         problemas cotidianos en la menor cantidad de tiempo posibles, acercando a tu mano un poder
         increible para <b>gestionar</b> tu tiempo y tus días de la mejor manera posible
       </p>
-      <p className="mt-8 text-center text-lg font-bold">
-        Selecciona el tipo de servicio para poder continuar
+      <p className="mt-8 text-center text-lg font-bold ">
+        {"<-- "}Selecciona el tipo de servicio para poder continuar
       </p>
     </div>
   );
