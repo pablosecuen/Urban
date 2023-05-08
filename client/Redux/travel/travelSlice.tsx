c5936a57a1f810277108bc3a318d260797814cd2
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-08 19:07:52 -0300

    fix redux a casos builder individuales

--- client/Redux/travel/travelSlice.tsx
+++ client/Redux/travel/travelSlice.tsx
@@ -1,34 +1,75 @@
 import { createSlice, createAsyncThunk, AsyncThunk } from "@reduxjs/toolkit";
 import { Travel } from "../../app/types/Travels";
-import { getAllTravels } from "./travelActions";
-import { AxiosResponse } from "axios";
+import { getAllTravels, getTravelsById, getTravelsByQuery } from "./travelActions";
 
 interface TravelState {
   allTravels: Travel[]; // change any[] to your specific type
+  allTravelsByQuery: Travel[];
+  travelById: { [key: string]: Travel | null };
+  status: "idle" | "loading" | "succeeded" | "failed";
+  error: string | null;
 }
 
 const initialState: TravelState = {
   allTravels: [], // provide an empty array as the initial state value for allUsers
+  allTravelsByQuery: [],
+  travelById: {},
+  status: "idle",
+  error: null,
 };
-
-type ResponseType = AxiosResponse<any, any>;
-
-export const fetchAllTravels: AsyncThunk<Travel[], void, {}> = createAsyncThunk(
-  "travels/fetchAllTravels",
-  async () => {
-    const response: ResponseType = await getAllTravels();
-    return response.data.travels; // return the users array
-  }
-);
 
 const travelSlice = createSlice({
   name: "travels",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchAllTravels.fulfilled, (state, action) => {
-      state.allTravels = action.payload;
-    });
+    builder
+      // Handle the pending state
+      .addCase(getAllTravels.pending, (state) => {
+        state.status = "loading";
+      })
+      // Handle the success state
+      .addCase(getAllTravels.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.allTravels = action.payload;
+      })
+      // Handle the error state
+      .addCase(getAllTravels.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message ?? "Something went wrong";
+      });
+
+    builder
+      // Handle the pending state
+      .addCase(getTravelsById.pending, (state) => {
+        state.status = "loading";
+      })
+      // Handle the success state
+      .addCase(getTravelsById.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.travelById[action.payload.id] = action.payload;
+      })
+      // Handle the error state
+      .addCase(getTravelsById.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message ?? "Something went wrong";
+      });
+
+    builder
+      // Handle the pending state
+      .addCase(getTravelsByQuery.pending, (state) => {
+        state.status = "loading";
+      })
+      // Handle the success state
+      .addCase(getTravelsByQuery.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.allTravelsByQuery = action.payload;
+      })
+      // Handle the error state
+      .addCase(getTravelsByQuery.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message ?? "Something went wrong";
+      });
   },
 });
 
