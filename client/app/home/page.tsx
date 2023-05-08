1140a4225b613a43690eef7069e89b6d66dd3486
Benja <benyamin0714@gmail.com>
2023-05-08 14:23:42 -0300

    responsive xl

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -38,12 +38,12 @@ export default function Home() {
   }, [token]);
 
   return (
-    <div className="shadow/30 mb-8 w-full justify-center rounded-3xl border-2 px-4 py-4 shadow-lg min-[420px]:mt-32 min-[420px]:w-4/5 ">
+    <div className="shadow/30 mb-8 w-full justify-center rounded-3xl border-2 px-4 py-4 shadow-lg min-[420px]:mt-32 min-[420px]:w-4/5 xl:h-4/5 xl:w-full ">
       <p className="my-2 text-center text-2xl font-bold">
         Bienvedios a <span className="text-verdeurban">U</span>
         <span className=" text-celeste">rban</span>!
       </p>
-      <p className="w-full text-center lg:text-xs ">
+      <p className="w-full text-center lg:text-xs xl:mt-10 xl:text-2xl ">
         Nuestra <b>misión</b> es entregar al usuario el poder de decidir su medio de transporte
         acercando a la oferta a la palma de su mano, <b>para empoderarte</b> con mayor información
         asistiendo en que tomes la mejor decision disponble dentro de tus necesidades, en nuestra
@@ -53,7 +53,7 @@ export default function Home() {
         problemas cotidianos en la menor cantidad de tiempo posibles, acercando a tu mano un poder
         increible para <b>gestionar</b> tu tiempo y tus días de la mejor manera posible
       </p>
-      <p className="mt-8 text-center text-lg font-bold ">
+      <p className="mt-8 text-center text-lg font-bold xl:mt-10">
         {"<-- "}Selecciona el tipo de servicio para poder continuar
       </p>
     </div>
