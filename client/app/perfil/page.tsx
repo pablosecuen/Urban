26ce599860107e97a6b70f806c1e2f30de795411
Benja <benyamin0714@gmail.com>
2023-05-09 16:19:46 -0300

    ya se renderizan los datos en scroll

--- client/app/perfil/page.tsx
+++ client/app/perfil/page.tsx
@@ -1,17 +1,28 @@
 "use client";
 import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { pestañasHistorialPerfil, contenidoHistorialPerfil } from "../../assets/data";
+import { RootState } from "@component/Redux/store/store";
+import { getTravelsByUserId } from "../../Redux/travel/travelActions";
+import { Dispatch } from "@reduxjs/toolkit";
 
 export default function Perfil() {
   const [loading, setLoading] = useState(true);
   const [activeTab, setActiveTab] = useState("Viajes");
   const [userData, setUserData] = useState(null);
 
+  const dispatch = useDispatch<Dispatch<any>>();
+  const userTravels = useSelector((state: RootState) => state.travel.userTravels);
+
+  console.log({ userTravels: userTravels });
+
   const handleTabClick = (tabName: string) => {
     setActiveTab(tabName);
   };
 
+  const BENJAUSER = "112977155091916444768";
   useEffect(() => {
+    dispatch(getTravelsByUserId("112977155091916444768"));
     setLoading(false);
     const userDataString = localStorage.getItem("user");
     if (userDataString) {
@@ -46,7 +57,19 @@ export default function Perfil() {
           {activeTab === `${item.name}` && (
             <div className="h-full w-full overflow-hidden border-2 bg-red-300 " title={item.title}>
               <h4>{item.p}</h4>
-              <div className=" h-full w-full overflow-scroll border-2">{item.placeHolder}</div>
+              <div className=" h-full w-full overflow-scroll border-2">
+                {userTravels.map((item, id) => (
+                  <div className="my-2 p-2 shadow-xl" key={id}>
+                    <h1>{item.travel}</h1>
+                    <h1>{item.price}</h1>
+                    <div className="flex">
+                      <h1>Desde: {item.origin}</h1>
+                      <p>TO</p>
+                      <h1>Hasta: {item.destination}</h1>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
           )}
         </>
