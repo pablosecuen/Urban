61477a6a3be06db0d69186e0e20736292dde8857
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-05-09 16:25:44 -0300

    ChauffeurDropDownSettings

---
+++ client/components/Dropdowns/ChauffeurDropDownSettings.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { createPopper } from "@popperjs/core";
+import Image from "next/image";
+
+const ChauffeurDropDownSettings = () => {
+  return (
+    <div
+      className={
+        "absolute right-7 top-14  z-50  w-48 list-none rounded bg-white py-2 text-left text-base shadow-lg"
+      }
+    >
+      <a
+        href="#pablo"
+        className={
+          "block w-48 whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-blueGray-700"
+        }
+        onClick={(e) => e.preventDefault()}
+      >
+        Action
+      </a>
+      <a
+        href="#pablo"
+        className={
+          "block w-48 whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-blueGray-700"
+        }
+        onClick={(e) => e.preventDefault()}
+      >
+        Another action
+      </a>
+      <a
+        href="#pablo"
+        className={
+          "block w-48 whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-blueGray-700"
+        }
+        onClick={(e) => e.preventDefault()}
+      >
+        Something else here
+      </a>
+    </div>
+  );
+};
+
+export default ChauffeurDropDownSettings;
