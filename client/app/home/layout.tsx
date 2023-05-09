34c0803f005b684fc294362d5ea0450931b2f4d9
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-09 18:56:08 -0300

    cambios fix, en mapeos y src de imagenes

--- client/app/home/layout.tsx
+++ client/app/home/layout.tsx
@@ -1,7 +1,7 @@
 import NavBar from "@component/components/NavBar/NavBar";
 import Footer from "@component/components/Footer/Footer";
 import logo from "../../assets/imagenes/UrbanIsoLogo.png";
-import Image, { StaticImageData } from "next/image";
+import Image from "next/image";
 
 import Menu from "@component/components/Menu/Menu";
 
@@ -12,9 +12,9 @@ export default function Home({ children }: { children: React.ReactNode }) {
       <div className="mx-auto flex h-[92vh] flex-col items-center justify-center py-10 lg:container lg:flex-row lg:pb-20">
         <div className="flex w-full flex-col items-center justify-center gap-4">
           <Image
-            src={logo as StaticImageData}
+            src={logo}
             alt="logo"
-            className=" mt-48  w-24 lg:mt-10 lg:h-52 lg:w-56 2xl:h-72 2xl:w-80"
+            className="  mt-10  w-24 lg:mt-10 lg:h-52 lg:w-56 2xl:h-72 2xl:w-80"
           />
           <Menu />
         </div>
