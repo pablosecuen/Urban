28d085789740567d0887e6b76590553d7e4d1caa
httpfido <fidel.romano.2016@hotmail.com>
2023-05-09 04:19:58 -0300

    estilos Menu

--- client/components/Menu/Menu.tsx
+++ client/components/Menu/Menu.tsx
@@ -11,8 +11,8 @@ export default function Menu() {
   return (
     <>
       {!isMobile && (
-        <div className="flex h-full w-full flex-col gap-3 pb-8 lg:w-full lg:items-center lg:justify-center lg:rounded-3xl lg:bg-blueGray-300 lg:px-6 lg:py-8 lg:shadow-2xl lg:shadow-black/40 2xl:w-3/4 2xl:py-16">
-          <Link
+        <div className="flex h-full w-full flex-col gap-3 pb-8 lg:w-full lg:items-center lg:justify-center lg:rounded-3xl lg:bg-blueGray-300 lg:px-6 lg:py-8 lg:shadow-2xl lg:shadow-black/40  2xl:py-16">
+        <Link
             href="/home/reserva"
             className={`link w-72 hover:bg-verde 2xl:w-full 2xl:text-lg ${
               pathname === "/home/reserva" && "!bg-verde "
