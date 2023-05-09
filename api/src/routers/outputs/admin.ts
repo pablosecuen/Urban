29aa84e5b073bd627a3e1765bba4443b272a21f9
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-05-09 00:30:41 -0300

    ADD ruta para choferes en false

--- api/src/routers/outputs/admin.ts
+++ api/src/routers/outputs/admin.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getAdminState, getProfit } from "../../controllers/outputs/admin";
+import { getAdminState, getInactiveChauffeur, getProfit } from "../../controllers/outputs/admin";
 
 const router = Router();
 
 router.get("/status", getAdminState);
 router.get("/profit", getProfit);
+router.get("/chaufeur", getInactiveChauffeur);
 
 export default router;
