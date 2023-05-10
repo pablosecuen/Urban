efeb54c2da3a0d39f085a6f9e7963d50ae0c76ad
Benja <benyamin0714@gmail.com>
2023-05-10 12:13:44 -0300

    lg y xl

--- client/app/perfil/layout.tsx
+++ client/app/perfil/layout.tsx
@@ -7,7 +7,7 @@ const layoutPerfil = ({ children }: any) => {
     <div className="flex w-full flex-col">
       <NavBar />
       <div className="flex items-center justify-center pb-32 ">
-        <div className="flex h-full flex-col items-center justify-center gap-5 px-4 lg:mt-20 lg:w-4/5 lg:flex-row xl:w-3/5 2xl:h-full ">
+        <div className="flex h-full flex-col items-center justify-center gap-5 px-4 lg:mt-20 lg:w-4/5 lg:flex-row xl:w-3/5">
           <Profile />
           <div className="flex h-96 flex-col items-center justify-center">{children}</div>
         </div>
