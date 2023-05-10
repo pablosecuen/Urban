8958ee0c3627aac61139b580b2e03d031b9bc9af
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-10 15:25:09 -0300

    passages dashboard refactor

--- client/app/admin/dashboard/passages/page.tsx
+++ client/app/admin/dashboard/passages/page.tsx
@@ -1,20 +1,58 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 import CardPassages from "../../../../components/Cards/CardPassages";
 
-import CardProfile from "@component/components/Cards/CardProfile";
+import CardProfilePassage from "@component/components/Cards/CardProfilePassage";
+import { ThunkDispatch } from "redux-thunk";
+import { Passage } from "@component/app/types/Passages";
+import { RootState } from "@component/Redux/store/store";
+import { AnyAction } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllPassages } from "@component/Redux/passage/passageActions";
 
 // components
 
-export default function Users() {
+export default function Passages() {
+  const dispatch: ThunkDispatch<RootState, undefined, AnyAction> = useDispatch();
+  const allPassages = useSelector((state: RootState) => state.passage.allPassages);
+  const [selectedPassage, setSelectedPassage] = useState<Passage | null>(null);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    dispatch(getAllPassages());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleSearchChange = (event: any) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleClick = (passage: Passage) => {
+    setSelectedPassage(passage);
+  };
+
+  const filteredPassages = allPassages.filter(
+    (passage: any) =>
+      (passage.origin && passage.origin.toLowerCase().includes(searchTerm.toLowerCase())) ||
+      (passage.userId && passage.userId.toLowerCase().includes(searchTerm.toLowerCase()))
+  );
+
   return (
     <>
       <div className="flex flex-wrap">
         <div className="w-full px-4 lg:w-8/12">
-          <CardPassages />
+          <CardPassages
+            allPassages={allPassages}
+            handleClick={handleClick}
+            selectedPassage={selectedPassage}
+            handleSearchChange={handleSearchChange}
+            searchTerm={searchTerm}
+            filteredPassages={filteredPassages}
+          />
         </div>
         <div className="w-full px-4 lg:w-4/12">
-          <CardProfile selectedUser={null} />
+          <CardProfilePassage selectedPassage={selectedPassage} />
         </div>
       </div>
     </>
