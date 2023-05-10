9c6823e95e12277b0fb20d7778560067e6e623c3
Benja <benyamin0714@gmail.com>
2023-05-10 15:51:57 -0300

    ya anda pero sigue en curso

--- client/components/PickDate/PickDate.tsx
+++ client/components/PickDate/PickDate.tsx
@@ -1,27 +1,62 @@
 "use client";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+import { addMonths, isSameMonth, format } from "date-fns";
 import { useState } from "react";
-import DatePicker from "react-datepicker";
-
-import "react-datepicker/dist/react-datepicker.css";
+import { es } from "date-fns/locale";
 
 export default function PickDate() {
-  const [fecha, setFecha] = useState(new Date());
+  const today = new Date();
+  const nextMonth = addMonths(new Date(), 1);
+  const [selected, setSelected] = useState<Date>();
 
-  const handleFechaChange = (date: any) => {
-    setFecha(date);
-  };
+  let footer = <p>Por favor elige un dia.</p>;
+  if (selected) {
+    footer = <p>seleccionaste {format(selected, "PP", { locale: es })}.</p>;
+  }
+
+  const defaultDate = new Date(2023, 4);
 
   return (
-    <DatePicker
-      className="xl:ml-1 xl:w-[310px] xl:pl-2 "
-      dateFormat="dd/MM/yyyy"
-      selected={fecha}
-      onChange={handleFechaChange}
-      showYearDropdown
-      yearDropdownItemNumber={4}
-      scrollableYearDropdown
-      placeholderText="Escriba o elija una fecha..."
-      calendarClassName="flex items-center relative justify-center xl:ml-[800px] xl:w-1/4"
+    <DayPicker
+      captionLayout="dropdown-buttons"
+      locale={es}
+      mode="single"
+      defaultMonth={defaultDate}
+      fromMonth={defaultDate}
+      selected={selected}
+      onSelect={setSelected}
+      fromYear={2023}
+      toYear={2026}
+      footer={footer}
     />
   );
 }
+
+// "use client";
+// import { useState } from "react";
+// import DatePicker from "react-datepicker";
+
+// import "react-datepicker/dist/react-datepicker.css";
+
+// export default function PickDate() {
+//   const [fecha, setFecha] = useState(new Date());
+
+//   const handleFechaChange = (date: any) => {
+//     setFecha(date);
+//   };
+
+//   return (
+//     <DatePicker
+//       className="xl:ml-1 xl:w-[310px] xl:pl-2 "
+//       dateFormat="dd/MM/yyyy"
+//       selected={fecha}
+//       onChange={handleFechaChange}
+//       showYearDropdown
+//       yearDropdownItemNumber={4}
+//       scrollableYearDropdown
+//       placeholderText="Escriba o elija una fecha..."
+//       calendarClassName="flex items-center relative justify-center xl:ml-[800px] xl:w-1/4"
+//     />
+//   );
+// }
