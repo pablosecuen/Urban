8a073f617261e72e042f186370c472b47208f6d9
Benja <benyamin0714@gmail.com>
2023-05-10 15:00:34 -0300

    estilos

--- client/app/perfil/layout.tsx
+++ client/app/perfil/layout.tsx
@@ -9,7 +9,9 @@ const layoutPerfil = ({ children }: any) => {
       <div className="flex items-center justify-center pb-32 ">
         <div className="flex h-full flex-col items-center justify-center gap-5 px-4 lg:mt-20 lg:w-4/5 lg:flex-row xl:w-3/5">
           <Profile />
-          <div className="flex h-96 flex-col items-center justify-center">{children}</div>
+          <div className="flex h-[450px] flex-col items-center justify-center shadow-lg shadow-black/40">
+            {children}
+          </div>
         </div>
       </div>
       <Footer />
