b0396939dbd99e05459338cfc5a0d563a4437125
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-05-10 01:04:35 -0300

    ADD config de multer

--- api/src/routers/inputs/passage.ts
+++ api/src/routers/inputs/passage.ts
@@ -5,13 +5,19 @@ import {
   updatePassage,
   deletePassage,
 } from "../../controllers/inputs/passage";
+import multer from "multer";
 import { newAndUpdatePassageValidate } from "../../utils/validations/passage";
 
 const router = Router();
-
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB size limit
+  },
+});
 //Ruta para crear pasajes
 
-router.post("/", newAndUpdatePassageValidate, newPassage);
+router.post("/", upload.single("img"), newPassage);
 router.put("/:id", newAndUpdatePassageValidate, updatePassage);
 router.patch("/:id", enablePassage);
 router.delete("/:id", deletePassage);
