aeb653f4eb6e72fb2b65a8c0bdac01c9c97928c8
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-11 10:12:41 -0300

    cree 2 nav bar distintas para terminar el con el problema de hidratacion

--- client/app/home/layout.tsx
+++ client/app/home/layout.tsx
@@ -4,11 +4,15 @@ import logo from "../../assets/imagenes/UrbanIsoLogo.png";
 import Image from "next/image";
 
 import Menu from "@component/components/Menu/Menu";
+import { useMediaQuery } from "react-responsive";
+import NavBarMobile from "@component/components/NavBar/NavBarMobile";
 
 export default function Home({ children }: { children: React.ReactNode }) {
+  const isMobile = useMediaQuery({ query: "(max-width: 700px)" });
   return (
     <div>
-      <NavBar />
+      {isMobile ? <NavBarMobile /> : <NavBar />}
+
       <div className="mx-auto flex h-[92vh] flex-col items-center justify-center py-10 lg:container lg:flex-row lg:pb-20">
         <div className="flex w-full flex-col items-center justify-center gap-4">
           <Image
