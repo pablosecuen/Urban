389455a8699356fc04c1fb00a80d756be2d64456
AgustinChavero <agustin1996orellano@gmail.com>
2023-05-11 18:42:03 -0300

    Refactorizacion de Slice Vehicle

--- client/Redux/vehicle/vehicleSlice.ts
+++ client/Redux/vehicle/vehicleSlice.ts
@@ -1,39 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
-import {
-  getAllVehicles,
-  getVehicleById,
-  getVehicleByPatent,
-  getVehicleByChauffeurId,
-  getVehicleByOwner,
-  getVehicleByBrand,
-  getVehicleByYear,
-} from "./vehicleActions";
+import { getAllVehicles, getVehicleById, getVehicleByQuery } from "./vehicleActions";
 import { Vehicle } from "@component/app/types/Vehicle";
 
 interface VehicleState {
-  allVehicles: Vehicle[];
-  vehicle: Vehicle[];
+  allVehicles: Vehicle[] | null;
+  allVehiclesByQuery: Vehicle[] | null;
   vehicleById: Vehicle | null;
-  vehicleByPatent: Vehicle | null;
-  vehicleByChauffeurId: [] | {} | null;
-  vehicleByOwner: Vehicle[];
-  vehicleByBrand: Vehicle[];
-  vehicleByYear: Vehicle[];
-  userById: { [key: string]: Vehicle | undefined };
   status: "idle" | "loading" | "succeeded" | "failed";
-  error: string | null; // change any[] to your specific type
+  error: string | null;
 }
 
 const initialState: VehicleState = {
   allVehicles: [],
-  vehicle: [],
+  allVehiclesByQuery: [],
   vehicleById: null,
-  vehicleByPatent: null,
-  vehicleByChauffeurId: null,
-  vehicleByOwner: [],
-  vehicleByBrand: [],
-  vehicleByYear: [],
-  userById: {},
   status: "idle",
   error: null,
 };
@@ -44,63 +24,41 @@ const vehicleSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addMatcher(
-        (action) =>
-          [
-            getAllVehicles.pending,
-            getVehicleById.pending,
-            getVehicleByPatent.pending,
-            getVehicleByChauffeurId.pending,
-            getVehicleByOwner.pending,
-            getVehicleByBrand.pending,
-            getVehicleByYear.pending,
-          ].includes(action.type),
-        (state) => {
-          state.status = "loading";
-        }
-      )
-      .addMatcher(
-        (action) =>
-          [
-            getAllVehicles.fulfilled,
-            getVehicleByPatent.fulfilled,
-            getVehicleByOwner.fulfilled,
-            getVehicleByBrand.fulfilled,
-            getVehicleByYear.fulfilled,
-          ].includes(action.type),
-        (state, action) => {
-          state.status = "succeeded";
-          state.allVehicles = action.payload;
-        }
-      )
-      .addMatcher(
-        (action) => getVehicleById.fulfilled.match(action),
-        (state, action) => {
-          state.status = "succeeded";
-          state.vehicleById = action.payload;
-        }
-      )
-      .addMatcher(
-        (action) => getVehicleByChauffeurId.fulfilled.match(action),
-        (state, action) => {
-          state.status = "succeeded";
-          state.vehicleByChauffeurId = action.payload;
-        }
-      )
-      .addMatcher(
-        (action) =>
-          [
-            getAllVehicles.rejected,
-            getVehicleByPatent.rejected,
-            getVehicleByOwner.rejected,
-            getVehicleByBrand.rejected,
-            getVehicleByYear.rejected,
-          ].includes(action.type),
-        (state, action) => {
-          state.status = "failed";
-          state.error = action.payload as string;
-        }
-      );
+      .addCase(getAllVehicles.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(getAllVehicles.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.allVehicles = action.payload;
+      })
+      .addCase(getAllVehicles.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message ?? "Something went wrong";
+      });
+    builder
+      .addCase(getVehicleById.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(getVehicleById.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.vehicleById = action.payload;
+      })
+      .addCase(getVehicleById.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message ?? "Something went wrong";
+      });
+    builder
+      .addCase(getVehicleByQuery.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(getVehicleByQuery.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.allVehiclesByQuery = action.payload;
+      })
+      .addCase(getVehicleByQuery.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message ?? "Something went wrong";
+      });
   },
 });
 
