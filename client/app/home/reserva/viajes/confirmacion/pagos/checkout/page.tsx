f3e797a6479330b8f2b6e15be53159f08fdeb165
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-11 13:47:36 -0300

    modularize todo el codigo armando cards tipo componente para sacar toda logica de las page.tsx y que solo renderizen componentes, que ninguna sea "use client"

--- client/app/home/reserva/viajes/confirmacion/pagos/checkout/page.tsx
+++ client/app/home/reserva/viajes/confirmacion/pagos/checkout/page.tsx
@@ -1,17 +1,5 @@
+import CardCheckout from "@component/components/Cards/CardCheckout";
 
-import Image, { StaticImageData } from "next/image";
-import Link from "next/link";
-import success from "../../../../../../../assets/imagenes/success.png"
-
-
-export default function Checkout () {
-    return (
-        <div className="flex flex-col justify-center gap-8">
-            <p className="text-center text-2xl font-bold">Su pago ha sido realizado con éxito !</p>
-            <Image src={success} alt="pago exitoso" className="h-80 w-80 self-center"></Image>
-            <Link href="/home">
-            <button className="w-1/2 flex  mx-auto py-1 text-center justify-center">Volver al inicio</button>
-            </Link>
-        </div>
-    )
+export default function Checkout() {
+  return <CardCheckout />;
 }
