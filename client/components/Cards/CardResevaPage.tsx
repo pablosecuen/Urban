a9772c9361bb675ae20df29f07e6d1cc06646410
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-11 13:48:05 -0300

    modularize todo el codigo armando cards tipo componente para sacar toda logica de las page.tsx y que solo renderizen componentes, que ninguna sea "use client"

---
+++ client/components/Cards/CardResevaPage.tsx
@@ -0,0 +1,140 @@
+"use client";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Dispatch } from "redux";
+import { useRouter } from "next/navigation";
+
+import { HiOutlineLocationMarker, HiTag, HiTrendingUp, HiTrendingDown } from "react-icons/hi";
+// import { MdPets } from "react-icons/md";
+
+import { Query } from "@component/app/types/Passages";
+
+export default function CardReservaPage() {
+  const dispatch = useDispatch<Dispatch<any>>(); // idea de chatGPT
+  const router = useRouter();
+
+  const today = new Date().toISOString().slice(0, 10); // la fecha actual en formato YYYY-MM-DD
+
+  // - - - - - - - - - - - - - -  ESTADOS LOCALES - - - - - - - - - - - - - - -
+
+  const [origin, setOrigin] = useState<string>("");
+  const [destination, setDestination] = useState<string>("");
+  const [price, setPrice] = useState<number | undefined>();
+  const [departureDate, setDepartureDate] = useState<string>("");
+  const [arrivalDate, setArrivalDate] = useState<string>("");
+
+  const isFormValid = origin && destination && departureDate ? true : false;
+
+  // - - - - - - - - - - - - -  HANDLERS DE LOS INPUTS - - - - - - - - - - - - -
+
+  const handleOriginChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setOrigin(e.target.value);
+  };
+  const handleDestinationChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDestination(e.target.value);
+  };
+
+  /*   const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPrice(e.target.value);
+  };
+ */
+  const handleDepartureDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDepartureDate(e.target.value);
+  };
+
+  const handleArrivalDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setArrivalDate(e.target.value);
+  };
+
+  // - - - - - - - - - - - - -  HANDLE SUBMIT - - - - - - - - - - - - -
+  const handleSubmit = (e: any) => {
+    e.preventDefault(); // evitar el envio del formulario predeterminado
+
+    const query: Query = {
+      origin: origin.toLowerCase(),
+      destination: destination.toLowerCase(),
+      departureDate: departureDate.split("-").reverse().join("-"),
+      ...(arrivalDate && { arrivalDate: arrivalDate.split("-").reverse().join("-") }),
+      ...(price && { price }),
+      // armo la query y agrego las propiedades extras si las hay
+    };
+    const URL = Object.values(query).join("/");
+
+    router.push(`/home/reserva/${URL}`);
+  };
+
+  return (
+    <section className="mx-auto mt-5 w-11/12 rounded-3xl border-2 px-2 pt-10 shadow-2xl shadow-black/40 lg:mt-0 lg:h-[510px]">
+      <h1 className="px-8 text-center text-blue lg:px-0 lg:text-xl">
+        Llena el formulario para encontrar tu viaje
+      </h1>
+      <form className="flex flex-col items-center justify-center gap-5 pb-16 pt-12">
+        <div className="flex items-center justify-center">
+          <HiOutlineLocationMarker className="w-10 text-blue" />
+          <input
+            className="w-2/3 pl-2"
+            placeholder="Desde que lugar..."
+            type="text"
+            value={origin}
+            onChange={handleOriginChange}
+          />
+        </div>
+
+        <div className="flex items-center justify-center">
+          <HiOutlineLocationMarker className="w-10 text-blue" />
+          <input
+            className="w-2/3 pl-2"
+            placeholder="Hasta que lugar..."
+            type="text"
+            value={destination}
+            onChange={handleDestinationChange}
+          />
+        </div>
+
+        <div className="flex items-center justify-center">
+          <HiTrendingUp className="w-10 text-blue" />
+          <input
+            className="w-2/3 pl-2"
+            placeholder="Fecha de salida"
+            type="date"
+            value={departureDate}
+            min={today}
+            onChange={handleDepartureDateChange}
+          />
+        </div>
+        <div className="flex items-center justify-center">
+          <HiTrendingDown className="w-10 text-blue" />
+          <input
+            className="w-2/3 pl-2"
+            placeholder="Fecha de llegada"
+            type="date"
+            value={arrivalDate}
+            min={departureDate ? departureDate : today}
+            onChange={handleArrivalDateChange}
+          />
+        </div>
+
+        {/*      <div className="flex items-center justify-center">
+          <HiTag className="w-10 text-blue" />
+          <input
+            className="w-2/3 pl-2"
+            placeholder="Precio"
+            type="number"
+            value={price}
+            onChange={handlePriceChange}
+          />
+        </div> */}
+
+        <button
+          onClick={handleSubmit}
+          disabled={!isFormValid}
+          className={`transition_all w-1/2 self-center ${
+            !isFormValid ? "!bg-gray-500" : "cursor-pointer"
+          }`}
+        >
+          Buscar tu viaje!
+        </button>
+      </form>
+    </section>
+  );
+}
