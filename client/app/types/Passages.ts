1624860d6ddbb5a23d42bba3f074f8ba8131762b
httpfido <fidel.romano.2016@hotmail.com>
2023-05-11 20:27:41 -0300

    TYPES PASSAGES  restaurado

--- client/app/types/Passages.ts
+++ client/app/types/Passages.ts
@@ -9,21 +9,13 @@ export interface PassageToRegister {
   destination: string;
   description: string;
   departureDate: string;
-  departureTime: string;
   stock: number;
   arrivalDate: string;
   status: boolean;
-  img: string;
 }
 
 export interface PassageResponse {
-  price: number;
-  img: string;
-  imageUrl: string;
-  destination: string;
-  origin: string;
-  id: string;
-  passages?: PassageToRegister[];
+  passages: PassageToRegister[];
   totalPages: number;
 }
 
