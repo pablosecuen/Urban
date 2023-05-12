0bab0d2834366d6fc63dea79fd1d27763c32d4a9
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-12 11:36:08 -0300

    estilos

--- client/components/Cards/CardGestion.tsx
+++ client/components/Cards/CardGestion.tsx
@@ -11,6 +11,7 @@ import logo from "../../assets/imagenes/UrbanIsoLogo.png";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 import { Ticket } from "@component/app/types/Ticket";
+import { FaBus, FaCar, FaTaxi } from "react-icons/fa";
 
 export default function CardGestion() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -58,89 +59,89 @@ export default function CardGestion() {
   };
 
   return (
-    <div className="flex flex-col gap-2">
-      <h3 className="rounded-3xl p-4  text-left font-bold tracking-widest">Historial de viajes</h3>
-      <div className="flex h-[550px] items-center justify-center  rounded-3xl border-2 bg-slate-100  shadow-inner-lg  shadow-black/40 ">
-        <section className="container flex h-80 w-full flex-col gap-2 overflow-y-scroll rounded-3xl bg-transparent  lg:container lg:mx-auto  lg:h-[500px] lg:p-10">
-          {allTickets.map((ticket) => (
-            <div
-              key={ticket.id}
-              className=" my-2 flex flex-col rounded-3xl bg-white px-4 py-2 text-sm  shadow-xl shadow-black/30 md:text-base"
-            >
-              <div className="flex flex-row items-center justify-between">
-                <strong className="flex w-1/4 flex-col justify-start">
-                  {ticket.passageInfo.origin}
-                </strong>
-                <p className="flex w-1/4 justify-start text-center">hacia</p>
-                <strong className="flex w-1/4 flex-col justify-start">
-                  {ticket.passageInfo.destination}
-                </strong>
-                <div className="hidden w-1/4 flex-col justify-start sm:flex">
-                  {ticket.passageInfo.departureDate}
+    <div className=" mt-10 flex   flex-col gap-2 rounded-3xl border-2 p-6 shadow-2xl shadow-black/40 lg:h-[530px]">
+      <h2 className="rounded-3xl p-4  text-center text-xl font-semibold tracking-wider text-blue">
+        Historial de viajes
+      </h2>
+
+      <section className=" flex h-full w-full flex-col gap-2 overflow-y-scroll rounded-3xl bg-transparent  lg:container lg:mx-auto  lg:h-[500px] lg:p-10">
+        {allTickets.map((ticket) => (
+          <>
+            <div key={ticket.id} className="flex  items-center justify-center align-middle ">
+              <div className="flex flex-col">
+                {" "}
+                <div className="flex items-center gap-2 ">
+                  <FaBus size="30" className=" w-auto pr-2 text-blue" />
+                  <span className="text-xl font-semibold text-gray-600">Bus intermunicipal</span>
                 </div>
-                <button
-                  className="w-auto shadow-transparent"
-                  onClick={() => handleOpenModal(ticket)}
-                >
-                  Ticket
-                </button>
+                <span className="pt-2 text-gray-600">
+                  Origen: {ticket.passageInfo.origin} - {ticket.passageInfo.departureTime} -
+                  Estrella St <br />
+                  Tiempo estimado: {ticket.passageInfo.duration}
+                </span>
               </div>
-            </div>
-          ))}
-          {isModalOpen && (
-            <div
-              className="fixed inset-0 z-50 flex items-center justify-center"
-              onClick={handleModalClick}
-              ref={modalRef}
-            >
-              <div
-                className="mx-auto h-96 w-96 rounded-2xl bg-white shadow-2xl shadow-black/60"
-                onClick={(e) => e.stopPropagation()}
+
+              <button
+                className="w-auto shadow-transparent "
+                onClick={() => handleOpenModal(ticket)}
               >
-                {selectedTicket && (
-                  <article key={selectedTicket.id} className="p-6 ">
-                    <Image src={logo} alt="logo" className="mx-auto  w-16  py-4" />
-                    <h2 className="mb-4 text-center text-2xl font-bold">
-                      <>
-                        {selectedTicket.passageInfo.origin} to{" "}
-                        {selectedTicket.passageInfo.destination}
-                      </>
-                    </h2>
-                    <div className="grid grid-cols-2 gap-4 text-center">
-                      <div className="">
-                        <p className="mb-2 flex flex-col">
-                          <strong>Fecha de salida:</strong>{" "}
-                          {selectedTicket.passageInfo.departureDate}
-                        </p>
-                        <p className="mb-2 flex flex-col">
-                          <strong>Horario de salida:</strong>{" "}
-                          {selectedTicket.passageInfo.departureTime}
-                        </p>
-                        <p className="mb-2 flex flex-col">
-                          <strong>Duracion del viaje:</strong> {selectedTicket.passageInfo.duration}
-                        </p>
-                      </div>
-                      <div>
-                        <p className="mb-2 flex flex-col">
-                          <strong>Ticket ID:</strong> {selectedTicket.id}
-                        </p>
-                        <p className="mb-2 flex flex-col">
-                          <strong>Precio:</strong> ${selectedTicket.price}
-                        </p>
-                        <p className="mb-2 flex flex-col">
-                          <strong>Número de asiento:</strong>{" "}
-                          {selectedTicket.passageInfo.numberSeat}
-                        </p>
-                      </div>
-                    </div>
-                    <button onClick={saveAsPDF}>Guardar Ticket</button>
-                  </article>
-                )}
-              </div>
+                Ticket
+              </button>
             </div>
-          )}
-        </section>
-      </div>
+            <hr className="mb-4" />
+          </>
+        ))}
+        {isModalOpen && (
+          <div
+            className="fixed inset-0 z-50 flex items-center justify-center"
+            onClick={handleModalClick}
+            ref={modalRef}
+          >
+            <div
+              className="mx-auto h-96 w-96 rounded-2xl bg-white shadow-2xl shadow-black/60"
+              onClick={(e) => e.stopPropagation()}
+            >
+              {selectedTicket && (
+                <article key={selectedTicket.id} className="p-6 ">
+                  <Image src={logo} alt="logo" className="mx-auto  w-16  py-4" />
+                  <h2 className="mb-4 text-center text-2xl font-bold">
+                    <>
+                      {selectedTicket.passageInfo.origin} to{" "}
+                      {selectedTicket.passageInfo.destination}
+                    </>
+                  </h2>
+                  <div className="grid grid-cols-2 gap-4 text-center">
+                    <div className="">
+                      <p className="mb-2 flex flex-col">
+                        <strong>Fecha de salida:</strong> {selectedTicket.passageInfo.departureDate}
+                      </p>
+                      <p className="mb-2 flex flex-col">
+                        <strong>Horario de salida:</strong>{" "}
+                        {selectedTicket.passageInfo.departureTime}
+                      </p>
+                      <p className="mb-2 flex flex-col">
+                        <strong>Duracion del viaje:</strong> {selectedTicket.passageInfo.duration}
+                      </p>
+                    </div>
+                    <div>
+                      <p className="mb-2 flex flex-col">
+                        <strong>Ticket ID:</strong> {selectedTicket.id}
+                      </p>
+                      <p className="mb-2 flex flex-col">
+                        <strong>Precio:</strong> ${selectedTicket.price}
+                      </p>
+                      <p className="mb-2 flex flex-col">
+                        <strong>Número de asiento:</strong> {selectedTicket.passageInfo.numberSeat}
+                      </p>
+                    </div>
+                  </div>
+                  <button onClick={saveAsPDF}>Guardar Ticket</button>
+                </article>
+              )}
+            </div>
+          </div>
+        )}
+      </section>
     </div>
   );
 }
