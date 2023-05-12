83d496d1140e783806c4aa355a0dea84885b2c5e
httpfido <fidel.romano.2016@hotmail.com>
2023-05-12 02:46:52 -0300

    CardConfirmacionReserva logica

--- client/components/Cards/CardConfirmacionReserva.tsx
+++ client/components/Cards/CardConfirmacionReserva.tsx
@@ -1,11 +1,46 @@
 "use client";
-import { useSelector, useDispatch } from "react-redux";
 import Link from "next/link";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "@component/Redux/store/store";
+import { ThunkDispatch } from "redux-thunk";
+import { AnyAction } from "@reduxjs/toolkit"
+import { getPassagesId } from "@component/Redux/passage/passageActions";
+import { useEffect } from "react";
+;
 
-export default function CardConfirmacionReserva(id: {id: string}) {
+export default function CardConfirmacionReserva({id}: {id: string}) {
+  const dispatch: ThunkDispatch<RootState, undefined, AnyAction> = useDispatch();
+  const passage = useSelector((state: RootState) => state.passage.passageById);
+
+  useEffect(() => {
+    dispatch(getPassagesId(id))
+  },[])
+
   return (
     <div className="flex flex-col items-center justify-center gap-4 rounded-3xl border-2 py-4 shadow-2xl shadow-black/40">
-
+      <p className="lg:text-center lg:text-3xl lg:font-bold">Viaje Confirmado</p>
+      <div className="flex justify-center">
+        <p className="lg:text-center lg:text-3xl lg:font-bold">Nombre del conductor</p>
+        <p className=" lg:text-center lg:text-3xl">Luis martin</p>
+      </div>
+      <div className="flex justify-center">
+        <p className="lg:text-center lg:text-3xl lg:font-bold"> Hora de recogida</p>
+        <p className=" lg:text-center lg:text-3xl">3:00 horas</p>
+      </div>
+      <div className="flex justify-center">
+        <p className="lg:text-center lg:text-3xl lg:font-bold">Lugar de recogida</p>
+        <p className=" lg:text-center lg:text-3xl">la casa de ima</p>
+      </div>
+      <div className="flex justify-center">
+        <p className="lg:text-center lg:text-3xl lg:font-bold">Valor a pagar</p>
+        <p className=" lg:text-center lg:text-3xl">un toco de plata</p>
+      </div>
+      <p className="text-center text-gray-400">
+        tocando el boton para abonar el servicio aceptas nuestros terminos y condiciones de uso
+      </p>
+      <Link href="/home/reserva/viajes/confirmacion/pagos" className="flex justify-center">
+        <button className="mt-10 w-1/2">Ir a pagar</button>
+      </Link>
     </div>
   );
 }
