556006652b54bc5fbc7b248b4c29ca19d4480d95
httpfido <fidel.romano.2016@hotmail.com>
2023-05-12 03:01:15 -0300

    CardConfirmacionReserva logica

--- client/components/Cards/CardConfirmacionReserva.tsx
+++ client/components/Cards/CardConfirmacionReserva.tsx
@@ -16,24 +16,38 @@ export default function CardConfirmacionReserva({id}: {id: string}) {
     dispatch(getPassagesId(id))
   },[])
 
+  console.log(passage);
+  
   return (
     <div className="flex flex-col items-center justify-center gap-4 rounded-3xl border-2 py-4 shadow-2xl shadow-black/40">
-      <p className="lg:text-center lg:text-3xl lg:font-bold">Viaje Confirmado</p>
+      <p className="lg:text-center lg:text-3xl lg:font-bold">Pasaje disponible!</p>
       <div className="flex justify-center">
-        <p className="lg:text-center lg:text-3xl lg:font-bold">Nombre del conductor</p>
-        <p className=" lg:text-center lg:text-3xl">Luis martin</p>
+        <p className="lg:text-center lg:text-xl lg:font-bold">Nombre del conductor</p>
+        <p className=" lg:text-center lg:text-xl">Hardcodeado</p>
       </div>
       <div className="flex justify-center">
-        <p className="lg:text-center lg:text-3xl lg:font-bold"> Hora de recogida</p>
-        <p className=" lg:text-center lg:text-3xl">3:00 horas</p>
+        <p className="lg:text-center lg:text-xl lg:font-bold"> Hora de salida</p>
+        <p className=" lg:text-center lg:text-xl">{passage?.departureTime}</p>
       </div>
       <div className="flex justify-center">
-        <p className="lg:text-center lg:text-3xl lg:font-bold">Lugar de recogida</p>
-        <p className=" lg:text-center lg:text-3xl">la casa de ima</p>
+        <p className="lg:text-center lg:text-xl lg:font-bold">Origen</p>
+        <p className=" lg:text-center lg:text-xl">{passage?.origin}</p>
       </div>
       <div className="flex justify-center">
-        <p className="lg:text-center lg:text-3xl lg:font-bold">Valor a pagar</p>
-        <p className=" lg:text-center lg:text-3xl">un toco de plata</p>
+        <p className="lg:text-center lg:text-xl lg:font-bold">Destino</p>
+        <p className=" lg:text-center lg:text-xl">{passage?.destination}</p>
+      </div>
+      <div className="flex justify-center">
+        <p className="lg:text-center lg:text-xl lg:font-bold">Fecha de salida</p>
+        <p className=" lg:text-center lg:text-xl">{passage?.departureDate}</p>
+      </div>
+      <div className="flex justify-center">
+        <p className="lg:text-center lg:text-xl lg:font-bold">Fecha de llegada</p>
+        <p className=" lg:text-center lg:text-xl">{passage?.arrivalDate}</p>
+      </div>
+      <div className="flex justify-center">
+        <p className="lg:text-center lg:text-xl lg:font-bold">Valor a pagar</p>
+        <p className=" lg:text-center lg:text-xl">{passage?.price}</p>
       </div>
       <p className="text-center text-gray-400">
         tocando el boton para abonar el servicio aceptas nuestros terminos y condiciones de uso
