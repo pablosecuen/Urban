2c40267507bcd55870a2a37d9bb323d8021fd69b
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-13 16:23:33 -0300

    extraccion de semantica de las cards

--- client/app/home/reserva/viajes/confirmacion/page.tsx
+++ client/app/home/reserva/viajes/confirmacion/page.tsx
@@ -1,5 +1,6 @@
 import CardConfirmacionViajes from "@component/components/Cards/CardConfirmacionReserva";
 
-export default function Confirmacion() {
-  return <CardConfirmacionViajes id={""} />;
+export default function Confirmacion({ params }: { params: { id: string } }) {
+  const { id } = params;
+  return <CardConfirmacionViajes id={id} />;
 }
