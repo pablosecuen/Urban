3c6e94adbe56076811903b1bbdfba0c22fe65cac
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-13 16:23:42 -0300

    extraccion de semantica de las cards

--- client/components/Cards/CardViajesPage.tsx
+++ client/components/Cards/CardViajesPage.tsx
@@ -16,28 +16,19 @@ export default function CardViajesPage() {
   }
 
   return (
-    <div className="mx-auto h-full rounded-3xl p-10 shadow-2xl shadow-black/40 lg:ml-12 ">
-      <div className="flex flex-col gap-4 ">
-        <h1 className="text-center text-xl text-blue">
-          Estas son las mejores opciones encontradas
-        </h1>
-
-        {passages.map((passage) => (
-          <Link
-            href={`/home/reserva/viajes/${passage.id}`}
-            key={passage.id}
-            className="group flex items-center justify-between gap-4 rounded-full border bg-white px-10 py-2 transition-all duration-200 hover:border-blue"
-          >
-            <small className="font-bold capitalize text-blueGray-700 ">
-              {passage.origin} - {passage.destination}
-            </small>
-            <small>{passage.departureDate}</small>
-          </Link>
-        ))}
-      </div>
-      <div className="flex gap-2 pt-10">
-        <button>Ver mas opciones</button>
-      </div>
-    </div>
+    <>
+      {passages.map((passage) => (
+        <Link
+          href={`/home/reserva/viajes/${passage.id}`}
+          key={passage.id}
+          className="group flex items-center justify-between gap-4 rounded-full border bg-white px-10 py-2 transition-all duration-200 hover:border-blue"
+        >
+          <small className="font-bold capitalize text-blueGray-700 ">
+            {passage.origin} - {passage.destination}
+          </small>
+          <small>{passage.departureDate}</small>
+        </Link>
+      ))}
+    </>
   );
 }
