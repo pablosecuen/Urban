3623d05a9ba18309cfa04ebfda300b91a3265596
httpfido <fidel.romano.2016@hotmail.com>
2023-05-14 16:48:02 -0300

    /paqueteria page extraccion de semantica

--- client/app/home/paqueteria/page.tsx
+++ client/app/home/paqueteria/page.tsx
@@ -1,5 +1,12 @@
 import CardPaqueteriaPage from "@component/components/Cards/CardPaqueteriaPage";
 
 export default function Paqueteria() {
-  return <CardPaqueteriaPage />;
+  return (
+    <section className="mx-auto mb-8 w-4/5 rounded-3xl border-2 shadow-2xl shadow-black/40 lg:mb-0">
+    <h1 className="mt-12 text-center text-xl text-blue">
+      Llena el formulario para encontrar tu cadete
+    </h1>
+  <CardPaqueteriaPage />
+  </section>
+  );
 }
