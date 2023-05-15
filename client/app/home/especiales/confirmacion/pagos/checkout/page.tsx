95a9bf070ced8344ea9cd5a58d8412125b7fae4b
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-05-15 09:49:31 -0300

    fix import

--- client/app/home/especiales/confirmacion/pagos/checkout/page.tsx
+++ client/app/home/especiales/confirmacion/pagos/checkout/page.tsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
-import success from "../../../../../../../assets/imagenes/success.png";
+import success from "../../../../../../assets/imagenes/success.png";
 import { useState } from "react";
 //import CardCheckout from "@component/components/Cards/CardCheckout";
 
