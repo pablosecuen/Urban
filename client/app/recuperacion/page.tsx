263127cc188b0b6862ce069c1491ed73ebf241b5
Benja <benyamin0714@gmail.com>
2023-05-15 17:07:41 -0300

    creacion Ruta de recuperacion password

---
+++ client/app/recuperacion/page.tsx
@@ -0,0 +1,9 @@
+import FormPassword from "../../components/FormPassword/FormPassword";
+
+export default function Recuperacion() {
+  return (
+    <div>
+      <FormPassword />
+    </div>
+  );
+}
