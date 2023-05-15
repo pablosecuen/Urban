17562a4c772c2b545bdf835dac0292ee894d6ad3
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-05-15 14:21:35 -0300

    incorporacion head SEO ejemplo

--- client/app/home/gestion/page.tsx
+++ client/app/home/gestion/page.tsx
@@ -1,17 +1,25 @@
 import CardGestion from "@component/components/Cards/CardGestion";
-
+import Head from "next/head";
 // Traigo todos los viajes del usuario
 
 export default function Gestion() {
   return (
-    <div className=" mt-10 flex   flex-col gap-2 rounded-3xl border-2 p-6 shadow-2xl shadow-black/40 lg:h-[530px]">
-      <h2 className="rounded-3xl p-4  text-center text-xl font-semibold tracking-wider text-blue">
-        Historial de viajes
-      </h2>
+    <>
+      <head>
+        <title>Urban | Gestion de viajes</title>
+        <meta name="description" content=" Gestion de viajes" />
+        <meta name="keywords" content="" />
+      </head>
 
-      <section className=" flex h-full w-full flex-col gap-2 overflow-y-scroll rounded-3xl bg-transparent  lg:container lg:mx-auto  lg:h-[500px] lg:p-10">
-        <CardGestion />
-      </section>
-    </div>
+      <div className=" mt-10 flex   flex-col gap-2 rounded-3xl border-2 p-6 shadow-2xl shadow-black/40 lg:h-[530px]">
+        <h2 className="rounded-3xl p-4  text-center text-xl font-semibold tracking-wider text-blue">
+          Historial de viajes
+        </h2>
+
+        <section className=" flex h-full w-full flex-col gap-2 overflow-y-scroll rounded-3xl bg-transparent  lg:container lg:mx-auto  lg:h-[500px] lg:p-10">
+          <CardGestion />
+        </section>
+      </div>
+    </>
   );
 }
