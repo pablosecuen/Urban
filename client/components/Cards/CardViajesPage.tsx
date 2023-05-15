964143abdd62717471a8dccb9316b23e1976d8c1
httpfido <fidel.romano.2016@hotmail.com>
2023-05-15 10:13:30 -0300

    SkeletonCard para /reservas/slug

--- client/components/Cards/CardViajesPage.tsx
+++ client/components/Cards/CardViajesPage.tsx
@@ -5,14 +5,16 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@component/Redux/store/store";
 import { ThunkDispatch } from "redux-thunk";
 import { AnyAction } from "@reduxjs/toolkit";
+import slugPassageSkeleton from "../Skeleton/slugPassageSkeleton"
 
 export default function CardViajesPage() {
   const passages = useSelector((state: RootState) => state.passage.allPassagesByQuery);
   const status = useSelector((state: RootState) => state.passage.status);
   const error = useSelector((state: RootState) => state.passage.error);
+console.log(status);
 
   if (status === "loading") {
-    return <div>Loading...</div>;
+    return (<span>loadin</span>);
   }
 
   return (
