37fb9631f105f13a1c83439e46b7a015d0342d19
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-05-15 14:51:46 -0300

    Metadata incorporada en gestion de viajes

--- client/app/home/gestion/page.tsx
+++ client/app/home/gestion/page.tsx
@@ -1,25 +1,24 @@
 import CardGestion from "@component/components/Cards/CardGestion";
-import Head from "next/head";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Urban | Historial de viajes",
+  description: "Historial de viajes del usuario en la que puede ver los viajes que ha realizado",
+  keywords: "urban, historial, viajes, usuario,  gestion de viajes, gestion de viajes del usuario",
+};
+
 // Traigo todos los viajes del usuario
 
 export default function Gestion() {
   return (
-    <>
-      <head>
-        <title>Urban | Gestion de viajes</title>
-        <meta name="description" content=" Gestion de viajes" />
-        <meta name="keywords" content="" />
-      </head>
+    <div className=" mt-10 flex   flex-col gap-2 rounded-3xl border-2 p-6 shadow-2xl shadow-black/40 lg:h-[530px]">
+      <h2 className="rounded-3xl p-4  text-center text-xl font-semibold tracking-wider text-blue">
+        Historial de viajes
+      </h2>
 
-      <div className=" mt-10 flex   flex-col gap-2 rounded-3xl border-2 p-6 shadow-2xl shadow-black/40 lg:h-[530px]">
-        <h2 className="rounded-3xl p-4  text-center text-xl font-semibold tracking-wider text-blue">
-          Historial de viajes
-        </h2>
-
-        <section className=" flex h-full w-full flex-col gap-2 overflow-y-scroll rounded-3xl bg-transparent  lg:container lg:mx-auto  lg:h-[500px] lg:p-10">
-          <CardGestion />
-        </section>
-      </div>
-    </>
+      <section className=" flex h-full w-full flex-col gap-2 overflow-y-scroll rounded-3xl bg-transparent  lg:container lg:mx-auto  lg:h-[500px] lg:p-10">
+        <CardGestion />
+      </section>
+    </div>
   );
 }
