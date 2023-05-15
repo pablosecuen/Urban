d29283c46cef69f2f59882ae54ec4e67a2292f74
Benja <benyamin0714@gmail.com>
2023-05-15 18:27:04 -0300

    page de recuperacion contrasenia

--- client/app/recuperacion/page.tsx
+++ client/app/recuperacion/page.tsx
@@ -2,7 +2,8 @@ import FormPassword from "../../components/FormPassword/FormPassword";
 
 export default function Recuperacion() {
   return (
-    <div>
+    <div className="flex h-screen flex-col items-center justify-center gap-2">
+      <h1 className="text-center text-3xl font-semibold text-blue">Cambia tu contraseña</h1>
       <FormPassword />
     </div>
   );
