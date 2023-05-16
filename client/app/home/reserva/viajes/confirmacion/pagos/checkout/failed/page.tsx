74b1ae1b7e3663f232da6557e4438c15ab02d971
Benja <benyamin0714@gmail.com>
2023-05-16 18:14:13 -0300

    responsive 2xl

--- client/app/home/reserva/viajes/confirmacion/pagos/checkout/failed/page.tsx
+++ client/app/home/reserva/viajes/confirmacion/pagos/checkout/failed/page.tsx
@@ -4,12 +4,9 @@ import Image from "next/image";
 export default function Failed() {
   return (
     <div className="flex flex-col items-center justify-center lg:ml-5 2xl:ml-14">
-      <Image src={jpg} alt="jpg" className="h-96 w-96" />
-      <p className="text-center font-mono text-xl font-semibold text-red-500">
-        Hubo un problema con el pago, lo sentimos
-      </p>
-      <p className="text-center font-mono text-xl font-semibold text-red-500">
-        Intentelo de nuevo mas tarde!
+      <Image src={jpg} alt="jpg" className="h-96 w-96 2xl:h-[450px] 2xl:w-[450px]" />
+      <p className="text-center font-mono text-xl font-semibold text-red-500 2xl:text-3xl">
+        Hubo un problema con el pago, lo sentimos. Intentelo de nuevo mas tarde!
       </p>
     </div>
   );
