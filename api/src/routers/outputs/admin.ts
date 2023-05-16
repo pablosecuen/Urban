827e2d1e6fc3c7fd01d7ce1432ba0942cd787281
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-05-16 14:10:48 -0300

    ruta getUserRecords

--- api/src/routers/outputs/admin.ts
+++ api/src/routers/outputs/admin.ts
@@ -5,6 +5,7 @@ import {
   getInactiveDeliverys,
   getProfit,
   getGrossIncome,
+  getUserRecords,
 } from "../../controllers/outputs/admin";
 
 const router = Router();
@@ -12,6 +13,7 @@ const router = Router();
 router.get("/status", getAdminState);
 router.get("/profit", getProfit);
 router.get("/grossIncome", getGrossIncome);
+router.get("/userRecords", getUserRecords);
 router.get("/chaufeur", getInactiveChauffeur);
 router.get("/deliverys", getInactiveDeliverys);
 
