efaca5f56ae7f20c1619055e0891e8693d28a97f
Benja <benyamin0714@gmail.com>
2023-05-16 19:08:30 -0300

    Falta un poco de logica

--- client/components/FormPassword/FormPassword.tsx
+++ client/components/FormPassword/FormPassword.tsx
@@ -6,9 +6,44 @@ import { useRouter } from "next/navigation";
 import setValidate from "./Validate";
 import Link from "next/link";
 
+interface UserToPassword {
+  email: string;
+  password: string;
+}
+
+interface LoginResponse {
+  success: boolean;
+  message?: string;
+  token?: string;
+}
+
+interface PasswordError {
+  messageEmail?: string;
+  messagePassword?: string;
+}
+
 export default function FormPassword() {
   const [showPassword1, setShowPassword1] = useState(false);
   const [showPassword2, setShowPassword2] = useState(false);
+  const [userData, setUserData] = useState<UserToPassword>({} as UserToPassword);
+  const [errores, setErrores] = useState<PasswordError>({} as PasswordError);
+  const router = useRouter();
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.toString();
+    const name = e.target.name.toString();
+
+    setUserData({ ...userData, [name]: value });
+    setErrores({ ...errores, ...setValidate({ [name]: value }) });
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name.toString();
+    const value = e.target.value.toString();
+    setUserData({ ...userData, [name]: value });
+    const { password } = userData;
+    setErrores({ ...errores, ...setValidateRepeatPassword(value, password) });
+  };
 
   const visiblePassword = (e) => {
     e.preventDefault();
@@ -22,7 +57,7 @@ export default function FormPassword() {
 
   return (
     <form
-      //   onSubmit={handleLogin}
+      onSubmit={handlePasswordChange}
       className="flex h-1/2 w-1/3 items-center justify-center rounded-lg border-2 px-20 text-lg shadow-xl shadow-black/40"
     >
       <div className="flex h-full flex-col justify-center gap-8">
@@ -30,7 +65,20 @@ export default function FormPassword() {
           <label htmlFor="" className="text-center">
             Email:
           </label>
-          <input type="text" name="email" className="pl-2" />
+          <input
+            type="email"
+            name="email"
+            value={userData.email}
+            onChange={handleInputChange}
+            className={`pl-2 ${errores.messageEmail && "focus-visible:outline-red-500"}`}
+          />
+          <small
+            className={`pl-2 ${
+              errores.messageEmail ? "opacity-100" : "opacity-0"
+            } -bottom-6 left-0 text-left font-medium text-red-500`}
+          >
+            {errores.messageEmail}
+          </small>
         </div>
         <div className="flex flex-col items-center gap-2">
           <label className="text-center">Contraseña:</label>
@@ -38,9 +86,11 @@ export default function FormPassword() {
             <input
               type={showPassword1 ? "text" : "password"}
               name="password"
-              className="pl-2"
-              // value={userData.password}
-              // onChange={handleInputChange}
+              value={userData.password}
+              onChange={handleInputChange}
+              className={`transition_all px-1 text-black ${
+                errores.messagePassword && " focus-visible:outline-red-500"
+              }`}
             />
             <button
               onClick={visiblePassword}
@@ -48,24 +98,26 @@ export default function FormPassword() {
             >
               {!showPassword1 ? <HiEyeOff className="w-full" /> : <HiEye className="w-full" />}
             </button>
+            <small
+              className={`transition_all text-right ${
+                errores.messagePassword ? "opacity-100" : "opacity-0"
+              } absolute -bottom-6 left-0 text-left font-medium text-red-500`}
+            >
+              {errores.messagePassword}
+            </small>
           </div>
-          {/* <small
-            className={`transition_all text-right ${
-              errores.messagePassword ? "opacity-100" : "opacity-0"
-            } absolute -bottom-6 left-0 text-left font-medium text-red-500`}
-          >
-            {errores.messagePassword}
-          </small> */}
         </div>
         <div className="flex flex-col items-center gap-2">
           <label className="text-center">Confirme la contraseña:</label>
           <div className="relative flex">
             <input
               type={showPassword2 ? "text" : "password"}
-              name="password"
-              className="pl-2"
-              // value={userData.password}
-              // onChange={handleInputChange}
+              name="repeatPassword"
+              value={userData.repeatPassword}
+              onChange={handlePasswordChange}
+              className={`px-1 text-black ${
+                errores.messageRepeatPassword ? "border-red-500 focus-visible:outline-red-500" : ""
+              }`}
             />
             <button
               onClick={visiblePassword2}
@@ -73,17 +125,28 @@ export default function FormPassword() {
             >
               {!showPassword2 ? <HiEyeOff className="w-full" /> : <HiEye className="w-full" />}
             </button>
+            <div className="absolute -bottom-6 left-0 text-right">
+              <small
+                className={`transition_all  text-right ${
+                  errores.messageRepeatPassword ? "opacity-100" : "opacity-0"
+                }  font-medium text-red-500`}
+              >
+                {errores.messageRepeatPassword}
+              </small>
+              <small
+                className={`transition_all  text-right ${
+                  errores.messageRepeatPassword ? "opacity-0" : "opacity-100"
+                }  font-medium text-emerald-500`}
+              >
+                {errores.messageRepeatPassword == "" &&
+                  userData.repeatPassword.length > 0 &&
+                  "Las contraseñas coinciden"}
+              </small>
+            </div>
           </div>
-          {/* <small
-            className={`transition_all text-right ${
-              errores.messagePassword ? "opacity-100" : "opacity-0"
-            } absolute -bottom-6 left-0 text-left font-medium text-red-500`}
-          >
-            {errores.messagePassword}
-          </small> */}
         </div>
         <div>
-          <button>Guardar contraseña</button>
+          <button>Enviar contraseña</button>
         </div>
         <small>
           Quieres volver al inicio?{" "}
