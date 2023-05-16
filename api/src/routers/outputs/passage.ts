9affb0f2e36c51444551e320039dab9d91681150
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-05-16 16:12:55 -0300

    ADD get locartions rutas

--- api/src/routers/outputs/passage.ts
+++ api/src/routers/outputs/passage.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { getAllPassages, getPassageById } from "../../controllers/outputs/passage";
+import { getAllPassages, getLocations, getPassageById } from "../../controllers/outputs/passage";
 
 const router = Router();
 
 //Ruta para crear bondis
 
 router.get("/", getAllPassages);
+
+router.get("/locations", getLocations);
+
 router.get("/:id", getPassageById);
 
 export default router;
