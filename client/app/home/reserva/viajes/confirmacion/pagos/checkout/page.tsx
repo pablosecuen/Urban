e9b672d4771c654241362522060084693578b3e8
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-05-16 11:34:27 -0300

    funcion de guardado de tickets postpago

--- client/app/home/reserva/viajes/confirmacion/pagos/checkout/page.tsx
+++ client/app/home/reserva/viajes/confirmacion/pagos/checkout/page.tsx
@@ -13,7 +13,7 @@ export default function Checkout() {
   const paymentId: string | null = queryParams.get("payment_id"); //id del usuario de mp
   const merchantOrder: string | null = queryParams.get("merchant_order_id"); //codigo factura
   const status: string | null = queryParams.get("status"); //estado de exito o no
-  const userId: string | null = "";
+  const user: any | null = JSON.parse(localStorage.getItem("user") || ""); //id del usuario de la app;
 
   // - - - - - - - - - - - - NOTIFICACIONES - - - - - - - - - - - -
   const notifySuccess = () =>
@@ -28,24 +28,20 @@ export default function Checkout() {
       theme: "light",
     });
 
-  useEffect(() => {
-    notifySuccess();
-  }, []);
-
   const getToken = async () => {
     try {
-      const { data } = await axios.get(`/payment/merchantOrder?merchantOrder=${merchantOrder}`);
+      const { data } = await axios.get(
+        `http://localhost:3000/payment/merchantOrder?merchantOrder=${merchantOrder}`
+      );
       //este axios pueeeede llegar a ser a mercadopago
       //aca se guarda la info y con esto generamos la factura
+
       const requestData = {
-        userId: userId,
+        userId: user?.id,
         /// reemplazar la variable objeto por data
-        products: data.items.map((product: any) => {
-          return {
-            id: product.id,
-            unitPrice: product.unit_price,
-          };
-        }),
+        passageId: data?.items[0].id,
+        price: data?.items[0].unit_price,
+        quantity: data?.items[0].quantity,
         paymentId: paymentId,
         merchantOrder: merchantOrder,
         statusMp: status,
@@ -55,9 +51,18 @@ export default function Checkout() {
       return error;
     }
   };
+
   useEffect(() => {
+    (async () => {
+      const ticket = await getToken();
+      console.log(ticket);
+      axios.post("http://localhost:3000/ticket", ticket);
+    })();
+
+    notifySuccess();
     //post con el endpoint del ticket usando requestData
-  });
+  }, []);
+
   return (
     <div className="flex flex-col justify-center gap-8">
       {/* <p className="text-center text-2xl font-bold">Su pago ha sido realizado con éxito !</p> */}
