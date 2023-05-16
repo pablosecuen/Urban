1c65761145452203304d1799aec588070a4b87e1
Benja <benyamin0714@gmail.com>
2023-05-16 18:11:28 -0300

    responsive para lg y xl

--- client/app/home/reserva/viajes/confirmacion/pagos/checkout/failed/page.tsx
+++ client/app/home/reserva/viajes/confirmacion/pagos/checkout/failed/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Failed() {
   return (
-    <div className="flex flex-col items-center justify-center 2xl:ml-14">
+    <div className="flex flex-col items-center justify-center lg:ml-5 2xl:ml-14">
       <Image src={jpg} alt="jpg" className="h-96 w-96" />
       <p className="text-center font-mono text-xl font-semibold text-red-500">
         Hubo un problema con el pago, lo sentimos
