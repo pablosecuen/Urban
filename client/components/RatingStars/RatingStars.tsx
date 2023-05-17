2cfc21063978c240d5d0ceb41c045c550ae48782
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-17 10:06:18 -0300

    retoques de estilos

--- client/components/RatingStars/RatingStars.tsx
+++ client/components/RatingStars/RatingStars.tsx
@@ -9,11 +9,11 @@ export default function RatingStars({ onClickFunction, stateValue }: RatingStars
   const enableColor = "#FEBC0B";
   const disabledColor = "gray";
   return (
-    <div className="mb-4 flex">
+    <div className="mx-auto mb-4 flex w-2/3 items-center justify-self-center ">
       <label htmlFor="q1" onClick={() => onClickFunction(1)}>
         <RatingStar
           title="1"
-          className="w-10"
+          className="w-10 cursor-pointer"
           color={stateValue >= 1 ? enableColor : disabledColor}
         />
       </label>
@@ -22,7 +22,7 @@ export default function RatingStars({ onClickFunction, stateValue }: RatingStars
       <label htmlFor="q2" onClick={() => onClickFunction(2)}>
         <RatingStar
           title="2"
-          className="w-10"
+          className="w-10  cursor-pointer"
           color={stateValue >= 2 ? enableColor : disabledColor}
         />
       </label>
@@ -31,7 +31,7 @@ export default function RatingStars({ onClickFunction, stateValue }: RatingStars
       <label htmlFor="q3" onClick={() => onClickFunction(3)}>
         <RatingStar
           title="3"
-          className="w-10"
+          className="w-10 cursor-pointer"
           color={stateValue >= 3 ? enableColor : disabledColor}
         />
       </label>
@@ -40,7 +40,7 @@ export default function RatingStars({ onClickFunction, stateValue }: RatingStars
       <label htmlFor="q4" onClick={() => onClickFunction(4)}>
         <RatingStar
           title="4"
-          className="w-10"
+          className="w-10 cursor-pointer"
           color={stateValue >= 4 ? enableColor : disabledColor}
         />
       </label>
@@ -49,7 +49,7 @@ export default function RatingStars({ onClickFunction, stateValue }: RatingStars
       <label htmlFor="q5" onClick={() => onClickFunction(5)}>
         <RatingStar
           title="5"
-          className="w-10"
+          className="w-10 cursor-pointer"
           color={stateValue >= 5 ? enableColor : disabledColor}
         />
       </label>
