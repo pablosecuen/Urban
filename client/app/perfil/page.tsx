4ddd3509c6f9c47c6c094881c2edf163310101e5
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-17 00:10:10 -0300

    stilos, put, y render de usuario actualizado en perfil

--- client/app/perfil/page.tsx
+++ client/app/perfil/page.tsx
@@ -1,5 +1,21 @@
 import CardPerfilPage from "@component/components/Cards/CardPerfilPage";
+import Footer from "@component/components/Footer/Footer";
+import NavBar from "@component/components/NavBar/NavBar";
+import Profile from "@component/components/Profile/Profile";
 
 export default function Perfil() {
-  return <CardPerfilPage />;
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center justify-center">
+      <NavBar />
+      <div className=" container flex min-h-[90vh] flex-col items-center justify-center gap-6 py-10 xl:flex-row ">
+        <section className="xl:w-1/3">
+          <Profile />
+        </section>
+        <section className="xl:w-2/3">
+          <CardPerfilPage />
+        </section>
+      </div>
+      <Footer />
+    </div>
+  );
 }
