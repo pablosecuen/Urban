41e7bfbc919160db5266dbfa6517b8c8e7f998ec
Benja <benyamin0714@gmail.com>
2023-05-18 17:14:01 -0300

    estilado mobile

--- client/app/home/reserva/[id]/page.tsx
+++ client/app/home/reserva/[id]/page.tsx
@@ -12,14 +12,17 @@ export const metadata: Metadata = {
 export default function Confirmacion({ params }: { params: { id: string } }) {
   const { id } = params;
   return (
-    <div className="flex flex-col items-center justify-center gap-4 rounded-3xl border-2 py-4 shadow-2xl shadow-black/40">
-      <h3 className="lg:text-center lg:text-3xl lg:font-bold">Pasaje disponible!</h3>
+    <div className="flex w-5/6 flex-col items-center justify-center gap-2 rounded-3xl border-2 border-gray-300 py-4 shadow-xl shadow-black/40">
+      <h3 className="text-center font-bold lg:text-3xl 2xl:bg-black">Pasaje disponible!</h3>
       <CardConfirmacionViajes id={id} />
-      <p className="w-4/5 text-center text-gray-400">
-        tocando el boton para abonar el servicio aceptas nuestros terminos y condiciones de uso
+      <p className="w-11/12 text-center text-xs text-gray-400 2xl:bg-blue">
+        Tocando el boton para abonar el servicio aceptas nuestros terminos y condiciones de uso
       </p>
-      <Link href="/home/reserva/viajes/confirmacion/pagos" className="flex justify-center">
-        <button className="mt-10 w-1/2">Ir a pagar</button>
+      <Link
+        href="/home/reserva/viajes/confirmacion/pagos"
+        className="flex justify-center 2xl:bg-black"
+      >
+        <button className="w-2/5 text-sm">Ir a pagar</button>
       </Link>
     </div>
   );
