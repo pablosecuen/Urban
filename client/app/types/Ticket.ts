1e33849f0180e1ce1e30ab84234f9af224843f44
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-05-18 13:56:15 -0300

    Ticket type - reviewSent prop added

--- client/app/types/Ticket.ts
+++ client/app/types/Ticket.ts
@@ -1,16 +1,16 @@
 import { TicketStatus } from "@component/../api/src/types/types";
 import { Passage } from "./Passages";
 
-
 export interface TicketToRegister {
   userId: string;
   passageId: string;
-  price: number
+  price: number;
 }
 
 export interface Ticket extends TicketToRegister {
   passageInfo: Passage;
   id: string;
+  reviewSent: Boolean;
   status: TicketStatus;
   createdAt: string;
   updatedAt: string;
