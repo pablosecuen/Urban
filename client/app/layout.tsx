a377045a16ecdff14a53d8977eb2ea8aa1bcba96
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-05-19 11:44:42 -0300

    fix main

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="max-w-screen-4xl mx-auto flex h-full flex-col items-center justify-center sm:flex-row ">
+        <main className="">
           <Providers>{children}</Providers>
         </main>
       </body>
