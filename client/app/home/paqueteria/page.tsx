408fcd814bfba905968284a9204dba9ce039b4c4
httpfido <fidel.romano.2016@hotmail.com>
2023-05-21 20:35:56 -0300

    /paqueteria react-select

--- client/app/home/paqueteria/page.tsx
+++ client/app/home/paqueteria/page.tsx
@@ -1,9 +1,17 @@
 import CardPaqueteriaPage from "@component/components/Cards/CardPaqueteriaPage";
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Urban | Reserva",
+  description:
+    "En nuestra página de gestión, puedes planificar tus viajes de manera sencilla y conveniente. Ingresa tu destino, origen y fechas de salida y regreso, y descubre una amplia selección de opciones de transporte. Nuestra plataforma te brinda la información necesaria para que tomes decisiones informadas y elijas la mejor opción para tu viaje. Ya sea que estés buscando buses intermunicipales, taxis públicos o transportes privados, nuestra página de gestión te ayuda a encontrar soluciones de transporte adaptadas a tus necesidades. Optimiza tus días y disfruta de una experiencia de viaje fluida y placentera con nuestra herramienta de gestión.",
+  keywords:
+    "gestión de viajes, buscar viajes, destino, origen, fecha de salida, fecha de regreso, opciones de transporte, buses intermunicipales, taxis públicos, transportes privados, planificar viaje, tomar decisiones informadas, optimizar viajes, experiencia de viaje.",
+};
 export default function Paqueteria() {
   return (
-    <section className="mx-auto mb-8 w-4/5 rounded-3xl border-2 shadow-2xl shadow-black/40 lg:mb-0">
-    <h1 className="mt-12 text-center text-xl text-blue">
+    <section className="mx-auto mt-5 w-11/12 rounded-3xl border-2 px-2 pt-10 shadow-2xl shadow-black/40 lg:mt-0 lg:h-[510px]">
+    <h1 className="px-8 text-center text-blue lg:px-0 lg:text-xl">
       Llena el formulario para encontrar tu cadete
     </h1>
   <CardPaqueteriaPage />
