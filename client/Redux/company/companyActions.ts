cc6859558470b5134a8d3faa5c183ed079bd1e3c
AgustinChavero <agustin1996orellano@gmail.com>
2023-05-22 13:22:04 -0300

    Action de Company

---
+++ client/Redux/company/companyActions.ts
@@ -0,0 +1,12 @@
+import { Company } from "@component/app/types/Company";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios, { AxiosResponse } from "axios";
+//Hay que refactorizar el codigo, tiene que haber actions unicamente para GetAll, GetById, y GetByQueryParams
+
+export const getAllCompanies = createAsyncThunk<Company[], void>(
+  "companies/getAllCompanies",
+  async () => {
+    const response = await axios.get(`http://localhost:3000/company`);
+    return response.data.buses;
+  }
+);
