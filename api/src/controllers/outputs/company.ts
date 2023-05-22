07f3e7dbd6fc802fa8197ee9ba23eb178e259578
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-05-22 18:13:01 -0300

    Refactor de errors en controller input copmany

--- api/src/controllers/outputs/company.ts
+++ api/src/controllers/outputs/company.ts
@@ -1,11 +1,20 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { db } from "../../connection/connection";
+import createHttpError from "http-errors";
 
-export const getCompanies = async (req: Request, res: Response): Promise<void> => {
+export const getCompanies = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
   try {
     let query: any = db.collection("companies");
 
     const companySnap = await query.get();
+
+    if (companySnap.empty) {
+      throw createHttpError(404, "No se encontraron companias");
+    }
 
     const page = Number(req.query.page) || 1;
     const pageSize = Number(req.query.pageSize) || 2;
@@ -21,24 +30,26 @@ export const getCompanies = async (req: Request, res: Response): Promise<void> =
     });
 
     res.status(200).json({ companies, totalPages });
-  } catch (innerError) {
-    console.error("Error al encontrar los buses", innerError);
-    res.status(400).json({ message: innerError.message });
+  } catch (error) {
+    next(error);
   }
 };
 
-export const getCompanyByName = async (req: Request, res: Response): Promise<void> => {
+export const getCompanyByName = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
   try {
     const id: string = req.params.id;
     const doc = await db.collection("companies").doc(id).get();
     if (!doc.exists) {
-      res.status(404).json({ message: "Compañia no encontrado" });
+      throw createHttpError(404, "No se encontro la compania");
     } else {
       const company = { id: doc.id, ...doc.data() };
       res.json(company);
     }
   } catch (error) {
-    console.error("Error al obtener el compañia", error);
-    res.status(500).json({ message: "Error al obtener el compañia" });
+    next(error);
   }
 };
