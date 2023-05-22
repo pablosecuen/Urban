a4d2e4f36ca955b7ad6a30845540f8614622b93d
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-05-22 18:10:46 -0300

    Refactor de errors en controller input company

--- api/src/controllers/inputs/company.ts
+++ api/src/controllers/inputs/company.ts
@@ -1,8 +1,14 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { db } from "../../connection/connection";
 import { Company } from "../../schema/company";
+import { errorMonitor } from "nodemailer/lib/xoauth2";
+import createHttpError from "http-errors";
 
-export const newCompany = async (req: Request, res: Response): Promise<void> => {
+export const newCompany = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
   try {
     const data: Company = req.body;
     const dataFormated = {
@@ -13,30 +19,30 @@ export const newCompany = async (req: Request, res: Response): Promise<void> => 
     const docRef = await db.collection("companies").add(dataFormated);
 
     res.status(200).json({ message: "Compañia creado correctamente", id: docRef.id });
-  } catch (innerError) {
-    console.error("Error al crear el compañia", innerError);
-    res.status(400).json({ message: innerError.message });
+  } catch (error) {
+    next(error);
   }
 };
 
-export const updateCompany = async (req: Request, res: Response): Promise<void> => {
+export const updateCompany = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
   try {
     const id: string = req.params.id;
     const data = req.body;
     const updatedAt: string = new Date(Date.now()).toISOString();
     const docRef = await db.collection("companies").doc(id).get();
     if (!docRef.exists) {
-      throw new Error("No se encontró el usuario");
+      throw createHttpError(404, "No se encontró el usuario");
     }
-
     await db
       .collection("companys")
       .doc(id)
       .update({ ...data, updatedAt: updatedAt });
-
     res.status(200).json({ message: "Compañia actualizado correctamente" });
   } catch (error) {
-    console.error("Error al actualizar el compañia", error);
-    res.status(400).json({ message: error.message });
+    next(error);
   }
 };
