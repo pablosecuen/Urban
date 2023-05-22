c819bd6d1221265fe1a1ad586d8b00f85bfaf410
AgustinChavero <agustin1996orellano@gmail.com>
2023-05-22 14:36:18 -0300

    ReduxCompany - Correccion de importaciones

--- client/Redux/company/companyActions.ts
+++ client/Redux/company/companyActions.ts
@@ -1,12 +1,13 @@
 import { Company } from "@component/app/types/Company";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 //Hay que refactorizar el codigo, tiene que haber actions unicamente para GetAll, GetById, y GetByQueryParams
 
 export const getAllCompanies = createAsyncThunk<Company[], void>(
   "companies/getAllCompanies",
   async () => {
     const response = await axios.get(`http://localhost:3000/company`);
+    console.log(response.data.companies);
     return response.data.companies;
   }
 );
