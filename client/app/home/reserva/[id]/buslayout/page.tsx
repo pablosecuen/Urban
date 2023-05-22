191e8e1ae13283786264124a45ff61056e22ce88
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-22 20:16:04 -0300

    configuracion de redux para manejar los estados habilitado/deshabilitado de los asientos del bus para poder ofrecer los pasajes.. agregado del componente en el admin para manejar los estados y en el cliente para ver habilitados y poder elegir su lugar en el bus

--- client/app/home/reserva/[id]/buslayout/page.tsx
+++ client/app/home/reserva/[id]/buslayout/page.tsx
@@ -1,19 +1,35 @@
-import PlantaAlta from "./PlantaAlta";
-import PlantaBaja from "./PlantaBaja";
+// BusLayout.tsx
+"use client";
+import React from "react";
+
+import dynamic from "next/dynamic";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "@component/Redux/store/store";
+
+const PlantaAlta = dynamic(
+  () => import("../../../../../components/PlantaAlta/PlantaAlta"),
+  { ssr: false } // Set ssr option to false to only render on the client side
+);
+const PlantaBaja = dynamic(
+  () => import("../../../../../components/PlantaAlta/PlantaAlta"),
+  { ssr: false } // Set ssr option to false to only render on the client side
+);
 
 export default function BusLayout() {
-  // return <div className="grid-container">{arr}</div>;
+  const seatEnabled = useSelector((state: RootState) => state.seats.seatEnabled);
+
+
   return (
-    <div className="mx-auto flex w-full flex-col items-center justify-center gap-5 rounded-3xl border-2 py-10 text-center shadow-2xl shadow-black/40 ">
+    <div className="mx-auto flex w-full flex-col items-center justify-center gap-5 rounded-3xl border-2 py-10 text-center shadow-2xl shadow-black/40">
       <h1 className="text-xl tracking-wider">Elige tu asiento</h1>
-      <section className="flex w-4/5 flex-col justify-center gap-4  px-4 xl:flex-row">
-        <article className="flex flex-col items-center justify-center gap-10 rounded-2xl border-2 border-blue/30 py-2 text-center  shadow-inner-md md:w-1/2">
+      <section className="flex w-4/5 flex-col justify-center gap-4 px-4 xl:flex-row">
+        <article className="flex flex-col items-center justify-center gap-10 rounded-2xl border-2 border-blue/30 py-2 text-center shadow-inner-md md:w-1/2">
           <h2 className="tracking-wider xl:text-lg">Planta Alta</h2>
-          <PlantaAlta />
+          <PlantaAlta seatEnabled={seatEnabled}/>
         </article>
         <article className="flex flex-col items-center justify-center gap-10 rounded-2xl border-2 border-blue/30 py-2 text-center shadow-inner-md md:w-1/2 ">
           <h2 className="tracking-wider xl:text-lg">Planta Baja</h2>
-          <PlantaBaja />
+          <PlantaBaja seatEnabled={seatEnabled}/>
         </article>
       </section>
       <button className="w-auto">Seguir con la compra</button>
