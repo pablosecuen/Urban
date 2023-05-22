81a1ed71f6f162234a5ffcf0020156fbb29163d9
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-05-22 14:50:06 -0300

    update companyId validator

--- api/src/utils/validations/validators.ts
+++ api/src/utils/validations/validators.ts
@@ -197,9 +197,11 @@ export const isProductIdValid = (productId: string): string | null => {
   return "El id del producto no es válido";
 };
 
-export const isCompanyIdValid = (companyId: string): string | null => {
-  if (typeof companyId === "string") return null;
-  return "El id de la compañía no es válido";
+export const isCompanyIdValid = (req: Request, res: Response): void => {
+  const companyId = req.body.companyId;
+  if (typeof companyId === "string") {
+    throw createHttpError(400, "El id de la compañía no es válido");
+  }
 };
 
 export const isLocalIdValid = (localId: string): string | null => {
