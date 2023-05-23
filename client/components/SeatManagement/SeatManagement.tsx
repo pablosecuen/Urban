aab51798bc0ffe2a80de1e357b13eeeb87ee16a5
httpfido <fidel.romano.2016@hotmail.com>
2023-05-23 13:52:23 -0300

    /admin/dashboard/passages pasar por prop el pasaje

--- client/components/SeatManagement/SeatManagement.tsx
+++ client/components/SeatManagement/SeatManagement.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import PlantaAltaAdmin from "../PlantaAltaAdmin/PlantaAltaAdmin";
 import PlantaBajaAdmin from "../PlantaBajaAdmin/PlantaBajaAdmin";
+import { CardProfilePropsPassage } from "@component/app/types/Passages";
 
-const SeatManagement = () => {
+const SeatManagement: React.FC<CardProfilePropsPassage>  = ({selectedPassage}) => {
   return (
     <div className="relative mb-6 mt-16 flex min-w-0 flex-col break-words rounded-lg bg-white shadow-xl">
       <h2 className="py-4 text-center">Seat Management</h2>
       <ul className="flex items-center justify-center gap-4">
         <li className="flex w-1/2 items-center justify-center rounded-xl border-2 border-blue p-4">
-          <PlantaAltaAdmin />
+          <PlantaAltaAdmin selectedPassage={selectedPassage}/>
         </li>
         <li className="flex w-1/2 items-center justify-center rounded-xl border-2 border-blue p-4">
-          <PlantaBajaAdmin />
+          <PlantaBajaAdmin selectedPassage={selectedPassage}/>
         </li>
       </ul>
     </div>
