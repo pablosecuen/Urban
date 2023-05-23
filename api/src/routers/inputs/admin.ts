e3ab7135be5062cfd4ccd12a7ab5e99073f2ad2d
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-05-22 22:55:46 -0300

    add disableSeatsPassage controller

--- api/src/routers/inputs/admin.ts
+++ api/src/routers/inputs/admin.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import {
+  disableSeatsPassage,
   updateAdminStatus,
   updateSeatPassage,
   updateStatusChauffeur,
@@ -14,5 +15,6 @@ router.patch("/changeStatusChauffeur/:id", updateStatusChauffeur);
 router.patch("/changeStatusDelivery/:id", updateStatusDelivery);
 router.patch("/changeStatusVehicle/:id", updateStatusVehicle);
 router.put("/enableSeats/:id", updateSeatPassage);
+router.put("/disableSeats/:id", disableSeatsPassage);
 
 export default router;
