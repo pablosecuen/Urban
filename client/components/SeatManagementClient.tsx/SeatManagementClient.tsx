495a353ad639b052687e65be26390c8f11bc67f6
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-24 16:17:40 -0300

    renderiza asientos disponibles, no permite abrir modal si el asiento no esta habilitado por admin. pushea array con todos los detalles al session storage correctamente, falta limitar la posibilidad de adiquirir asientos repetidos, o mas asientos de los que esta  intentando comprar en relacion al contador

--- client/components/SeatManagementClient.tsx/SeatManagementClient.tsx
+++ client/components/SeatManagementClient.tsx/SeatManagementClient.tsx
@@ -1,25 +1,32 @@
 "use client";
 import React from "react";
-import { CardProfilePropsPassage } from "@component/app/types/Passages";
+import { CardProfilePropsEnabledSeats } from "@component/app/types/Passages";
 import PlantaAlta from "@component/components/PlantaAlta/PlantaAlta";
 import PlantaBaja from "@component/components/PlantaBaja/PlantaBaja";
 import { useSelector } from "react-redux";
 
-const SeatManagement: React.FC<CardProfilePropsPassage> = ({ selectedPassage }) => {
-  const count = useSelector((state: any) => state.payment.passageById);
-  console.log(count);
+const SeatManagement: React.FC<CardProfilePropsEnabledSeats> = ({ enabledSeats }) => {
+  const count = useSelector((state: any) => state.payment.passageById[0]);
+
+  const passangers = count?.quantity
 
   return (
-    <div className="relative mb-6 mt-16 flex min-w-0 flex-col break-words rounded-lg bg-white shadow-xl">
-      <h2 className="py-4 text-center">Seat Management</h2>
-      <ul className="flex items-center justify-center gap-4">
-        <li className="flex w-1/2 items-center justify-center rounded-xl border-2 border-blue p-4">
-          <PlantaAlta selectedPassage={selectedPassage} />
-        </li>
-        <li className="flex w-1/2 items-center justify-center rounded-xl border-2 border-blue p-4">
-          <PlantaBaja selectedPassage={selectedPassage} />
-        </li>
-      </ul>
+    <div className="flex w-full flex-col  items-center justify-center gap-2 rounded-3xl border-2 border-gray-300 bg-white py-4 shadow-xl shadow-black/40 xl:h-[650px] xl:justify-between">
+      <div className="relative  flex min-w-0 flex-col break-words rounded-lg bg-transparent ">
+        <h2 className="py-4 text-center">Elige tus asientos </h2>
+        <p className="italic text-gray-400">
+          Al hacer click se te solicitara la informacion de los pasajeros
+        </p>
+        <p>Pasajeros restantes: {count?.quantity}</p>
+        <ul className="flex items-center justify-center gap-4 bg-transparent">
+          <li className="flex w-1/2 items-center justify-center rounded-xl border-2 border-blue p-4">
+            <PlantaAlta enabledSeats={enabledSeats} passangers={passangers} />
+          </li>
+          <li className="flex w-1/2 items-center justify-center rounded-xl border-2 border-blue p-4">
+            <PlantaBaja enabledSeats={enabledSeats} passangers={passangers} />
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
