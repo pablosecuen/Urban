0cf7d4a5e7d3566ffa750dce21f73f08525b8ec2
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-24 16:17:19 -0300

    renderiza asientos disponibles, no permite abrir modal si el asiento no esta habilitado por admin. pushea array con todos los detalles al session storage correctamente, falta limitar la posibilidad de adiquirir asientos repetidos, o mas asientos de los que esta  intentando comprar en relacion al contador

--- client/app/home/reserva/[id]/buslayout/page.tsx
+++ client/app/home/reserva/[id]/buslayout/page.tsx
@@ -3,8 +3,8 @@ import { RootState } from "@component/Redux/store/store";
 import SeatManagementClient from "@component/components/SeatManagementClient.tsx/SeatManagementClient";
 import { useSelector } from "react-redux";
 
+
 export default function Buslayout() {
   const passage = useSelector((state: RootState) => state.passage.passageById);
-  console.log(passage);
-  return <SeatManagementClient selectedPassage={passage} />;
+  return <SeatManagementClient enabledSeats={passage}  />;
 }
