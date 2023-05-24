446222b8ad0f26e6f635db3d4c41fb762f2fb03f
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-05-24 20:44:15 -0300

    Pasajes Disponible Dinamico

--- client/components/SeatManagementClient.tsx/SeatManagementClient.tsx
+++ client/components/SeatManagementClient.tsx/SeatManagementClient.tsx
@@ -7,8 +7,11 @@ import { useSelector } from "react-redux";
 
 const SeatManagement: React.FC<CardProfilePropsEnabledSeats> = ({ enabledSeats }) => {
   const count = useSelector((state: any) => state.payment.passageById[0]);
+  const passagerData = useSelector((state: any) => state.payment?.passengerData)
+  const lengthData = passagerData.length
+  console.log({longitud:passagerData.length});
 
-  const passangers = count?.quantity
+  const passangers = count?.quantity 
 
   return (
     <div className="flex w-full flex-col  items-center justify-center gap-2 rounded-3xl border-2 border-gray-300 bg-white py-4 shadow-xl shadow-black/40 xl:h-[650px] xl:justify-between">
@@ -17,7 +20,7 @@ const SeatManagement: React.FC<CardProfilePropsEnabledSeats> = ({ enabledSeats }
         <p className="italic text-gray-400">
           Al hacer click se te solicitara la informacion de los pasajeros
         </p>
-        <p>Pasajeros restantes: {count?.quantity}</p>
+        <p>Pasajeros restantes: {count?.quantity - lengthData}</p>
         <ul className="flex items-center justify-center gap-4 bg-transparent">
           <li className="flex w-1/2 items-center justify-center rounded-xl border-2 border-blue p-4">
             <PlantaAlta enabledSeats={enabledSeats} passangers={passangers} />
