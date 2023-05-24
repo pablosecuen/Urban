7cf07763576c2e480cf32b405c372a459fa3b37a
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-05-24 20:44:32 -0300

    ADD slice para passageData

--- client/Redux/payment/paymentSlice.ts
+++ client/Redux/payment/paymentSlice.ts
@@ -1,10 +1,10 @@
 import { Passage } from "@component/app/types/Passages";
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { getPassagesIdForPayment } from "./paymentActions";
-
-
+import { PassengerFormData } from "@component/app/types/Passenger";
 
 interface PassageState {
+  passengerData: PassengerFormData[];
   payment: Passage[];
   count: number;
   passageById: Passage[];
@@ -13,36 +13,40 @@ interface PassageState {
 }
 
 const initialState: PassageState = {
-    payment: [],
-    count: 0,
-    passageById: [],
-    status: "idle",
-    error: null
+  passengerData: [],
+  payment: [],
+  count: 0,
+  passageById: [],
+  status: "idle",
+  error: null,
 };
 
 const paymentSlice = createSlice({
   name: "payment",
   initialState,
-  reducers: {},
+  reducers: {
+    savePassengerData: (state, action: PayloadAction<PassengerFormData[]>) => {
+      state.passengerData = action.payload;
+    },
+  },
   extraReducers: (builder) => {
-
     builder
-    // Handle the pending state
-    .addCase(getPassagesIdForPayment.pending, (state) => {
-      state.status = "loading";
-    })
-    // Handle the success state
-    .addCase(getPassagesIdForPayment.fulfilled, (state, action) => {
-      state.status = "succeeded";
-      state.passageById = action.payload;
-    })
-    // Handle the error state
-
-    .addCase(getPassagesIdForPayment.rejected, (state, action) => {
-      state.status = "failed";
-      state.error = action.error.message ?? "Something went wrong";
-    });
+      // Handle the pending state
+      .addCase(getPassagesIdForPayment.pending, (state) => {
+        state.status = "loading";
+      })
+      // Handle the success state
+      .addCase(getPassagesIdForPayment.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.passageById = action.payload;
+      })
+      // Handle the error state
+      .addCase(getPassagesIdForPayment.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message ?? "Something went wrong";
+      });
   },
 });
+export const { savePassengerData } = paymentSlice.actions;
 
 export default paymentSlice.reducer;
