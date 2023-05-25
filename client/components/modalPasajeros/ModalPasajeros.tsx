d6096829c6095e4dd247bf2ae210c922c3074d3d
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-25 20:19:16 -0300

    completao el component para manejar los estados de los asiestos del bus, el submit del form para subir la informacion de los pasajeros con alertas push, tmb se puede cancelar la reserva de un asiento con un solo click, estilos para para las cards que renderizan la informacion que el usuario ingreso como metodo de confirmacion antes de proceder a pasarela de pago

--- client/components/modalPasajeros/ModalPasajeros.tsx
+++ client/components/modalPasajeros/ModalPasajeros.tsx
@@ -2,7 +2,7 @@
 import { savePassengerData } from "@component/Redux/payment/paymentSlice";
 import { PassengerFormData, PassengerFormModalProps } from "@component/app/types/Passenger";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 
@@ -11,6 +11,9 @@ const ModalPasajeros: React.FC<PassengerFormModalProps> = ({
   setIsModalOpen,
   seat,
   enabledSeats,
+  notifySeatSelected,
+  setSelectedSeats,
+  selectedSeats,
 }) => {
   const [formData, setFormData] = useState<PassengerFormData>({
     nombre: "",
@@ -28,11 +31,6 @@ const ModalPasajeros: React.FC<PassengerFormModalProps> = ({
   const dispatch = useDispatch();
   const existingData = useSelector((state: any) => state.payment?.passengerData);
 
-  useEffect(() => {
-    // Aquí puedes realizar cualquier acción que necesites cuando cambie `existingData`
-    console.log("existingData has changed:", existingData);
-  }, [existingData]);
-
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
@@ -46,11 +44,6 @@ const ModalPasajeros: React.FC<PassengerFormModalProps> = ({
     const updatedPassengerData = [...existingData, { ...formData, seat, quantity: "1" }];
 
     dispatch(savePassengerData(updatedPassengerData));
-
-    const updatedEnabledSeats = {
-      ...enabledSeats,
-      numberSeat: [...enabledSeats.numberSeat, seat],
-    };
 
     setIsModalOpen(false);
 
@@ -67,169 +60,175 @@ const ModalPasajeros: React.FC<PassengerFormModalProps> = ({
       cc: "",
       quantity: "",
     });
+    notifySeatSelected();
   };
 
   const handleCloseModal = () => {
-    setIsModalOpen(!isModalOpen);
+    setIsModalOpen(false);
+    setSelectedSeats(selectedSeats?.filter((prevSeat) => prevSeat !== seat));
   };
 
   return (
     <>
       {isModalOpen && (
-        <div className='fixed left-1/2 top-1/2 z-50 h-[660px] w-96 -translate-x-1/2 -translate-y-1/2 rounded-3xl bg-white p-6 shadow-2xl shadow-black/40 '>
-          <h2 className=' text-2xl font-bold'>Información del pasajero</h2>
+        <div className="fixed left-1/2 top-1/2 z-50 h-[660px] w-96 -translate-x-1/2 -translate-y-1/2 rounded-3xl bg-white p-6 shadow-2xl shadow-black/40 ">
+          <h2 className=" text-2xl font-bold">Información del pasajero</h2>
           <form onSubmit={handleSubmit}>
-            <div className=''>
-              <label className=' block' htmlFor='nombre'>
+            <div className="">
+              <label className=" block" htmlFor="nombre">
                 Nombre
               </label>
               <input
-                type='text'
-                id='nombre'
-                name='nombre'
+                type="text"
+                id="nombre"
+                name="nombre"
                 value={formData.nombre}
                 onChange={handleChange}
-                className='w-full rounded border border-gray-300 px-3 py-1'
+                className="w-full rounded border border-gray-300 px-3 py-1"
                 maxLength={25}
                 required
               />
             </div>
-            <div className=''>
-              <label className='mb-2 block' htmlFor='apellido'>
+            <div className="">
+              <label className="mb-2 block" htmlFor="apellido">
                 Apellido
               </label>
               <input
-                type='text'
-                id='apellido'
-                name='apellido'
+                type="text"
+                id="apellido"
+                name="apellido"
                 value={formData.apellido}
                 onChange={handleChange}
-                className='w-full rounded border border-gray-300 px-3 py-1'
+                className="w-full rounded border border-gray-300 px-3 py-1"
                 maxLength={25}
                 required
               />
             </div>
-            <div className=''>
-              <label className='mb-2 block' htmlFor='nacionalidad'>
+            <div className="">
+              <label className="mb-2 block" htmlFor="nacionalidad">
                 Nacionalidad
               </label>
               <input
-                type='text'
-                id='nacionalidad'
-                name='nacionalidad'
+                type="text"
+                id="nacionalidad"
+                name="nacionalidad"
                 value={formData.nacionalidad}
                 onChange={handleChange}
-                className='w-full rounded border border-gray-300 px-3 py-1'
+                className="w-full rounded border border-gray-300 px-3 py-1"
                 required
               />
             </div>
-            <div className=''>
-              <label className='mb-2 block' htmlFor='tipoDocumento'>
+            <div className="">
+              <label className="mb-2 block" htmlFor="tipoDocumento">
                 Tipo de Documento
               </label>
-              <div className='flex'>
+              <div className="flex">
                 {" "}
                 <select
-                  id='tipoDocumento'
-                  name='tipoDocumento'
+                  id="tipoDocumento"
+                  name="tipoDocumento"
                   value={formData.tipoDocumento}
                   onChange={handleChange}
-                  className='w-1/4 rounded border border-gray-300 px-3 py-1'
+                  className="w-1/4 rounded border border-gray-300 px-3 py-1"
                   required
                 >
-                  <option value=''>Seleccione una opción</option>
-                  <option value='CC'>C.C</option>
-                  <option value='CE'>C.E</option>
+                  <option value="">Seleccione una opción</option>
+                  <option value="CC">C.C</option>
+                  <option value="CE">C.E</option>
                 </select>
                 <input
-                  type='text'
-                  id='cc'
-                  name='cc'
+                  type="text"
+                  id="cc"
+                  name="cc"
                   value={formData.cc}
                   onChange={handleChange}
-                  className='w-full rounded border border-gray-300 px-3 py-1'
+                  className="w-full rounded border border-gray-300 px-3 py-1"
                   maxLength={8}
                   required
                 />
               </div>
             </div>
             <div>
-              <label htmlFor='fechaNacimiento'>Fecha de Nacimiento:</label>
+              <label htmlFor="fechaNacimiento">Fecha de Nacimiento:</label>
               <input
-                type='date'
-                id='fechaNacimiento'
-                name='fechaNacimiento'
+                type="date"
+                id="fechaNacimiento"
+                name="fechaNacimiento"
                 value={formData.fechaNacimiento}
                 onChange={handleChange}
                 required
-                className='w-full rounded border border-gray-300 px-3 py-1'
+                className="w-full rounded border border-gray-300 px-3 py-1"
               />
             </div>
             <div>
-              <label htmlFor='genero'>Género:</label>
+              <label htmlFor="genero">Género:</label>
               <select
-                id='genero'
-                name='genero'
+                id="genero"
+                name="genero"
                 value={formData.genero}
                 onChange={handleChange}
-                className='w-full rounded border border-gray-300 px-3 py-1'
+                className="w-full rounded border border-gray-300 px-3 py-1"
                 required
               >
-                <option value=''>Seleccione un género</option>
-                <option value='masculino'>Masculino</option>
-                <option value='femenino'>Femenino</option>
-                <option value='prefiero declarar'>No Declara</option>
+                <option value="">Seleccione un género</option>
+                <option value="masculino">Masculino</option>
+                <option value="femenino">Femenino</option>
+                <option value="prefiero declarar">No Declara</option>
               </select>
             </div>
             <div>
-              <div className='flex gap-4'>
-                <div className='flex w-1/3 flex-col'>
-                  <label htmlFor='telefono'>Area:</label>
+              <div className="flex gap-4">
+                <div className="flex w-1/3 flex-col">
+                  <label htmlFor="telefono">Area:</label>
                   <input
-                    type='text'
-                    id='codigoArea'
-                    name='codigoArea'
+                    type="text"
+                    id="codigoArea"
+                    name="codigoArea"
                     value={formData.codigoArea}
                     onChange={handleChange}
                     maxLength={3}
-                    className='w-full rounded border border-gray-300 px-3 py-1'
+                    className="w-full rounded border border-gray-300 px-3 py-1"
                     required
                   />
                 </div>
-                <div className='flex flex-col'>
-                  <label htmlFor='telefono'>Teléfono:</label>
+                <div className="flex flex-col">
+                  <label htmlFor="telefono">Teléfono:</label>
                   <input
-                    type='text'
-                    id='telefono'
-                    name='telefono'
+                    type="text"
+                    id="telefono"
+                    name="telefono"
                     value={formData.telefono}
                     onChange={handleChange}
                     maxLength={10}
-                    className='w-full rounded border border-gray-300 px-3 py-1'
+                    className="w-full rounded border border-gray-300 px-3 py-1"
                     required
                   />
                 </div>
               </div>
             </div>
             <div>
-              <label htmlFor='email'>Email:</label>
+              <label htmlFor="email">Email:</label>
               <input
-                type='email'
-                id='email'
-                name='email'
+                type="email"
+                id="email"
+                name="email"
                 value={formData.email}
                 onChange={handleChange}
-                pattern='^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-                className='w-full rounded border border-gray-300 px-3 py-1'
+                pattern="^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+                className="w-full rounded border border-gray-300 px-3 py-1"
                 required
               />
             </div>
             {/* Continue adding the rest of the form fields */}
-            <div className='flex justify-center gap-4 pt-8'>
-              <button type='button' onClick={handleCloseModal} className=' bg-blue px-4 py-2'>
+            <div className="flex justify-center gap-4 pt-8">
+              <button
+                type="button"
+                className="bg-blue px-4 py-2 text-white"
+                onClick={handleCloseModal}
+              >
                 Cancel
               </button>
-              <button type='submit' className='bg-blue px-4 py-2 text-white'>
+              <button type="submit" className="bg-blue px-4 py-2 text-white">
                 Submit
               </button>
             </div>
