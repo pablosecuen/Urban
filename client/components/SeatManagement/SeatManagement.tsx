9a30a5d2c4bc7302a30a3f1b040828e435973ad2
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-26 15:11:23 -0300

    fix estilos y prep para funcionalidad de mopa de asientos en admin para habilitar y deshabilitar asientos a volutand

--- client/components/SeatManagement/SeatManagement.tsx
+++ client/components/SeatManagement/SeatManagement.tsx
@@ -3,16 +3,16 @@ import PlantaAltaAdmin from "../PlantaAltaAdmin/PlantaAltaAdmin";
 import PlantaBajaAdmin from "../PlantaBajaAdmin/PlantaBajaAdmin";
 import { CardProfilePropsPassage } from "@component/app/types/Passages";
 
-const SeatManagement: React.FC<CardProfilePropsPassage>  = ({selectedPassage}) => {
+const SeatManagement: React.FC<CardProfilePropsPassage> = ({ selectedPassage }) => {
   return (
     <div className="relative mb-6 mt-16 flex min-w-0 flex-col break-words rounded-lg bg-white shadow-xl">
       <h2 className="py-4 text-center">Seat Management</h2>
       <ul className="flex items-center justify-center gap-4">
         <li className="flex w-1/2 items-center justify-center rounded-xl border-2 border-blue p-4">
-          <PlantaAltaAdmin selectedPassage={selectedPassage}/>
+          <PlantaAltaAdmin selectedPassage={selectedPassage} />
         </li>
         <li className="flex w-1/2 items-center justify-center rounded-xl border-2 border-blue p-4">
-          <PlantaBajaAdmin selectedPassage={selectedPassage}/>
+          <PlantaBajaAdmin selectedPassage={selectedPassage} />
         </li>
       </ul>
     </div>
