1e2d4e9dd0cbbd113b131223eba14ac09deefd98
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-26 14:29:34 -0300

    "estilos de perfil y fix de erres de consola a nivel general"

--- client/components/SeatManagementClient.tsx/SeatManagementClient.tsx
+++ client/components/SeatManagementClient.tsx/SeatManagementClient.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import { CardProfilePropsEnabledSeats } from "@component/app/types/Passages";
 import PlantaAlta from "@component/components/PlantaAlta/PlantaAlta";
 import PlantaBaja from "@component/components/PlantaBaja/PlantaBaja";
@@ -13,29 +13,22 @@ const SeatManagement: React.FC<CardProfilePropsEnabledSeats> = ({ enabledSeats }
   const lengthData = passagerData.length;
 
   const passangers = count?.quantity;
-  useEffect(() => {
-    if (count?.quantity - lengthData === 0) {
-      router.push("/home/reserva/viajes/confirmacion/pagos");
-    }
-  }, [count, lengthData, router]);
+  if (count?.quantity - lengthData == 0) {
+    router.push("/home/reserva/viajes/confirmacion/pagos");
+  }
   return (
-    <div className="flex w-full flex-col  items-center justify-center gap-2 rounded-3xl border-2 border-gray-300 bg-white py-4 shadow-xl shadow-black/40 xl:h-[650px] xl:justify-between">
-      <div className="relative  mt-4 flex min-w-0 flex-col break-words rounded-lg bg-transparent">
-        <h2 className="flex text-center text-xl text-gray-700">
-          Elige tus asientos{" "}
-          <p className="text-bas ml-20 flex items-center text-center align-middle">
-            Pasajeros restantes: {count?.quantity - lengthData}
-          </p>
-        </h2>
-
-        <p className="pb-2 text-center text-sm italic text-gray-400">
+    <div className='flex w-full flex-col  items-center justify-center gap-2 rounded-3xl border-2 border-gray-300 bg-white py-4 shadow-xl shadow-black/40 xl:h-[650px] xl:justify-between'>
+      <div className='relative  flex min-w-0 flex-col break-words rounded-lg bg-transparent '>
+        <h2 className='py-4 text-center'>Elige tus asientos </h2>
+        <p className='italic text-gray-400'>
           Al hacer click se te solicitara la informacion de los pasajeros
         </p>
-        <ul className="flex items-center justify-center gap-4 bg-transparent">
-          <li className="flex w-1/2 items-center justify-center rounded-xl border-2 border-blue p-4">
+        <p>Pasajeros restantes: {count?.quantity - lengthData}</p>
+        <ul className='flex items-center justify-center gap-4 bg-transparent'>
+          <li className='flex w-1/2 items-center justify-center rounded-xl border-2 border-blue p-4'>
             <PlantaAlta enabledSeats={enabledSeats} passangers={passangers} />
           </li>
-          <li className="flex w-1/2 items-center justify-center rounded-xl border-2 border-blue p-4">
+          <li className='flex w-1/2 items-center justify-center rounded-xl border-2 border-blue p-4'>
             <PlantaBaja enabledSeats={enabledSeats} passangers={passangers} />
           </li>
         </ul>
