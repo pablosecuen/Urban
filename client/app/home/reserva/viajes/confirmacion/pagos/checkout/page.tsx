96453f1e194fe4793ae68b64e101978aa78b7f1f
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-27 15:52:45 -0300

    estilos, alertas y confirmacion para que confirmer cuando viajan menores de edad

--- client/app/home/reserva/viajes/confirmacion/pagos/checkout/page.tsx
+++ client/app/home/reserva/viajes/confirmacion/pagos/checkout/page.tsx
@@ -2,11 +2,11 @@
 import axios from "axios";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
 //import CardCheckout from "@component/components/Cards/CardCheckout";
 
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Ticket } from "../../../../../../types/Ticket";
 
 export default function Checkout() {
   //datos mercadopago
@@ -15,61 +15,88 @@ export default function Checkout() {
   const merchantOrder: string | null = queryParams.get("merchant_order_id"); //codigo factura
   const status: string | null = queryParams.get("status"); //estado de exito o no
   const user: any | null = JSON.parse(localStorage.getItem("user") || ""); //id del usuario de la app;
-  // - - - - - - - - - - - - NOTIFICACIONES - - - - - - - - - - - -
-  const notifySuccess = () =>
-    toast.success(`Su pago ha sido realizado con éxito!`, {
-      position: "bottom-center",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "light",
-    });
+  const [ticket, setTicket] = useState<Ticket | null>(null);
 
-  const getToken = async () => {
-    try {
-      const { data } = await axios.get(
-        `http://localhost:3000/payment/merchantOrder?merchantOrder=${merchantOrder}`
-      );
-      //este axios pueeeede llegar a ser a mercadopago
-      //aca se guarda la info y con esto generamos la factura
-      const requestData = {
-        userId: user?.id,
-        passageId: data?.items[0].id,
-        price: data?.items[0].unit_price,
-        quantity: data?.items[0].quantity,
-        paymentId: paymentId,
-        merchantOrder: merchantOrder,
-        statusMp: status,
-        passengersData: {
-          description: data?.items[0].description,
-        },
-      };
-      return requestData;
-    } catch (error) {
-      return error;
+  // - - - - - - - - - - - - NOTIFICACIONES - - - - - - - - - - - -
+  const notifySuccess = () => {
+    if (!toast.isActive("success")) {
+      toast.success(`Su pago ha sido realizado con éxito!`, {
+        toastId: "success",
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    }
+  };
+
+  const notifyError = () => {
+    if (!toast.isActive("error")) {
+      toast.error(`Su pago no ha podido realizarse!`, {
+        toastId: "error",
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
     }
   };
 
   useEffect(() => {
-    (async () => {
-      const ticket = await getToken();
-      axios.post("http://localhost:3000/ticket", ticket);
-    })();
+    const getToken = async () => {
+      try {
+        const { data } = await axios.get(
+          `http://localhost:3000/payment/merchantOrder?merchantOrder=${merchantOrder}`
+        );
+        //este axios pueeeede llegar a ser a mercadopago
+        //aca se guarda la info y con esto generamos la factura
+        const requestData = {
+          userId: user?.id,
+          passageId: data?.items[0].id,
+          price: data?.items[0].unit_price,
+          quantity: data?.items[0].quantity,
+          paymentId: paymentId,
+          merchantOrder: merchantOrder,
+          statusMp: status,
+          passengersData: {
+            description: data?.items[0].description,
+          },
+        };
+        return requestData;
+      } catch (error) {
+        return error;
+      }
+    };
 
-    notifySuccess();
-    //post con el endpoint del ticket usando requestData
+    (async () => {
+      try {
+        const ticketData = await getToken();
+        axios.post("http://localhost:3000/ticket", ticketData);
+        setTicket(ticketData as Ticket | null);
+        notifySuccess();
+      } catch (error) {
+        notifyError();
+      }
+    })();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  console.log(ticket);
+
   return (
-    <div className='flex  flex-col justify-center gap-8 bg-white'>
+    <div className="relative mt-10 flex h-full w-full flex-col items-center gap-2 rounded-3xl border-2 bg-white p-6 shadow-2xl shadow-black/40 lg:h-[530px]">
       {/* <p className="text-center text-2xl font-bold">Su pago ha sido realizado con éxito !</p> */}
       {/* <Image src={success} alt="pago exitoso" className="h-80 w-80 self-center"></Image> */}
       <ToastContainer
-        position='bottom-right'
+        position="top-right"
         autoClose={5000}
         hideProgressBar={false}
         newestOnTop={false}
@@ -78,15 +105,32 @@ export default function Checkout() {
         pauseOnFocusLoss
         draggable
         pauseOnHover
-        theme='light'
+        theme="light"
         closeButton={false}
       />
-
-      <Link href='/home'>
-        <button className='mx-auto flex  w-1/2 justify-center py-1 text-center'>
-          Volver al inicio
-        </button>
-      </Link>
+      <h1 className="abolsute top-4 border-2 text-center text-lg font-bold">
+        Su pago ha sido realizado con éxito
+      </h1>
+      <div className="mt-10 flex h-full flex-col">
+        <div className="flex h-full flex-col gap-4 overflow-y-auto">
+          <h2 className="text-center text-sm font-light italic">resumen informativo</h2>
+          {ticket &&
+            Object.entries(ticket.passengersData || {}).map(([key, value]) => (
+              <div key={key} className="py-4">
+                <h3 className="text-gray-700">Descripcion:</h3>
+                <h4 className="text-gray-400">{JSON.stringify(value)}</h4>
+              </div>
+            ))}
+        </div>
+        <div className="absolute bottom-10 left-0 flex w-full justify-center">
+          <Link
+            href="/home"
+            className="mx-auto rounded-md bg-blue px-2 py-1 text-center text-white hover:bg-indigo-700"
+          >
+            Volver al inicio
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
