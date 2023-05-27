7bb37ab53fb6ab0bb689141feebc99c439158fa8
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-05-27 16:48:27 -0300

    ADD logica SeatManagement

--- client/components/SeatManagement/SeatManagement.tsx
+++ client/components/SeatManagement/SeatManagement.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import PlantaAltaAdmin from "../PlantaAltaAdmin/PlantaAltaAdmin";
 import PlantaBajaAdmin from "../PlantaBajaAdmin/PlantaBajaAdmin";
@@ -7,30 +7,50 @@ import { CardProfilePropsPassage } from "@component/app/types/Passages";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const SeatManagement: React.FC<CardProfilePropsPassage> = ({ selectedPassage }) => {
-  const [newSeats, setNewSeats] = useState<string[]>([]);
+const SeatManagement: React.FC<CardProfilePropsPassage> = ({ selectedPassage, newSeats }) => {
+  const [selectedSeats, setSelectedSeats] = useState<string[]>(newSeats || []);
+  const [disabledSeats, setDisabledSeats] = useState<string[]>([]);
 
   const handleSeatToggle = (seat: string) => {
-    if (newSeats.includes(seat)) {
-      setNewSeats((prevSeats) => prevSeats.filter((s) => s !== seat));
+    if (selectedSeats.includes(seat)) {
+      setSelectedSeats((prevSeats) => prevSeats.filter((selectedSeat) => selectedSeat !== seat));
     } else {
-      setNewSeats((prevSeats) => [...prevSeats, seat]);
+      setSelectedSeats((prevSeats) => [...prevSeats, seat]);
+    }
+  };
+
+  const handleDisableSeatToggle = (seat: string) => {
+    if (disabledSeats.includes(seat)) {
+      setDisabledSeats((prevSeats) => prevSeats.filter((disabledSeat) => disabledSeat !== seat));
+    } else {
+      setDisabledSeats((prevSeats) => [...prevSeats, seat]);
     }
   };
 
   const enableSeats = async () => {
     try {
       await axios.put(`http://localhost:3000/admin/enableSeats/${selectedPassage?.id}`, {
-        numberSeat: newSeats,
+        numberSeat: selectedSeats,
       });
       toastSuccess();
-      setNewSeats([]); // Clean selected seats
+      setSelectedSeats([]);
       console.log("Todo Ok");
     } catch (error) {
       console.error("Error", error);
     }
   };
 
+  const disabledSeat = async () => {
+    try {
+      await axios.put(`http://localhost:3000/admin/disableSeats/${selectedPassage?.id}`, {
+        numberSeat: disabledSeats,
+      });
+      toastSuccess();
+      console.log("Todo Ok");
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
   const toastSuccess = () => {
     toast.success("Asientos habilitados exitosamente", {
       position: "top-center",
@@ -49,9 +69,9 @@ const SeatManagement: React.FC<CardProfilePropsPassage> = ({ selectedPassage }) 
   }
 
   return (
-    <div className="relative mb-6 mt-16 flex min-w-0 flex-col break-words rounded-lg bg-white shadow-xl">
+    <div className='relative mb-6 mt-16 flex min-w-0 flex-col break-words rounded-lg bg-white shadow-xl'>
       <ToastContainer
-        position="top-center"
+        position='top-center'
         autoClose={5000}
         hideProgressBar={false}
         newestOnTop={false}
@@ -61,46 +81,69 @@ const SeatManagement: React.FC<CardProfilePropsPassage> = ({ selectedPassage }) 
         draggable
         pauseOnHover
         //Light, dark o colored... elijan el mas conveniente, recuerden cambiar esto tambien en el toast
-        theme="light"
+        theme='light'
         closeButton={false}
       />
-      <h2 className="py-2 text-center text-xl text-gray-700">Seat Management</h2>
-      <ul className="flex items-center justify-center gap-4">
-        <li className="flex w-1/2 items-center justify-center rounded-xl border-2 border-blue p-4">
-          <PlantaAltaAdmin
-            selectedPassage={selectedPassage}
-            newSeats={newSeats}
-            onSeatToggle={handleSeatToggle}
-          />
-        </li>
-        <li className="flex w-1/2 items-center justify-center rounded-xl border-2 border-blue p-4">
-          <PlantaBajaAdmin
-            selectedPassage={selectedPassage}
-            newSeats={newSeats}
-            onSeatToggle={handleSeatToggle}
-          />
-        </li>
-      </ul>
-      <button onClick={enableSeats} className="mx-auto mt-4 w-1/2">
-        Habilitar asientos
-      </button>
-      {newSeats.length === 0 ? (
+      <h2 className='py-4 text-center'>Seat Management</h2>
+
+      {selectedSeats.length === 0 ? (
         <div>No se han seleccionado asientos</div>
       ) : (
-        <div className="flex w-full flex-wrap ">
-          <h3 className="mx-auto py-2 text-center italic">Asientos seleccionados:</h3>
-          <ul className="flex w-full flex-wrap ">
-            {newSeats.map((seat, index) => (
+        <div className='flex w-full flex-wrap '>
+          <h3 className='mx-auto py-2 text-center italic'>Asientos seleccionados:</h3>
+          <ul className='flex w-full flex-wrap '>
+            {selectedSeats.map((seat, index) => (
               <li
+                className='mx-1 mt-2 w-10 rounded-xl border-b-2 border-r-2 border-blue px-2 text-center text-blue'
                 key={index}
-                className="mx-1 mt-2 w-10 rounded-xl border-b-2 border-r-2 border-blue px-2 text-center text-blue"
               >
                 {seat}
               </li>
             ))}
           </ul>
+          <button onClick={enableSeats}>Habilitar asientos</button>
         </div>
       )}
+
+      {disabledSeats.length === 0 ? (
+        <div>No se han deshabilitado asientos</div>
+      ) : (
+        <div className='flex w-full flex-wrap '>
+          <h3 className='mx-auto py-2 text-center italic'>Asientos deshabilitados:</h3>
+          <ul className='flex w-full flex-wrap '>
+            {disabledSeats.map((seat, index) => (
+              <li
+                className='mx-1 mt-2 w-10 rounded-xl border-b-2 border-r-2 border-blue px-2 text-center text-blue'
+                key={index}
+              >
+                {seat}
+              </li>
+            ))}
+          </ul>
+          <button onClick={disabledSeat}>Deshabilitar Asientos</button>
+        </div>
+      )}
+
+      <ul className='flex items-center justify-center gap-4'>
+        <li className='flex w-1/2 items-center justify-center rounded-xl border-2 border-blue p-4'>
+          <PlantaAltaAdmin
+            selectedPassage={selectedPassage}
+            newSeats={selectedSeats}
+            disabledSeats={disabledSeats}
+            onSeatToggle={handleSeatToggle}
+            onDisableSeatToggle={handleDisableSeatToggle}
+          />
+        </li>
+        <li className='flex w-1/2 items-center justify-center rounded-xl border-2 border-blue p-4'>
+          <PlantaBajaAdmin
+            selectedPassage={selectedPassage}
+            newSeats={selectedSeats}
+            disabledSeats={disabledSeats}
+            onSeatToggle={handleSeatToggle}
+            onDisableSeatToggle={handleDisableSeatToggle}
+          />
+        </li>
+      </ul>
     </div>
   );
 };
