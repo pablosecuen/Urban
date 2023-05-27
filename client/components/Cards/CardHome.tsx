03d6eb072547ef0eb7bd1b9ca7bfda03bdfa1e94
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-26 21:27:25 -0300

    guardados algunos estilos en variables para dejar un html mas limpio, trabajo parcial, con el tiempo esto hay que realizarlo en todo el sitio para modularilzar los estilos

--- client/components/Cards/CardHome.tsx
+++ client/components/Cards/CardHome.tsx
@@ -4,23 +4,20 @@ import { useEffect } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import InfiniteSlider from "../InfiniteSlider/InfiniteSlider";
-
 export default function CardHome() {
   let token: any;
   if (typeof window !== "undefined") {
-    const urlParams = new URLSearchParams(window.location.search);
+    const urlParams = new URLSearchParams(window.location.search); //fijarse problema de window
     token = urlParams.get("token");
   }
-
+  
   const userString = typeof window !== "undefined" ? localStorage.getItem("user") : null;
   const user = userString ? JSON.parse(userString) : null;
 
-  const containerStyles =
-    "mb-8 mt-10 flex w-full flex-col items-center justify-evenly rounded-3xl border-2 bg-white px-4 py-4 shadow-2xl shadow-black/40 min-[420px]:mt-32 min-[420px]:w-4/5 lg:mt-10 lg:h-[400px] xl:h-[450px] xl:w-full 2xl:h-4/5";
-  const titleStyles = "py-4 text-center text-xl font-bold xl:text-2xl 2xl:text-2xl";
-  const paragraphStyles = "w-full px-4 text-center lg:text-xs xl:text-base 2xl:text-lg";
-
   const notifySuccess = () =>
+    //Aca es donde se define el funcionamiento de la notificacion, si dura mucho o poco, si es positiva o negativa
+    //Si miran cada Toast solo con cambiar el success, error, warn o info, cambie su funcion
+    //No hace falta cambiar el ToastContainer a la par si solo se cambia el Toast
     toast.success(`Bienvenido ${user?.name} `, {
       position: "bottom-right",
       autoClose: 5000,
@@ -31,7 +28,6 @@ export default function CardHome() {
       progress: undefined,
       theme: "light",
     });
-
   if (user) {
     notifySuccess();
   }
@@ -53,12 +49,12 @@ export default function CardHome() {
   }, []);
 
   return (
-    <div className={containerStyles}>
-      <p className={titleStyles}>
+    <div className="mb-8 mt-10 flex w-full flex-col items-center justify-evenly rounded-3xl border-2 bg-white px-4 py-4 shadow-2xl shadow-black/40 min-[420px]:mt-32 min-[420px]:w-4/5 lg:mt-10 lg:h-[400px] xl:h-[450px] xl:w-full 2xl:h-4/5">
+      <p className="py-4 text-center text-xl font-bold xl:text-2xl 2xl:text-2xl">
         Bienvedios a <span className="text-verdeurban">U</span>
         <span className=" text-celeste">rban</span>!
       </p>
-      <p className={paragraphStyles}>
+      <p className="w-full px-4 text-center lg:text-xs xl:text-base 2xl:text-lg ">
         Nuestra <b>misión</b> es entregar al usuario el poder de decidir su medio de transporte
         acercando a la oferta a la palma de su mano, <b>para empoderarte</b> con mayor información
         asistiendo en que tomes la mejor decision disponble dentro de tus necesidades, en nuestra
@@ -68,6 +64,9 @@ export default function CardHome() {
         problemas cotidianos en la menor cantidad de tiempo posibles, entregando al cliente un poder
         increible para <b>gestionar</b> tu tiempo y tus días de la mejor manera posible
       </p>
+      {/* <p className="py-4 text-center text-lg font-bold 2xl:text-xl">
+        {"<-- "}Selecciona el tipo de servicio para poder continuar
+      </p> */}
       <InfiniteSlider />
       <ToastContainer
         position="bottom-right"
