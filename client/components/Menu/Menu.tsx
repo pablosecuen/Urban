ffd07f19e76fddc14fe2483181bc170d8601172b
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-26 21:27:34 -0300

    guardados algunos estilos en variables para dejar un html mas limpio, trabajo parcial, con el tiempo esto hay que realizarlo en todo el sitio para modularilzar los estilos

--- client/components/Menu/Menu.tsx
+++ client/components/Menu/Menu.tsx
@@ -9,42 +9,49 @@ export default function Menu() {
   const pathname = usePathname();
   const isMobile = useMediaQuery({ maxWidth: 750 });
 
-  const containerStyles =
-    "relative flex flex-col gap-4 overflow-hidden rounded-2xl pb-8 pt-10 lg:w-2/3 lg:items-center lg:justify-center lg:px-24 lg:py-8 lg:shadow-2xl lg:shadow-black/40 2xl:w-3/4 2xl:py-16";
-  const imageStyles = "absolute z-20 scale-125 contrast-150 saturate-150 backdrop-blur-lg";
-  const overlayStyles = "absolute left-0 top-0 z-30 h-full w-full bg-black/40";
-  const linksContainerStyles = "z-50 flex h-full w-96 flex-col gap-4 p-8";
-  const linkStyles =
-    "w-full rounded-2xl bg-white py-1 text-center hover:bg-verde 2xl:w-full 2xl:text-lg";
-
   return (
     <>
       {!isMobile && (
-        <div className={containerStyles}>
-          <Image className={imageStyles} layout="fill" objectFit="cover" alt="" src={bg} />
-          <div className={overlayStyles}></div>
-          <div className={linksContainerStyles}>
+        <div className="relative flex flex-col gap-4 overflow-hidden rounded-2xl pb-8 pt-10 lg:w-2/3 lg:items-center lg:justify-center lg:px-24 lg:py-8 lg:shadow-2xl lg:shadow-black/40 2xl:w-3/4 2xl:py-16">
+          {" "}
+          <Image
+            className="absolute z-20 scale-125 contrast-150 saturate-150 backdrop-blur-lg "
+            layout="fill"
+            objectFit="cover"
+            alt=""
+            src={bg}
+          />
+          <div className="absolute left-0 top-0 z-30 h-full w-full bg-black/40"></div>
+          <div className="z-50 flex h-full w-96 flex-col gap-4  p-8">
             <Link
               href="/home/reserva"
-              className={`${linkStyles} ${pathname === "/home/reserva" && "!bg-verde"}`}
+              className={` w-full rounded-2xl bg-white py-1 text-center hover:bg-verde 2xl:w-full 2xl:text-lg ${
+                pathname === "/home/reserva" && "!bg-verde "
+              }`}
             >
               Reserva tu viaje
             </Link>
             <Link
               href="/home/gestion"
-              className={`${linkStyles} ${pathname === "/home/gestion" && "!bg-verde"}`}
+              className={`w-full rounded-2xl bg-white py-1 text-center hover:bg-verde 2xl:w-full 2xl:text-lg ${
+                pathname === "/home/gestion" && "!bg-verde "
+              }`}
             >
               Gestiona tus viajes
             </Link>
             <Link
               href="/home/paqueteria"
-              className={`${linkStyles} ${pathname === "/home/paqueteria" && "!bg-verde"}`}
+              className={`w-full rounded-2xl bg-white py-1 text-center hover:bg-verde 2xl:w-full 2xl:text-lg ${
+                pathname === "/home/paqueteria" && "!bg-verde "
+              }`}
             >
               Envío de paquetes
             </Link>
             <Link
               href="/home/especiales"
-              className={`${linkStyles} ${pathname === "/home/especiales" && "!bg-verde"}`}
+              className={`w-full rounded-2xl bg-white py-1 text-center hover:bg-verde 2xl:w-full 2xl:text-lg ${
+                pathname === "/home/especiales" && "!bg-verde "
+              }`}
             >
               Tengo una necesidad especial
             </Link>
