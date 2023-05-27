e7b46751c92ae41dad13f8a1775a04e5a70ac41d
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-27 20:11:08 -0300

    fix

--- client/app/home/especiales/confirmacion/pagos/checkout/page.tsx
+++ client/app/home/especiales/confirmacion/pagos/checkout/page.tsx
@@ -1,27 +1,61 @@
-"use client";
-import axios from "axios";
-import Image, { StaticImageData } from "next/image";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Image from "next/image";
 import Link from "next/link";
 import success from "../../../../../../assets/imagenes/success.png";
-import { useState } from "react";
-//import CardCheckout from "@component/components/Cards/CardCheckout";
+import axios from "axios";
+
+interface Product {
+  id: string;
+  unitPrice: number;
+  quantity: number;
+}
+
+interface RequestData {
+  userId: string;
+  products: Product[];
+  paymentId: string | null;
+  merchantOrder: string | null;
+  status: string | null;
+}
 
 export default function Checkout() {
-  //datos mercadopago
-  const queryParams = new URLSearchParams(location.search);
-  const paymentId: string | null = queryParams.get("payment_id"); //id del pago de mp
-  const merchantOrder: string | null = queryParams.get("merchant_order_id"); //codigo factura
-  const status: string | null = queryParams.get("status"); //estado de exito o no
-  const [dataState, setDataState] = useState({});
-  const userId: string | null = "";
+  const router = useRouter();
+  const [dataState, setDataState] = useState<RequestData>({
+    userId: "",
+    products: [],
+    paymentId: null,
+    merchantOrder: null,
+    status: null,
+  });
 
-  const getToken = async () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const queryParams = new URLSearchParams(router.query.search as string);
+      const paymentId = queryParams.get("payment_id");
+      const merchantOrder = queryParams.get("merchant_order_id");
+      const status = queryParams.get("status");
+
+      if (paymentId && merchantOrder && status) {
+        const requestData = await getToken(paymentId, merchantOrder, status);
+        setDataState(requestData);
+      }
+    };
+
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getToken = async (
+    paymentId: string,
+    merchantOrder: string,
+    status: string
+  ): Promise<RequestData> => {
     const { data } = await axios.get(`/token?merchantOrder=${merchantOrder}`);
     //este axios pueeeede llegar a ser a mercadopago
-    setDataState(data); //aca se guarda la info y con esto generamos la factura
-    const requestData = {
-      userId: userId,
-      /// reemplazar la variable objeto por data
+
+    const requestData: RequestData = {
+      userId: "",
       products: data.items.map((product: any) => {
         return {
           id: product.id,
@@ -35,10 +69,11 @@ export default function Checkout() {
     };
     return requestData;
   };
+
   return (
     <div className="flex flex-col justify-center gap-8">
       <p className="text-center text-2xl font-bold">Su pago ha sido realizado con éxito !</p>
-      <Image src={success} alt="pago exitoso" className="h-80 w-80 self-center"></Image>
+      <Image src={success} alt="pago exitoso" className="h-80 w-80 self-center" />
 
       <Link href="/home">
         <button className="mx-auto flex  w-1/2 justify-center py-1 text-center">
