84c1d46fd8865d92a0f5cdd3111ee1360dbc9283
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-05-27 16:48:09 -0300

    ADD logica de plantaAltaAdmin

--- client/components/PlantaAltaAdmin/PlantaAltaAdmin.tsx
+++ client/components/PlantaAltaAdmin/PlantaAltaAdmin.tsx
@@ -1,69 +1,61 @@
 import Seat from "@component/assets/icons/svg/Seat";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { plantaAlta } from "../../assets/data";
 import { CardProfilePropsPassage } from "@component/app/types/Passages";
 
 const PlantaAltaAdmin: React.FC<CardProfilePropsPassage> = ({
   selectedPassage,
   newSeats,
+  disabledSeats,
   onSeatToggle,
+  onDisableSeatToggle,
 }) => {
-  const [seatEnabled, setSeatEnabled] = useState<boolean[]>([]);
+  const handleClick = (index: number) => {
+    const seatNumber = (index + 41).toString();
 
-  const handleSeatToggle = (seatIndex: number) => {
-    const seatNumber = seatIndex + 41;
-    onSeatToggle?.(seatNumber.toString());
-    setSeatEnabled((prevSeats) => {
-      const updatedSeats = [...prevSeats];
-      updatedSeats[seatIndex] = !updatedSeats[seatIndex];
-      return updatedSeats;
-    });
+    if (selectedPassage?.numberSeat.includes(seatNumber)) {
+      if (disabledSeats?.includes(seatNumber)) {
+        // Habilitar asiento
+        onDisableSeatToggle && onDisableSeatToggle(seatNumber);
+      } else {
+        // Deshabilitar asiento
+        onDisableSeatToggle && onDisableSeatToggle(seatNumber);
+      }
+      return; // No agregar asientos seleccionados por props a newSeats
+    }
+
+    onSeatToggle && onSeatToggle(seatNumber);
   };
-
-  useEffect(() => {
-    setSeatEnabled(Array(plantaAlta.length).fill(false));
-  }, []);
-
-  const isSeatSelected = (seatIndex: number) => {
-    const seatNumber = seatIndex + 41;
-    return selectedPassage?.numberSeat.includes(`${seatNumber}`);
-  };
-
-  if (!selectedPassage) {
-    return <div>Aun no has seleccionado ningún pasaje</div>;
-  }
 
   return (
     <div className='flex w-4/5 flex-col gap-4'>
       <p className='text-center'>Planta alta</p>
       <ul className='grid grid-cols-5 gap-2'>
         {plantaAlta.map((seat, index) => {
-          const seatNumber = index + 41;
+          const seatNumber = (index + 41).toString();
+          const isSelected = selectedPassage?.numberSeat.includes(seatNumber);
+          const isNewSeat = newSeats?.includes(seatNumber);
+          const isDisabledSeat = disabledSeats?.includes(seatNumber);
+          let seatColor = isSelected ? "#000000" : isNewSeat ? "#0000FF" : "#888888";
+          const seatStyle = isSelected ? {} : { cursor: "pointer" };
+
+          if (isDisabledSeat) {
+            seatColor = "#FF0000";
+          }
+
           return (
-            <React.Fragment key={seatNumber}>
+            <React.Fragment key={index}>
               {(index === 2 || (index - 2) % 4 === 0) && <li className='' />}
-              <li className='relative'>
+              <li className='relative' style={seatStyle}>
                 <input
                   type='checkbox'
-                  name={`checkbox-${seatNumber}`}
-                  id={`checkbox-${seatNumber}`}
+                  name={`checkbox-${seat}`}
+                  id={`checkbox-${seat}`}
                   className='absolute bottom-0 left-0 right-0 top-0 -z-10 opacity-0'
-                  onClick={() => handleSeatToggle(index)}
-                  disabled={isSeatSelected(index)}
+                  onClick={() => handleClick(index)}
                 />
-                <label
-                  className={`cursor-pointer ${
-                    seatEnabled[index] ? "hover:bg-blue-200" : "cursor-not-allowed"
-                  }`}
-                  htmlFor={`checkbox-${seatNumber}`}
-                >
-                  <Seat
-                    fill={
-                      isSeatSelected(index) ? "#000000" : seatEnabled[index] ? "#0000FF" : "#C0C0C0"
-                    }
-                    width='30px'
-                    height='36px'
-                  />
+                <label htmlFor={`checkbox-${seat}`} style={seatStyle}>
+                  <Seat fill={seatColor} width='30px' height='36px' />
                 </label>
               </li>
             </React.Fragment>
