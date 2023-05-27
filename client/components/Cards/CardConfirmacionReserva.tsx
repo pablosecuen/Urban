bcfbf040ed6456bb2f90c024be309921b2ab89ad
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-26 21:27:19 -0300

    guardados algunos estilos en variables para dejar un html mas limpio, trabajo parcial, con el tiempo esto hay que realizarlo en todo el sitio para modularilzar los estilos

--- client/components/Cards/CardConfirmacionReserva.tsx
+++ client/components/Cards/CardConfirmacionReserva.tsx
@@ -19,7 +19,6 @@ export default function CardConfirmacionReserva({ id }: { id: string }) {
   const passage = useSelector((state: RootState) => state.passage.passageById);
   const stock: any = passage?.stock; // Stock disponible del objeto (ejemplo)
   const router = useRouter();
-
   const handleIncrement = () => {
     if (count < stock) {
       setCount(count + 1);
@@ -39,50 +38,37 @@ export default function CardConfirmacionReserva({ id }: { id: string }) {
   const closeModal = () => {
     setIsModalOpen(false);
   };
-
   const handleClick = () => {
     dispatch(getPassagesIdForPayment([{ passageId: id, quantity: count }]));
-    router.push(`/home/reserva/${id}/buslayout`);
+    router.push(`/home/reserva/${id}}/buslayout`);
   };
-
   useEffect(() => {
     dispatch(getPassagesId(id));
   }, [id]); // Include 'count' and 'id' in the dependency array
 
-  const containerStyles = "bg-white  lg:flex";
-  const infoContainerStyles = "flex flex-col p-2 lg:w-1/2 lg:p-4";
-  const originStyles = "mx-auto flex w-20 flex-col text-center";
-  const destinationStyles = "mx-auto flex w-20 flex-col";
-  const timeStyles = "flex flex-col";
-  const priceStyles = "mx-auto flex w-20 flex-col text-center";
-  const counterStyles = "flex w-auto items-center justify-center px-28 text-center";
-  const modalOverlayStyles = "fixed inset-0 z-50 flex items-center justify-center bg-black/30";
-  const modalImageContainerStyles = "flex w-[800px] items-center justify-center";
-  const modalImageStyles = "w-full rounded-3xl shadow-2xl shadow-black/40";
-
   return (
     <>
-      <article className={containerStyles}>
-        <div className={infoContainerStyles}>
+      <article className="bg-white  lg:flex ">
+        <div className="flex flex-col p-2 lg:w-1/2 lg:p-4">
           <section className="flex p-2 lg:p-4">
-            <div className={originStyles}>
-              <small className="font-semibold">Origen: </small>
-              <small>{passage?.origin}</small>
+            <div className=" mx-auto flex w-20 flex-col text-center">
+              <small className=" font-semibold">Origen: </small>
+              <small className="">{passage?.origin}</small>
             </div>
-            <div className={destinationStyles}>
+            <div className=" mx-auto flex w-20 flex-col">
               <small className="text-center font-semibold">Destino: </small>
               <small className="text-center">{passage?.destination}</small>
             </div>
           </section>
-          <hr className="mx-auto w-3/4 bg-black/70" />
+          <hr className="mx-auto w-3/4 bg-black/70 " />
           <section className="flex flex-col gap-2">
-            <div className={timeStyles}>
+            <div className=" flex flex-col">
               <small className="text-center font-semibold">Salida: </small>
               <small className="text-center">
                 {passage?.departureTime}, {passage?.departureDate}
               </small>
             </div>
-            <div className={timeStyles}>
+            <div className=" flex flex-col">
               <small className="text-center font-semibold">Llegada: </small>
               <small className="text-center">
                 {passage?.arrivalTime}, {passage?.arrivalDate}
@@ -91,42 +77,49 @@ export default function CardConfirmacionReserva({ id }: { id: string }) {
             <hr className="mx-auto w-3/4 bg-black/70" />
           </section>
           <section className="flex p-2 lg:p-4">
-            <div className={priceStyles}>
-              <small className="font-semibold">Valor unitario: </small>
-              <small>${passage?.price}</small>
+            <div className=" mx-auto flex w-20 flex-col text-center">
+              <small className=" font-semibold">Valor unitario: </small>
+              <small className="">${passage?.price}</small>
             </div>
-            <div className={priceStyles}>
+            <div className=" mx-auto flex w-20 flex-col">
               <small className="text-center font-semibold">Pasajes disponibles: </small>
               <small className="text-center">{passage?.stock}</small>
             </div>
           </section>
-          <div className={counterStyles}>
-            <button onClick={handleDecrement}>-</button>
+          <div className="flex w-auto items-center justify-center px-28 text-center">
+            <button onClick={handleDecrement} className="w-auto">
+              -
+            </button>
             <small className="p-4">{count}</small>
-            <button onClick={handleIncrement}>+</button>
+            <button onClick={handleIncrement} className="w-auto">
+              +
+            </button>
           </div>
         </div>
         <div className="flex flex-col gap-5 lg:w-1/2">
           <section className="mx-auto flex h-1/3 w-64 justify-center">
-            <div className="flex h-32 w-64 items-center justify-center overflow-hidden">
+            <div className="flex h-32 w-64  items-center justify-center overflow-hidden ">
               <Image
                 onClick={handleOpenModal}
                 src={passage?.img !== undefined ? passage.img : ""}
                 alt="amaga-bolombolo"
-                className="transition_all cursor-zoom-in hover:scale-125"
+                className="transition_all  cursor-zoom-in hover:scale-125"
                 width={250}
                 height={250}
               />
             </div>
             {isModalOpen && (
               <div className="transition-opacity duration-700">
-                <div className={modalOverlayStyles} onClick={closeModal}>
-                  <div className={modalImageContainerStyles}>
+                <div
+                  className=" fixed inset-0 z-50 flex items-center justify-center  bg-black/30 "
+                  onClick={closeModal}
+                >
+                  <div className="flex w-[800px] items-center justify-center ">
                     <Image
                       onClick={handleOpenModal}
                       src={passage?.img !== undefined ? passage.img : ""}
                       alt="amaga-bolombolo"
-                      className={modalImageStyles}
+                      className="w-full rounded-3xl shadow-2xl shadow-black/40"
                       width={800}
                       height={800}
                     />
@@ -148,7 +141,7 @@ export default function CardConfirmacionReserva({ id }: { id: string }) {
                   >
                     <div className="flex gap-2">
                       <p className="text-sm font-semibold text-slate-800">USUARIO</p>
-                      <span className="text-sm font-semibold text-amber-700">
+                      <span className="text-sm font-semibold text-amber-700	">
                         {e.rating} estrellas
                       </span>
                     </div>
