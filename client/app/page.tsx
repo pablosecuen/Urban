5f8c0ba2e9f67c1879d1d5a825317e3a671df6dc
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-27 23:36:12 -0300

    asd

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,20 +6,26 @@ import Image from "next/image";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function LandingPage() {
-  // const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-  // useEffect(() => {
-  //   if (window) {
-  //     const userString = localStorage.getItem("user");
-  //     const userData = userString ? JSON.parse(userString) : null;
-  //     setUser(userData);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const userString = localStorage.getItem("user");
+      const userData = userString ? JSON.parse(userString) : null;
+      setUser(userData);
 
-  //     if (userData) {
-  //       window.location.href = "/home";
-  //     }
-  //   }
-  // }, []);
+      if (userData) {
+        window.location.href = "/home";
+      }
+    }
+  }, []);
 
+  useEffect(() => {
+    if (user) {
+      window.location.href = "/home";
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className="flex flex-col items-center justify-center  lg:flex-row">
       <GoogleOAuthProvider clientId="your-client-id">
