3340dcf24a381942d33b8a894f897f03f2899567
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-28 18:06:12 -0300

    asd

--- client/services/axiosInstance.ts
+++ client/services/axiosInstance.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // Aquí puedes configurar las opciones de tu instancia de Axios
-  baseURL: "https://api-urban.onrender.com",
-  // Otros opciones...
+  baseURL: "http://localhost:3000"
 });
 
 export default axiosInstance;
