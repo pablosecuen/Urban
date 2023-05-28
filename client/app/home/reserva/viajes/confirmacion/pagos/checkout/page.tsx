39a07a69846c92537ad8240923d5f0a2902229e4
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-28 00:15:02 -0300

    cambio de url de axios en cada archivo con llamadas a la end points

--- client/app/home/reserva/viajes/confirmacion/pagos/checkout/page.tsx
+++ client/app/home/reserva/viajes/confirmacion/pagos/checkout/page.tsx
@@ -93,7 +93,7 @@ export default function Checkout() {
     const getToken = async () => {
       try {
         const { data } = await axios.get(
-          `http://localhost:3000/payment/merchantOrder?merchantOrder=${merchantOrder}`
+          `https://api-urban.onrender.com/payment/merchantOrder?merchantOrder=${merchantOrder}`
         );
         //este axios pueeeede llegar a ser a mercadopago
         //aca se guarda la info y con esto generamos la factura
