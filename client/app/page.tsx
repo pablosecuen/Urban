e7186bfd0192f0cf4464d536e7c3f9fec08f4621
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-27 21:51:11 -0300

    asd

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,16 +2,9 @@
 import Login from "@component/components/Login/Login";
 import logo from "../assets/imagenes/UrbanLogo.png";
 import { useEffect } from "react";
-import Image, { StaticImageData } from "next/image";
+import Image from "next/image";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { useRouter } from "next/router";
-
-interface LoginProps {
-  onLogin: (email: string, password: string) => void;
-  onRegisterClick: () => void;
-  onRegister: (name: string, email: string, password: string) => void;
-  onLoginClick: () => void;
-}
 
 export default function LandingPage() {
   const userString = typeof window !== "undefined" ? localStorage.getItem("user") : null;
@@ -26,10 +19,10 @@ export default function LandingPage() {
 
   return (
     <div className="flex flex-col items-center justify-center  lg:flex-row">
-      <GoogleOAuthProvider clientId={"your-client-id"}>
+      <GoogleOAuthProvider clientId="your-client-id">
         <div className="mx-auto flex w-1/2 justify-center lg:justify-end lg:pr-28 ">
           <Image
-            src={logo as StaticImageData}
+            src={logo}
             alt="logo"
             className="aspect-ratio-square h-40 w-40  sm:h-72 sm:w-72  lg:h-96 lg:w-96"
           />
