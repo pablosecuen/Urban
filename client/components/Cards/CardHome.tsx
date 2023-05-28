46f658bbc080a20d6bdabcae4a94a7e0145ee028
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-27 23:26:27 -0300

    local storage comentados

--- client/components/Cards/CardHome.tsx
+++ client/components/Cards/CardHome.tsx
@@ -8,58 +8,58 @@ import "react-toastify/dist/ReactToastify.css";
 import InfiniteSlider from "../InfiniteSlider/InfiniteSlider";
 
 export default function CardHome() {
-  const [user, setUser] = React.useState<User | null>(null);
-  const [token, setToken] = React.useState<string | null>(null);
-  useEffect(() => {
-    if (window) {
-      const urlParams = new URLSearchParams(window.location.search);
-      const tokendata = urlParams.get("token");
-      const userString = localStorage.getItem("user");
-      const userData = userString ? JSON.parse(userString) : null;
-      setUser(userData);
-      setToken(tokendata);
-    }
-  }, []);
+  // const [user, setUser] = React.useState<User | null>(null);
+  // const [token, setToken] = React.useState<string | null>(null);
+  // useEffect(() => {
+  //   if (window) {
+  //     const urlParams = new URLSearchParams(location.search);
+  //     const tokendata = urlParams.get("token");
+  //     const userString = localStorage.getItem("user");
+  //     const userData = userString ? JSON.parse(userString) : null;
+  //     setUser(userData);
+  //     setToken(tokendata);
+  //   }
+  // }, []);
 
   const containerStyles =
     "mb-8 mt-10 flex w-full flex-col items-center justify-evenly rounded-3xl border-2 bg-white px-4 py-4 shadow-2xl shadow-black/40 min-[420px]:mt-32 min-[420px]:w-4/5 lg:mt-10 lg:h-[400px] xl:h-[450px] xl:w-full 2xl:h-4/5";
   const titleStyles = "py-4 text-center text-xl font-bold xl:text-2xl 2xl:text-2xl";
   const paragraphStyles = "w-full px-4 text-center lg:text-xs xl:text-base 2xl:text-lg";
 
-  const notifySuccess = () => {
-    if (!toast.isActive("success")) {
-      toast.success(`Bienvenido ${user?.name} `, {
-        toastId: "success",
-        position: "top-center",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
-      });
-    }
-  };
+  // const notifySuccess = () => {
+  //   if (!toast.isActive("success")) {
+  //     toast.success(`Bienvenido ${user?.name} `, {
+  //       toastId: "success",
+  //       position: "top-center",
+  //       autoClose: 5000,
+  //       hideProgressBar: false,
+  //       closeOnClick: true,
+  //       pauseOnHover: true,
+  //       draggable: true,
+  //       progress: undefined,
+  //       theme: "dark",
+  //     });
+  //   }
+  // };
 
-  if (user) {
-    notifySuccess();
-  }
-  useEffect(() => {
-    token &&
-      axios
-        .get("http://localhost:3000/user/decoding", {
-          headers: {
-            Authorization: "Bearer " + token,
-          },
-        })
-        .then((data) => {
-          const userData = data.data;
-          localStorage.setItem("user", JSON.stringify(userData));
-          location.replace("/home");
-        });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // if (user) {
+  //   notifySuccess();
+  // }
+  // useEffect(() => {
+  //   token &&
+  //     axios
+  //       .get("http://localhost:3000/user/decoding", {
+  //         headers: {
+  //           Authorization: "Bearer " + token,
+  //         },
+  //       })
+  //       .then((data) => {
+  //         const userData = data.data;
+  //         localStorage.setItem("user", JSON.stringify(userData));
+  //         location.replace("/home");
+  //       });
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
   return (
     <div className={containerStyles}>
