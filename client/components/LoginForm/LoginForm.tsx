534bb4b18c38912df383da9222474d13c94677b9
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-29 12:40:59 -0300

    fix estilos login y register

--- client/components/LoginForm/LoginForm.tsx
+++ client/components/LoginForm/LoginForm.tsx
@@ -69,22 +69,22 @@ const LoginForm = ({ isRegister, setIsRegister }: { isRegister: boolean; setIsRe
   return (
     <form
       onSubmit={handleLogin}
-      className='relative mx-2 h-[500px] w-96 max-w-md rounded-lg bg-white px-4 py-8 text-sm text-white/90 shadow-lg shadow-black/40'
+      className="relative mx-2 h-[500px] w-96 max-w-md rounded-lg bg-white px-4 py-8 text-sm text-white/90 shadow-lg shadow-black/40"
     >
       {/* div que contiene Email y Password */}
-      <div className='flex h-[250px] flex-col gap-5 px-5'>
+      <div className="flex h-[250px] flex-col gap-5 px-5 pt-10">
         {/* email */}
-        <div className='relative'>
+        <div className="relative">
           <label
-            className={`transition_all relative px-1 text-center ${
+            className={`transition_all relative px-1 pr-8  text-center ${
               errores.messageEmail ? "text-red-500" : ""
             }`}
           >
             Email:
           </label>
           <input
-            type='email'
-            name='email'
+            type="email"
+            name="email"
             value={userData.email}
             onChange={handleInputChange}
             className={`transition_all px-2 text-black ${
@@ -100,7 +100,7 @@ const LoginForm = ({ isRegister, setIsRegister }: { isRegister: boolean; setIsRe
           </small>
         </div>
         {/* password  */}
-        <div className='relative'>
+        <div className="relative">
           <label
             className={`relative px-1 text-center ${
               errores.messagePassword ? "text-red-500" : ""
@@ -109,8 +109,8 @@ const LoginForm = ({ isRegister, setIsRegister }: { isRegister: boolean; setIsRe
             Password:
           </label>
           <input
-            type='password'
-            name='password'
+            type="password"
+            name="password"
             value={userData.password}
             onChange={handleInputChange}
             className={`transition_all px-1  text-black ${
@@ -127,52 +127,50 @@ const LoginForm = ({ isRegister, setIsRegister }: { isRegister: boolean; setIsRe
         </div>
       </div>
       {/* div que contiene boton de login  */}
-      <div className='py-4 text-center'>
-        <button className='mx-auto w-1/2' onClick={handleLogin}>
+      <div className="py-4 text-center">
+        <button className="mx-auto px-[76px] py-[10px]" onClick={handleLogin}>
           Login
         </button>
       </div>
 
       {/* <button onClick={handleRegisterClick}>Apple Id</button> */}
 
-      <div className='mx-auto flex w-4/5 flex-col gap-2'>
+      <div className="mx-auto flex w-4/5 flex-col gap-2">
         <Link
-          className='mx-auto flex w-2/3 items-center justify-center gap-1  rounded-md border border-[#888] bg-white py-2  font-semibold text-[#757575] shadow-md shadow-black/30'
-          href='http://localhost:3000/login/auth/google'
-          rel='noopener noreferrer'
+          className="mx-auto flex w-2/3 items-center justify-center gap-1  rounded-md border border-[#888] bg-white py-2  font-semibold text-[#757575] shadow-md shadow-black/30"
+          href="http://localhost:3000/login/auth/google"
+          rel="noopener noreferrer"
         >
-          <Google width='23' height='23' />
+          <Google width="23" height="23" />
           Login with Google
         </Link>
         <Link
-          className='mx-auto flex w-2/3 items-center justify-center   gap-1 rounded-md border border-[#888] bg-white py-2  font-semibold text-blue shadow-md shadow-black/30'
-          href='/login/auth/google'
-          rel='noopener noreferrer'
+          className="mx-auto flex w-2/3 items-center justify-center   gap-1 rounded-md border border-[#888] bg-white py-2  font-semibold text-blue shadow-md shadow-black/30"
+          href="/login/auth/google"
+          rel="noopener noreferrer"
         >
-          <svg className=' h-6 w-6 fill-current' viewBox='0 0 24 24'>
-            <path d='M20.02 0H3.98A3.98 3.98 0 0 0 0 3.98v16.04A3.98 3.98 0 0 0 3.98 24h8.22v-9.29H8.65v-3.62h3.55V9.02c0-3.52 2.14-5.44 5.28-5.44 1.54 0 2.87.12 3.26.18v3.66l-2.23.001c-1.75 0-2.09.83-2.09 2.05v2.69h4.19l-.55 3.62h-3.64V24h7.12A3.98 3.98 0 0 0 24 20.02V3.98C24 1.78 22.2 0 20.02 0z' />
+          <svg className=" h-6 w-6 fill-current" viewBox="0 0 24 24">
+            <path d="M20.02 0H3.98A3.98 3.98 0 0 0 0 3.98v16.04A3.98 3.98 0 0 0 3.98 24h8.22v-9.29H8.65v-3.62h3.55V9.02c0-3.52 2.14-5.44 5.28-5.44 1.54 0 2.87.12 3.26.18v3.66l-2.23.001c-1.75 0-2.09.83-2.09 2.05v2.69h4.19l-.55 3.62h-3.64V24h7.12A3.98 3.98 0 0 0 24 20.02V3.98C24 1.78 22.2 0 20.02 0z" />
           </svg>
           Login with Facebook
         </Link>
       </div>
-      <div className='mt-2 flex'>
-        <small className='bottom-5 right-5 block pt-2 text-right text-black'>
-          Olvidaste tu contraseña?{" "}
-          <Link href='https://api-urban.onrender.com/recuperacion'>
-            <span className='font-semibold text-blue hover:cursor-pointer hover:underline'>
-              Click aqui
-            </span>
-          </Link>
-        </small>
-        <small className='bottom-5 right-5 block pt-2 text-right text-black'>
-          No tienes cuenta?{" "}
-          <span
-            onClick={handleRegisterClick}
-            className='font-semibold text-blue hover:cursor-pointer hover:underline'
-          >
-            Registrate aqui
+      <div className="mt-2 flex justify-evenly gap-6">
+        <Link
+          href="https://api-urban.onrender.com/recuperacion"
+          className="font-semibold text-blue hover:cursor-pointer hover:underline"
+        >
+          <span className="bottom-5 right-5 block pt-2 text-right text-[0.7rem] font-semibold text-blue hover:cursor-pointer hover:underline">
+            Olvidaste tu contraseña?{" "}
           </span>
-        </small>
+        </Link>
+
+        <span
+          onClick={handleRegisterClick}
+          className="bottom-5 right-5 flex gap-1 pt-2 text-right text-[0.7rem] font-semibold text-blue hover:cursor-pointer hover:underline"
+        >
+          <p className="text-gray-500">No tienes cuenta?</p>Registrate!
+        </span>
       </div>
       {/* <button onClick={handleRegisterClick}>Facebook</button> */}
     </form>
